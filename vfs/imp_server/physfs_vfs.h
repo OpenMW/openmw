d763b9dbb62fefe0f0ab4c641f051762fb7b1ad9
Nicolay Korslund <korslund@gmail.com>
2009-12-26 13:35:34 +0100

    Added PhysFS VFS server +tests

---
+++ vfs/imp_server/physfs_vfs.h
@@ -0,0 +1,71 @@
+#ifndef MANGLE_VFS_PHYSFS_SERVER_H
+#define MANGLE_VFS_PHYSFS_SERVER_H
+
+#include "../vfs.h"
+#include "../../stream/imp_server/phys_stream.h"
+
+#include <physfs.h>
+#include <assert.h>
+
+namespace Mangle {
+namespace VFS {
+
+/** @brief An interface into the PhysFS virtual file system library
+
+    You have to set up PhysFS on your own before using this class.
+ */
+class PhysVFS : public VFS
+{
+ public:
+  PhysVFS()
+    {
+      hasList = true;
+      hasFind = false;
+      isCaseSensitive = true;
+    }
+
+  /// Open a new data stream. Deleting the object should be enough to
+  /// close it.
+  virtual Stream::InputStream *open(const std::string &name)
+    { return new Stream::PhysFile(PHYSFS_openRead(name.c_str())); }
+
+  /// Check for the existence of a file
+  virtual bool isFile(const std::string &name) const
+    { return PHYSFS_exists(name.c_str()); }
+
+  /// Checks for a directory
+  virtual bool isDir(const std::string &name) const
+    { return PHYSFS_isDirectory(name.c_str()); }
+
+  /// This doesn't work
+  virtual FileInfo stat(const std::string &name) const
+    { assert(0); return FileInfo(); }
+
+  virtual FileInfoList list(const std::string& dir = "",
+                            bool recurse=true,
+                            bool dirs=false) const
+    {
+      char **files = PHYSFS_enumerateFiles(dir.c_str());
+      FileInfoList lst;
+
+      // Add all teh files
+      int i = 0;
+      while(files[i] != NULL)
+        {
+          FileInfo fi;
+          fi.name = files[i];
+          fi.isDir = false;
+
+          lst.push_back(fi);
+        }
+      return lst;
+    }
+
+  virtual FileInfoList find(const std::string& pattern,
+                            bool recursive=true,
+                            bool dirs=false) const
+    { assert(0); }
+};
+
+}} // namespaces
+#endif
---
+++ vfs/tests/physfs_server_test.cpp
@@ -0,0 +1,21 @@
+#include "../imp_server/physfs_vfs.h"
+
+#include "server_common.cpp"
+
+#include <physfs.h>
+
+int main()
+{
+  // Set up the library and paths
+  PHYSFS_init("blah");
+  PHYSFS_addToSearchPath("test.zip", 1);
+  PHYSFS_addToSearchPath("./", 1);
+
+  // Create our interface
+  PhysVFS vfs;
+
+  // Run the test
+  testAll(vfs);
+
+  return 0;
+}
