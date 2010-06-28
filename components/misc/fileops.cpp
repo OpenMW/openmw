67f89f27a256ae6a61aa188ed3d208419542942a
athile <athile.g@gmail.com>
2010-06-27 18:12:31 -0700

    Properly removing the old 'misc' file and fixing a minor error in the platform CMake

--- components/misc/fileops.cpp
+++
@@ -1,8 +0,0 @@
-#include "fileops.hpp"
-#include <boost/filesystem.hpp>
-
-bool isFile(const char *name)
-{
-  boost::filesystem::path cfg_file_path(name);
-  return boost::filesystem::exists(cfg_file_path);
-}
--- components/misc/fileops.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __FILEOPS_H_
-#define __FILEOPS_H_
-
-/// Check if a given path is an existing file (not a directory)
-bool isFile(const char *name);
-
-#endif
