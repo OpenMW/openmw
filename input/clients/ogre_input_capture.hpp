9f21081c13f70bc41e20b50c19ed2dac9b458833
Nicolay Korslund <korslund@gmail.com>
2010-07-10 13:29:44 +0200

    Added ogre input capturer

---
+++ input/clients/ogre_input_capture.hpp
@@ -0,0 +1,29 @@
+#ifndef MANGLE_INPUT_OGREINPUTFRAME_H
+#define MANGLE_INPUT_OGREINPUTFRAME_H
+
+/*
+  This Ogre FrameListener calls capture() on an input driver every frame.
+ */
+
+#include <OgreFrameListener.h>
+#include "../driver.hpp"
+
+namespace Mangle {
+namespace Input {
+
+  struct OgreInputCapture : Ogre::FrameListener
+  {
+    Mangle::Input::Driver &driver;
+
+    ExitListener(Mangle::Input::Driver &drv)
+      : driver(drv) {}
+
+    bool frameStarted(const FrameEvent &evt)
+    {
+      driver.capture();
+      return true;
+    }
+  };
+}
+
+#endif
