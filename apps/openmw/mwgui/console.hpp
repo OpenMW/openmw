281f2d1136f4b7190515c5822cb529ef165b1bf8
Marc Zinnschlag <marc@zpages.de>
2010-07-21 10:08:38 +0200

    added script interpreter for console

--- apps/openmw/mwgui/console.hpp
+++ apps/openmw/mwgui/console.hpp
@@ -10,10 +10,11 @@
 #include <components/compiler/lineparser.hpp>
 #include <components/compiler/scanner.hpp>
 #include <components/compiler/locals.hpp>
-#include <components/compiler/literals.hpp>
-#include <components/compiler/exception.hpp>
+#include <components/compiler/output.hpp>
+#include <components/interpreter/interpreter.hpp>
 
 #include "../mwscript/compilercontext.hpp"
+#include "../mwscript/interpretercontext.hpp"
 
 namespace MWGui
 {
@@ -22,51 +23,15 @@ namespace MWGui
     private:
     
         MWScript::CompilerContext mCompilerContext;
+        MWWorld::Environment& mEnvironment;
         
-        bool compile (const std::string& cmd)
-        {
-            try
-            {        
-                std::istringstream input (cmd + '\n');
-            
-                Compiler::Scanner scanner (*this, input, mCompilerContext.getExtensions());
-            
-                Compiler::Locals locals;
-                Compiler::Literals literals;
-                std::vector<Interpreter::Type_Code> code;
-                Compiler::LineParser parser (*this, mCompilerContext, locals, literals, code);
-            
-                scanner.scan (parser);
-                
-                return isGood();
-            }
-            catch (const Compiler::SourceException&)
-            {
-                // error has already been reported via error handler
-            }
-            catch (const std::exception& error)
-            {
-                printError (std::string ("An exception has been thrown: ") + error.what());
-            }
-                    
-            return false;
-        }
+        bool compile (const std::string& cmd, Compiler::Output& output);
 
         /// Report error to the user.
-        virtual void report (const std::string& message, const Compiler::TokenLoc& loc, Type type)
-        {
-            std::ostringstream error;
-            error << "column " << loc.mColumn << " (" << loc.mLiteral << "):";
-        
-            printError (error.str());
-            printError ((type==ErrorMessage ? "error: " : "warning: ") + message);
-        }
+        virtual void report (const std::string& message, const Compiler::TokenLoc& loc, Type type);
 
         /// Report a file related error
-        virtual void report (const std::string& message, Type type)
-        {
-            printError ((type==ErrorMessage ? "error: " : "warning: ") + message);
-        }
+        virtual void report (const std::string& message, Type type);
                     
   public:
     MyGUI::EditPtr command;
@@ -79,127 +44,35 @@ namespace MWGui
     StringList::iterator current;
     std::string editString;
 
-    Console(int w, int h, MWWorld::Environment& environment, const Compiler::Extensions& extensions)
-      : Layout("openmw_console_layout.xml"),
-        mCompilerContext (MWScript::CompilerContext::Type_Console, environment)
-    {
-      setCoord(10,10, w-10, h/2);
+    Console(int w, int h, MWWorld::Environment& environment, const Compiler::Extensions& extensions);
 
-      getWidget(command, "edit_Command");
-      getWidget(history, "list_History");
+    void enable();
 
-      // Set up the command line box
-      command->eventEditSelectAccept =
-        newDelegate(this, &Console::acceptCommand);
-      command->eventKeyButtonPressed =
-        newDelegate(this, &Console::keyPress);
+    void disable();
 
-      // Set up the log window
-      history->setOverflowToTheLeft(true);
-      history->setEditStatic(true);
-      history->setVisibleVScroll(true);
-      
-      // compiler
-      mCompilerContext.setExtensions (&extensions);
-    }
+    void setFont(const std::string &fntName);
 
-    void enable()
-    {
-      setVisible(true);
-
-      // Give keyboard focus to the combo box whenever the console is
-      // turned on
-      MyGUI::InputManager::getInstance().setKeyFocusWidget(command);
-    }
-
-    void disable()
-    {
-      setVisible(false);
-    }
-
-    void setFont(const std::string &fntName)
-    {
-      history->setFontName(fntName);
-      command->setFontName(fntName);
-    }
-
-    void clearHistory()
-    {
-      history->setCaption("");
-    }
+    void clearHistory();
 
     // Print a message to the console. Messages may contain color
     // code, eg. "#FFFFFF this is white".
-    void print(const std::string &msg)
-    { history->addText(msg); }
+    void print(const std::string &msg);
 
     // These are pre-colored versions that you should use.
 
     /// Output from successful console command
-    void printOK(const std::string &msg)
-    {  print("#FF00FF" + msg + "\n"); }
+    void printOK(const std::string &msg);
 
     /// Error message
-    void printError(const std::string &msg)
-    {  print("#FF2222" + msg + "\n"); }
+    void printError(const std::string &msg);
 
   private:
 
     void keyPress(MyGUI::WidgetPtr _sender,
                   MyGUI::KeyCode key,
-                  MyGUI::Char _char)
-    {
-      if(command_history.empty()) return;
+                  MyGUI::Char _char);
 
-      // Traverse history with up and down arrows
-      if(key == MyGUI::KeyCode::ArrowUp)
-        {
-          // If the user was editing a string, store it for later
-          if(current == command_history.end())
-            editString = command->getCaption();
-
-          if(current != command_history.begin())
-            {
-              current--;
-              command->setCaption(*current);
-            }
-        }
-      else if(key == MyGUI::KeyCode::ArrowDown)
-        {
-          if(current != command_history.end())
-            {
-              current++;
-
-              if(current != command_history.end())
-                command->setCaption(*current);
-              else
-                // Restore the edit string
-                command->setCaption(editString);
-            }
-        }
-    }
-
-    void acceptCommand(MyGUI::EditPtr _sender)
-    {
-      const std::string &cm = command->getCaption();
-      if(cm.empty()) return;
-
-      // Add the command to the history, and set the current pointer to
-      // the end of the list
-      command_history.push_back(cm);
-      current = command_history.end();
-      editString.clear();
-
-      // Log the command
-      print("#FFFFFF> " + cm + "\n");
-
-      if (compile (cm))
-      {
-        // TODO execute command
-      }
-
-      command->setCaption("");
-    }
+    void acceptCommand(MyGUI::EditPtr _sender);
   };
 }
 #endif
