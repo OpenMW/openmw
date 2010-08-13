5d4dba981fa56f1cf43c0a68f537401e9d9618e3
Nicolay Korslund <korslund@gmail.com>
2010-08-13 15:51:42 +0200

    Fixed mistake in NPC record struct

---
+++ components/esm/load_impl.cpp
@@ -0,0 +1,48 @@
+#include "records.hpp"
+
+/** Implementation for some of the load() functions. Most are found in
+    the header files themselves, but this is a bit irritating to
+    compile if you're changing the functions often, as virtually the
+    entire engine depends on these headers.
+ */
+
+namespace ESM
+{
+  void NPC::load(ESMReader &esm, const std::string& id)
+  {
+    mId = id;
+
+    npdt52.gold = -10;
+
+    model = esm.getHNOString("MODL");
+    name = esm.getHNOString("FNAM");
+
+    race = esm.getHNString("RNAM");
+    cls = esm.getHNString("CNAM");
+    faction = esm.getHNString("ANAM");
+    head = esm.getHNString("BNAM");
+    hair = esm.getHNString("KNAM");
+
+    script = esm.getHNOString("SCRI");
+
+    esm.getSubNameIs("NPDT");
+    esm.getSubHeader();
+    if(esm.getSubSize() == 52) esm.getExact(&npdt52, 52);
+    else if(esm.getSubSize() == 12) esm.getExact(&npdt12, 12);
+    else esm.fail("NPC_NPDT must be 12 or 52 bytes long");
+
+    esm.getHNT(flags, "FLAG");
+
+    inventory.load(esm);
+    spells.load(esm);
+
+    if(esm.isNextSub("AIDT"))
+      {
+	esm.getHExact(&AI, sizeof(AI));
+	hasAI = true;
+      }
+    else hasAI = false;
+
+    esm.skipRecord();
+  }
+}
