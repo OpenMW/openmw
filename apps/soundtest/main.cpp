cf4185faef2db62e9baea94eb36c6e0ab2880e39
Nicolay Korslund <korslund@gmail.com>
2010-08-13 14:43:57 +0200

    Added sound test (remember to git submodule update)

---
+++ apps/soundtest/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include <mangle/stream/servers/file_stream.hpp>
+#include <mangle/sound/filters/openal_audiere.hpp>
+#include <mangle/sound/sources/stream_source.hpp>
+#include <mangle/stream/filters/buffer_stream.hpp>
+
+using namespace std;
+using namespace Mangle::Stream;
+using namespace Mangle::Sound;
+
+AudiereLoader loader;
+OpenAL_Factory openal;
+
+void play(const char* name)
+{
+  try
+    {
+      cout << "Opening " << name << " via Audiere\n";
+      SampleSourcePtr samples = loader.load(name);
+      cout << "Loading entire file into memory\n";
+      StreamPtr buf(new BufferStream(samples));
+
+      // Recreate the stream as a sample source (we're only doing it
+      // this complicated to test each step individually)
+      int a,b,c;
+      samples->getInfo(&a,&b,&c);
+      samples.reset(new Stream2Samples(buf, a,b,c));
+
+      cout << "Creating OpenAL sound from data\n";
+      SoundPtr snd = openal.loadRaw(samples);
+      cout << "Playing (abort with Ctrl-C)\n";
+      snd->play();
+
+      while(snd->isPlaying())
+        usleep(10000);
+      cout << "Done playing\n";
+    }
+  catch(exception &e)
+    {
+      cout << "  ERROR: " << e.what() << "\n";
+    }
+}
+
+int main(int argc, char** argv)
+{
+  if(argc==1)
+    cout << "Specify sound file (wav, mp3, ogg) on command line.\n";
+  for(int i=1; i<argc; i++)
+    play(argv[i]);
+  return 0;
+}
