1c79a629577448c2d1a3aaf080e384c1485c1884
Jan Borsodi <jborsodi@gmail.com>
2010-09-14 14:12:19 +0200

    Added a static list of skill names to ESM::Skill.

--- components/esm/loadskil.hpp
+++ components/esm/loadskil.hpp
@@ -30,6 +30,39 @@ struct Skill
 
   std::string description;
 
+    enum SkillEnum
+    {
+        Block = 0,
+        Armorer = 1,
+        MediumArmor = 2,
+        HeavyArmor = 3,
+        BluntWeapon = 4,
+        LongBlade = 5,
+        Axe = 6,
+        Spear = 7,
+        Athletics = 8,
+        Enchant = 9,
+        Destruction = 10,
+        Alteration = 11,
+        Illusion = 12,
+        Conjuration = 13,
+        Mysticism = 14,
+        Restoration = 15,
+        Alchemy = 16,
+        Unarmored = 17,
+        Security = 18,
+        Sneak = 19,
+        Acrobatics = 20,
+        LightArmor = 21,
+        ShortBlade = 22,
+        Marksman = 23,
+        Mercantile = 24,
+        Speechcraft = 25,
+        HandToHand = 26,
+        Length
+    };
+  static const std::string sSkillNames[Length];
+
   void load(ESMReader &esm)
     {
       esm.getHNT(data, "SKDT", 24);
---
+++ components/esm/skill.cpp
@@ -0,0 +1,34 @@
+#include "loadskil.hpp"
+
+namespace ESM
+{
+    const std::string Skill::sSkillNames[Length] = {
+		"Block",
+		"Armorer",
+		"Medium Armor",
+		"Heavy Armor",
+		"Blunt Weapon",
+		"Long Blade",
+		"Axe",
+		"Spear",
+		"Athletics",
+		"Enchant",
+		"Destruction",
+		"Alteration",
+		"Illusion",
+		"Conjuration",
+		"Mysticism",
+		"Restoration",
+		"Alchemy",
+		"Unarmored",
+		"Security",
+		"Sneak",
+		"Acrobatics",
+		"Light Armor",
+		"Short Blade",
+		"Marksman",
+        "Mercantile",
+		"Speechcraft",
+		"Hand To Hand",
+	};
+}
