7bfae798e8d6dd0fbb0e9fed6f5afff9ab12de2c
Jan Borsodi <jborsodi@gmail.com>
2010-09-18 21:36:16 +0200

    Changed indentation of stats window class from 2 spaces and to 4 spaces.

--- apps/openmw/mwgui/layouts.hpp
+++ apps/openmw/mwgui/layouts.hpp
@@ -177,68 +177,68 @@ namespace MWGui
     }
   };
 
-  class StatsWindow : public OEngine::GUI::Layout
-  {
-  public:
-    typedef std::pair<std::string, int> Faction;
-    typedef std::vector<Faction> FactionList;
+    class StatsWindow : public OEngine::GUI::Layout
+    {
+    public:
+        typedef std::pair<std::string, int> Faction;
+        typedef std::vector<Faction> FactionList;
 
-    typedef std::vector<int> SkillList;
+        typedef std::vector<int> SkillList;
 
-    StatsWindow (MWWorld::Environment& environment);
+        StatsWindow (MWWorld::Environment& environment);
 
-    void setBar(const std::string& name, const std::string& tname, int val, int max);
-    void setPlayerName(const std::string& playerName);
+        void setBar(const std::string& name, const std::string& tname, int val, int max);
+        void setPlayerName(const std::string& playerName);
 
-    /// Set value for the given ID.
-    void setValue (const std::string& id, const MWMechanics::Stat<int>& value);
-    void setValue (const std::string& id, const MWMechanics::DynamicStat<int>& value);
-    void setValue (const std::string& id, const std::string& value);
-    void setValue (const std::string& id, int value);
+        /// Set value for the given ID.
+        void setValue (const std::string& id, const MWMechanics::Stat<int>& value);
+        void setValue (const std::string& id, const MWMechanics::DynamicStat<int>& value);
+        void setValue (const std::string& id, const std::string& value);
+        void setValue (const std::string& id, int value);
 
-    void setValue (const std::string& id, const MWMechanics::Stat<float>& value);
+        void setValue (const std::string& id, const MWMechanics::Stat<float>& value);
 
-      void configureSkills (const SkillList& major, const SkillList& minor);
-      void setFactions (const std::vector<Faction>& factions);
-      void setBirthSign (const std::string &signId);
-      void setReputation (int reputation) { this->reputation = reputation; }
-      void setBounty (int bounty) { this->bounty = bounty; }
-      void updateSkillArea();
+        void configureSkills (const SkillList& major, const SkillList& minor);
+        void setFactions (const std::vector<Faction>& factions);
+        void setBirthSign (const std::string &signId);
+        void setReputation (int reputation) { this->reputation = reputation; }
+        void setBounty (int bounty) { this->bounty = bounty; }
+        void updateSkillArea();
 
-  private:
-      enum ColorStyle
-      {
+    private:
+        enum ColorStyle
+        {
           CS_Sub,
           CS_Normal,
           CS_Super
-      };
-      void setStyledText(MyGUI::WidgetPtr widget, ColorStyle style, const std::string &value);
-      void addSkills(const SkillList &skills, const std::string &titleId, const std::string &titleDefault, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
-      void addSeparator(MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
-      void addGroup(const std::string &label, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
-      MyGUI::WidgetPtr addValueItem(const std::string text, const std::string &value, ColorStyle style, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
-      void addItem(const std::string text, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
-      void updateScroller();
+        };
+        void setStyledText(MyGUI::WidgetPtr widget, ColorStyle style, const std::string &value);
+        void addSkills(const SkillList &skills, const std::string &titleId, const std::string &titleDefault, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
+        void addSeparator(MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
+        void addGroup(const std::string &label, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
+        MyGUI::WidgetPtr addValueItem(const std::string text, const std::string &value, ColorStyle style, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
+        void addItem(const std::string text, MyGUI::IntCoord &coord1, MyGUI::IntCoord &coord2);
+        void updateScroller();
 
-      void onScrollChangePosition(MyGUI::VScrollPtr scroller, size_t pos);
-      void onWindowResize(MyGUI::WidgetPtr window);
+        void onScrollChangePosition(MyGUI::VScrollPtr scroller, size_t pos);
+        void onWindowResize(MyGUI::WidgetPtr window);
 
-      static const int lineHeight;
+        static const int lineHeight;
 
-      MWWorld::Environment& environment;
-      MyGUI::WidgetPtr skillAreaWidget, skillClientWidget;
-      MyGUI::VScrollPtr skillScrollerWidget;
-      int lastPos, clientHeight;
+        MWWorld::Environment& environment;
+        MyGUI::WidgetPtr skillAreaWidget, skillClientWidget;
+        MyGUI::VScrollPtr skillScrollerWidget;
+        int lastPos, clientHeight;
 
-      SkillList majorSkills, minorSkills, miscSkills;
-      std::map<int, MWMechanics::Stat<float> > skillValues;
-      std::map<int, MyGUI::WidgetPtr> skillWidgetMap;
-      std::map<std::string, MyGUI::WidgetPtr> factionWidgetMap;
-      FactionList factions; ///< Stores a list of factions and the current rank
-      std::string birthSignId;
-      int reputation, bounty;
-      std::vector<MyGUI::WidgetPtr> skillWidgets; //< Skills and other information
-  };
+        SkillList majorSkills, minorSkills, miscSkills;
+        std::map<int, MWMechanics::Stat<float> > skillValues;
+        std::map<int, MyGUI::WidgetPtr> skillWidgetMap;
+        std::map<std::string, MyGUI::WidgetPtr> factionWidgetMap;
+        FactionList factions; ///< Stores a list of factions and the current rank
+        std::string birthSignId;
+        int reputation, bounty;
+        std::vector<MyGUI::WidgetPtr> skillWidgets; //< Skills and other information
+    };
 
 #if 0
   class InventoryWindow : public OEngine::GUI::Layout
