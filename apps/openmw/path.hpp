a724de2429abffb0dd411c0c2ddd5cf669aebea1
Jan-Peter Nilsson <peppe@pappkartong.se>
2011-01-14 19:43:51 +0100

    Follow the 'XDG Base Directory Specification' instead of putting the configfile in ~/.openmw/
    Also updated based on feedback from athile:
    Fix broken Linux elif.
    Use boost:filesystem instead of mkdir for increased future portability.
    Break appart class definition and implementation.

--- apps/openmw/path.hpp
+++ apps/openmw/path.hpp
@@ -4,12 +4,6 @@
 #include <OgrePlatform.h>
 #include <string>
 
-#if OGRE_PLATFORM_LINUX
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdlib.h> //getenv
-#endif
-
 namespace OMW
 {
     class Path
@@ -21,46 +15,7 @@ namespace OMW
                 GLOBAL_CFG_PATH
             };
 
-            //TODO use application data dir on windows?
-            static std::string getPath(PathTypeEnum parType, const std::string parApp, const std::string parFile)
-            {
-                std::string theBasePath;
-                if(parType == GLOBAL_CFG_PATH)
-                {
-#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
-                    theBasePath = macBundlePath() + "/Contents/MacOS/"; //FIXME do we have global/local with OSX?
-#elif OGRE_PLATFORM_LINUX
-                    theBasePath = "/etc/"+parApp+"/";
-#else
-                    theBasePath = "";
-#endif
-
-                }
-                else
-                {
-#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
-                    theBasePath = macBundlePath() + "/Contents/MacOS/"; //FIXME do we have global/local with OSX?
-#elif OGRE_PLATFORM_LINUX
-                    const char* homedir;
-                    if ((homedir = getenv("OPENMW_HOME")) != NULL)
-                    {
-                        theBasePath = std::string(homedir)+"/";
-                    }
-                    else
-                    {
-                        if ((homedir = getenv("HOME")) == NULL)
-                            return NULL;
-                        theBasePath = std::string(homedir)+"/."+parApp+"/";
-                    }
-                    mkdir(theBasePath.c_str(), 0777);
-#else
-                    theBasePath = "";
-#endif
-                }
-
-                theBasePath.append(parFile);
-                return theBasePath;
-            }
+            static std::string getPath(PathTypeEnum parType, const std::string parApp, const std::string parFile);
     };
 }
 #endif
