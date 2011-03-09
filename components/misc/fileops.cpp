3552ad045db07cd14a3639d4d61a3ccf7e84312d
Nikolay Kasyanov <corrmage@gmail.com>
2011-03-09 21:54:01 +0300

    Removed obsolete macBundlePath function from fileops.cpp. Using Ogre::macBundlePath

--- components/misc/fileops.cpp
+++ components/misc/fileops.cpp
@@ -10,26 +10,3 @@ bool isFile(const char *name)
   return boost::filesystem::exists(cfg_file_path);
 }
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
-#include <CoreFoundation/CoreFoundation.h>
-
-std::string macBundlePath()
-{
-    char path[1024];
-    CFBundleRef mainBundle = CFBundleGetMainBundle();
-    assert(mainBundle);
-
-    CFURLRef mainBundleURL = CFBundleCopyBundleURL(mainBundle);
-    assert(mainBundleURL);
-
-    CFStringRef cfStringRef = CFURLCopyFileSystemPath(mainBundleURL, kCFURLPOSIXPathStyle);
-    assert(cfStringRef);
-
-    CFStringGetCString(cfStringRef, path, 1024, kCFStringEncodingASCII);
-
-    CFRelease(mainBundleURL);
-    CFRelease(cfStringRef);
-
-    return std::string(path);
-}
-#endif
