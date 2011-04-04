b40117449a2bd282fe5210f0d1fd775021a989ca
Marc Zinnschlag <marc@zpages.de>
2011-04-04 11:23:15 +0200

    added journal script interface

--- apps/openmw/mwdialogue/journal.cpp
+++ apps/openmw/mwdialogue/journal.cpp
@@ -1,10 +1,26 @@
 
 #include "journal.hpp"
 
+
+#include <iostream>
 namespace MWDialogue
 {
-    MWDialogue::Journal::Journal (MWWorld::Environment& environment)
+    Journal::Journal (MWWorld::Environment& environment)
     : mEnvironment (environment)
     {}
 
+    void Journal::addEntry (const std::string& id, int index)
+    {
+        std::cout << "journal: " << id << " at " << index << std::endl;
+    }
+
+    void Journal::setJournalIndex (const std::string& id, int index)
+    {
+        std::cout << "journal (no entry): " << id << " at " << index << std::endl;
+    }
+
+    int Journal::getJournalIndex (const std::string& id) const
+    {
+        return 0;
+    }
 }
--- apps/openmw/mwdialogue/journal.hpp
+++ apps/openmw/mwdialogue/journal.hpp
@@ -1,6 +1,8 @@
 #ifndef GAME_MMDIALOG_JOURNAL_H
 #define GAME_MWDIALOG_JOURNAL_H
 
+#include <string>
+
 namespace MWWorld
 {
     struct Environment;
@@ -15,6 +17,15 @@ namespace MWDialogue
         public:
 
             Journal (MWWorld::Environment& environment);
+
+            void addEntry (const std::string& id, int index);
+            ///< Add a journal entry.
+
+            void setJournalIndex (const std::string& id, int index);
+            ///< Set the journal index without adding an entry.
+
+            int getJournalIndex (const std::string& id) const;
+            ///< Get the journal index.
     };
 }
 
