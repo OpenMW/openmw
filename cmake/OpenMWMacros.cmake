ad086312b8a2be033d45cbee6c3423773b98422e
Marc Zinnschlag <marc@zpages.de>
2011-10-22 13:55:06 +0200

    testing: cmake script simplification

---
+++ cmake/OpenMWMacros.cmake
@@ -0,0 +1,9 @@
+
+macro (add_openmw_dir dir)
+set (files)
+foreach (u ${ARGN})
+list (APPEND files "${dir}/${u}")
+list (APPEND OPENMW_FILES "${dir}/${u}")
+endforeach (u)
+source_group (apps\\openmw\\${dir} FILES ${files})
+endmacro (add_openmw_dir)
