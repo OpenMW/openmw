dc878e2c30acb0edd062ee05c56f013547bf698c
Marc Zinnschlag <marc@zpages.de>
2011-10-22 22:40:31 +0200

    testing MSVC fix, another try

--- cmake/OpenMWMacros.cmake
+++ cmake/OpenMWMacros.cmake
@@ -2,7 +2,7 @@
 macro (add_openmw_dir dir)
 set (files)
 foreach (u ${ARGN})
-list (APPEND files "${dir}/${u}")
+list (APPEND files "${dir}/${u}.*")
 list (APPEND OPENMW_FILES "${dir}/${u}")
 endforeach (u)
 source_group ("apps\\openmw\\${dir}" FILES ${files})
