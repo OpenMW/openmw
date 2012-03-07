08db70e1370d9dbc2574f9dc467d2ae8c6ae624f
Nikolay Kasyanov <corrmage@gmail.com>
2012-03-08 01:00:13 +0400

    disabled debug output

--- components/esm/loadpgrd.cpp
+++ components/esm/loadpgrd.cpp
@@ -18,7 +18,7 @@ void PathGrid::load(ESMReader &esm)
 {
     esm.getHNT(data, "DATA", 12);
     cell = esm.getHNString("NAME");
-    std::cout << "loading PGRD for " << cell << " x=" << data.x << " y=" << data.y << std::endl;
+    //std::cout << "loading PGRD for " << cell << " x=" << data.x << " y=" << data.y << std::endl;
 
     // Remember this file position
     context = esm.getContext();
@@ -28,22 +28,22 @@ void PathGrid::load(ESMReader &esm)
     {
         esm.getSubHeader();
         int size = esm.getSubSize();
-        std::cout << "PGRP size is " << size << std::endl;
+        //std::cout << "PGRP size is " << size << std::endl;
         if (size != 16 * data.s2)
             esm.fail("Path grid table size mismatch");
         else
         {
             pointCount = data.s2;
-            std::cout << "Path grid points count is " << data.s2 << std::endl;
+            //std::cout << "Path grid points count is " << data.s2 << std::endl;
             points = new Point[pointCount];
             esm.getExact(points, size);
-            for (int i = 0; i < pointCount; ++i)
-            {
-                std::cout << i << "'s point: " << points[i].x;
-                std::cout << " " << points[i].y;
-                std::cout << " " << points[i].z;
-                std::cout << std::endl;
-            }
+//            for (int i = 0; i < pointCount; ++i)
+//            {
+//                std::cout << i << "'s point: " << points[i].x;
+//                std::cout << " " << points[i].y;
+//                std::cout << " " << points[i].z;
+//                std::cout << std::endl;
+//            }
         }
     }
 
@@ -54,23 +54,22 @@ void PathGrid::load(ESMReader &esm)
     {
         esm.getSubHeader();
         int size = esm.getSubSize();
-        std::cout << "PGRC size is " << size << std::endl;
+        //std::cout << "PGRC size is " << size << std::endl;
         if (size % 4 != 0)
             esm.fail("PGRC size not a multiple of 4");
         else
         {
             edgeCount = size / sizeof(Edge);
-            std::cout << "Path grid edge count is " << edgeCount << std::endl;
+            //std::cout << "Path grid edge count is " << edgeCount << std::endl;
             edges = new Edge[edgeCount];
             esm.getExact(edges, size);
-            for (int i = 0; i < edgeCount; ++i)
-            {
-                std::cout << i << "'s edge: " << edges[i].v0 << " "
-                << edges[i].v1 << std::endl;
-            }
+//            for (int i = 0; i < edgeCount; ++i)
+//            {
+//                std::cout << i << "'s edge: " << edges[i].v0 << " "
+//                << edges[i].v1 << std::endl;
+//            }
         }
     }
-    std::cin.ignore(1);
 }
 
 }
