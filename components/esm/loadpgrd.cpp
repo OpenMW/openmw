988c1111e9e42aee9b90876021c09580ac709773
Nikolay Kasyanov <corrmage@gmail.com>
2012-03-14 12:45:42 +0400

    Feature #161 (Fix) Load REC_PGRD records fixed edge decoding, thanks Project Aedra!

--- components/esm/loadpgrd.cpp
+++ components/esm/loadpgrd.cpp
@@ -7,37 +7,28 @@ void Pathgrid::load(ESMReader &esm)
 {
     esm.getHNT(data, "DATA", 12);
     cell = esm.getHNString("NAME");
-//    std::cout << "loading PGRD for " <<
-//                 cell << " x=" << data.x << " y=" << data.y <<
-//                 " " << data.s1
-//                 << std::endl;
 
-    // Check that the sizes match up. Size = 16 * s2 (path points)
+    // keep track of total connections so we can reserve edge vector size
+    int edgeCount = 0;
+
     if (esm.isNextSub("PGRP"))
     {
         esm.getSubHeader();
         int size = esm.getSubSize();
-        //std::cout << "PGRP size is " << size << std::endl;
+        // Check that the sizes match up. Size = 16 * s2 (path points)
         if (size != sizeof(Point) * data.s2)
-            esm.fail("Path grid table size mismatch");
+            esm.fail("Path point subrecord size mismatch");
         else
         {
             int pointCount = data.s2;
-            //std::cout << "Path grid points count is " << data.s2 << std::endl;
             points.reserve(pointCount);
             for (int i = 0; i < pointCount; ++i)
             {
                 Point p;
                 esm.getExact(&p, sizeof(Point));
                 points.push_back(p);
+                edgeCount += p.connectionNum;
             }
-//            for (int i = 0; i < pointCount; ++i)
-//            {
-//                std::cout << i << "'s point: " << points[i].x;
-//                std::cout << " " << points[i].y;
-//                std::cout << " " << points[i].z;
-//                std::cout << std::endl;
-//            }
         }
     }
 
@@ -45,31 +36,34 @@ void Pathgrid::load(ESMReader &esm)
     {
         esm.getSubHeader();
         int size = esm.getSubSize();
-        //std::cout << "PGRC size is " << size << std::endl;
         if (size % sizeof(int) != 0)
-            esm.fail("PGRC size not a multiple of 8");
+            esm.fail("PGRC size not a multiple of 4");
         else
         {
-            int edgeCount = size / sizeof(int) - 1;
-            //std::cout << "Path grid edge count is " << edgeCount << std::endl;
-            edges.reserve(edgeCount);
-            int prevValue;
-            esm.getT(prevValue);
-            for (int i = 0; i < edgeCount; ++i)
+            int rawConnNum = size / sizeof(int);
+            std::vector<int> rawConnections;
+            rawConnections.reserve(rawConnNum);
+            for (int i = 0; i < rawConnNum; ++i)
             {
-                int nextValue;
-                esm.getT(nextValue);
-                Edge e;
-                e.v0 = prevValue;
-                e.v1 = nextValue;
-                edges.push_back(e);
-                prevValue = nextValue;
+                int currentValue;
+                esm.getT(currentValue);
+                rawConnections.push_back(currentValue);
             }
-//            for (int i = 0; i < edgeCount; ++i)
-//            {
-//                std::cout << i << "'s edge: " << edges[i].v0 << " "
-//                << edges[i].v1 << std::endl;
-//            }
+
+            std::vector<int>::const_iterator rawIt = rawConnections.begin();
+            int pointIndex = 0;
+            edges.reserve(edgeCount);
+            for(PointList::const_iterator it = points.begin(); it != points.end(); it++, pointIndex++)
+            {
+                unsigned char connectionNum = (*it).connectionNum;
+                for (int i = 0; i < connectionNum; ++i) {
+                    Edge edge;
+                    edge.v0 = pointIndex;
+                    edge.v1 = *rawIt;
+                    rawIt++;
+                    edges.push_back(edge);
+                }
+            }
         }
     }
 }
