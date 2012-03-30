c160bc708091039cccdea53bc584704713193852
Sebastian Wick <wick.sebastian@gmail.com>
2012-03-30 22:58:54 +0200

    works, sort of

--- apps/mwiniimporter/importer.hpp
+++ apps/mwiniimporter/importer.hpp
@@ -2,14 +2,28 @@
 #define MWINIIMPORTER_IMPORTER 1
 
 #include <string>
+#include <map>
+#include <exception>
+
+typedef std::map<std::string, std::string> strmap;
+
+class IniParseException : public std::exception {
+    virtual const char* what() const throw() {
+        return "unexpected end of line";
+    }
+};
 
 class MwIniImporter {
 
   public:
-    void test();
+    void    setVerbose(bool verbose);
+    strmap  loadIniFile(std::string filename);
+    strmap  loadCfgFile(std::string filename);
+    void    merge(strmap &cfg, strmap &ini);
+    void    writeToFile(std::string file, strmap &cfg);
     
   private:
-    
+    bool mVerbose;
 
 };
 
