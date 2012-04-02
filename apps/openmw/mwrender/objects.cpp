e5cc9adbb02938809938f3ea8a0fe222372d82af
scrawl <scrawl@baseoftrash.de>
2012-04-02 15:30:29 +0200

    added settings for rendering distance of small objects

--- apps/openmw/mwrender/objects.cpp
+++ apps/openmw/mwrender/objects.cpp
@@ -89,18 +89,16 @@ void Objects::insertMesh (const MWWorld::Ptr& ptr, const std::string& mesh)
     NifOgre::NIFLoader::load(mesh);
     Ogre::Entity *ent = mRenderer.getScene()->createEntity(mesh);
 
-/*
+
     Ogre::Vector3 extents = ent->getBoundingBox().getSize();
     extents *= insert->getScale();
-//    float size = std::max(std::max(extents.x, extents.y), extents.z);
+    float size = std::max(std::max(extents.x, extents.y), extents.z);
 
-    bool small = (size < 250); /// \todo config value
+    bool small = (size < Settings::Manager::getInt("small object size", "Objects")) && Settings::Manager::getBool("limit small object distance", "Objects");
 
     // do not fade out doors. that will cause holes and look stupid
     if (ptr.getTypeName().find("Door") != std::string::npos)
         small = false;
-*/
-    const bool small = false;
 
     if (mBounds.find(ptr.getCell()) == mBounds.end())
         mBounds[ptr.getCell()] = Ogre::AxisAlignedBox::BOX_NULL;
@@ -118,7 +116,7 @@ void Objects::insertMesh (const MWWorld::Ptr& ptr, const std::string& mesh)
     {
         insert->attachObject(ent);
 
-        ent->setRenderingDistance(small ? 2500 : 0); /// \todo config value
+        ent->setRenderingDistance(small ? Settings::Manager::getInt("small object distance", "Objects") : 0); /// \todo config value
     }
     else
     {
@@ -132,7 +130,7 @@ void Objects::insertMesh (const MWWorld::Ptr& ptr, const std::string& mesh)
                 sg = mRenderer.getScene()->createStaticGeometry( "sg" + Ogre::StringConverter::toString(uniqueID));
                 mStaticGeometrySmall[ptr.getCell()] = sg;
 
-                sg->setRenderingDistance(2500); /// \todo config value
+                sg->setRenderingDistance(Settings::Manager::getInt("small object distance", "Objects")); /// \todo config value
             }
             else
                 sg = mStaticGeometrySmall[ptr.getCell()];
