a4d6d1bafff74e1e08aeb07792df92f312dee5a8
scrawl <scrawl@baseoftrash.de>
2012-05-04 18:09:45 +0200

    added a utility for creating texture atlas at runtime

--- apps/openmw/mwgui/cursorreplace.cpp
+++ apps/openmw/mwgui/cursorreplace.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/filesystem.hpp>
 #include <openengine/ogre/imagerotate.hpp>
+#include <openengine/ogre/atlas.hpp>
 
 #include <OgreResourceGroupManager.h>
 #include <OgreRoot.h>
@@ -13,4 +14,6 @@ CursorReplace::CursorReplace()
     OEngine::Render::ImageRotate::rotate("textures\\tx_cursormove.dds", "mwpointer_vresize.png", 90);
     OEngine::Render::ImageRotate::rotate("textures\\tx_cursormove.dds", "mwpointer_dresize1.png", -45);
     OEngine::Render::ImageRotate::rotate("textures\\tx_cursormove.dds", "mwpointer_dresize2.png", 45);
+
+    OEngine::Render::Atlas::createFromFile("atlas1.cfg", "mwgui1", "textures\\");
 }
