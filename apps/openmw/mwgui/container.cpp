6a0cb32b7ee781857cec0fcc4676ce3607bfb339
scrawl <scrawl@baseoftrash.de>
2012-05-16 14:30:02 +0200

    removed unnecessary code

--- apps/openmw/mwgui/container.cpp
+++ apps/openmw/mwgui/container.cpp
@@ -424,19 +424,16 @@ void ContainerWindow::onTakeAllButtonClicked(MyGUI::Widget* _sender)
         int i=0;
         for (MWWorld::ContainerStoreIterator iter (containerStore.begin()); iter!=containerStore.end(); ++iter)
         {
-            if(iter->getRefData().getCount() > 0)
+            playerStore.add(*iter);
+
+            if (i==0)
             {
-                playerStore.add(*iter);
-
-                if (i==0)
-                {
-                    // play the sound of the first object
-                    std::string sound = MWWorld::Class::get(*iter).getUpSoundId(*iter);
-                    MWBase::Environment::get().getSoundManager()->playSound (sound, 1.0, 1.0);
-                }
-
-                ++i;
+                // play the sound of the first object
+                std::string sound = MWWorld::Class::get(*iter).getUpSoundId(*iter);
+                MWBase::Environment::get().getSoundManager()->playSound (sound, 1.0, 1.0);
             }
+
+            ++i;
         }
 
         containerStore.clear();
