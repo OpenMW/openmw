c4c8288af8d51f01f22df3813a6b01b8f0e76a14
Jason Hooks <jhooks1@mix.wvu.edu>
2012-06-13 19:34:13 -0400

    Seven digit scales

--- libs/openengine/bullet/physic.cpp
+++ libs/openengine/bullet/physic.cpp
@@ -324,10 +324,11 @@ namespace Physic
 
     RigidBody* PhysicEngine::createRigidBody(std::string mesh,std::string name,float scale)
     {
-        char uniqueID[4];
-        sprintf( uniqueID, "%1.2f", scale );
+        char uniqueID[8];
+        sprintf( uniqueID, "%07.3f", scale );
         std::string sid = uniqueID;
-        std::string outputstring = mesh + sid + ">|";
+        std::string outputstring = mesh + uniqueID + "\"|";
+        //std::cout << "The string" << outputstring << "\n";
 
         //get the shape from the .nif
         mShapeLoader->load(outputstring,"General");
