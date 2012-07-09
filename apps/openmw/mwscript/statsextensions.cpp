67c1c5ce1811271a15d93b8e0be906432cfe8a06
Marc Zinnschlag <marc@zpages.de>
2012-07-09 20:42:45 +0200

    Issue #324: adjusted setSkill script instructions according to the recent research

--- apps/openmw/mwscript/statsextensions.cpp
+++ apps/openmw/mwscript/statsextensions.cpp
@@ -1,7 +1,11 @@
 
 #include "statsextensions.hpp"
 
+#include <cmath>
+
 #include <boost/algorithm/string.hpp>
+
+#include <components/esm/loadnpc.hpp>
 
 #include <components/esm_store/store.hpp>
 
@@ -260,8 +264,30 @@ namespace MWScript
                     Interpreter::Type_Integer value = runtime[0].mInteger;
                     runtime.pop();
 
-                    MWWorld::Class::get (ptr).getNpcStats (ptr).getSkill (mIndex).
-                        setModified (value, 0);
+                    MWMechanics::NpcStats& stats = MWWorld::Class::get (ptr).getNpcStats (ptr);
+
+                    MWWorld::LiveCellRef<ESM::NPC> *ref = ptr.get<ESM::NPC>();
+
+                    assert (ref);
+
+                    const ESM::Class& class_ =
+                        *MWBase::Environment::get().getWorld()->getStore().classes.find (ref->base->cls);
+
+                    float level = 0;
+                    float progress = std::modf (stats.getSkill (mIndex).getBase(), &level);
+
+                    float modifier = stats.getSkill (mIndex).getModifier();
+
+                    int newLevel = static_cast<int> (value-modifier);
+
+                    progress = (progress / stats.getSkillGain (mIndex, class_, -1, level))
+                        * stats.getSkillGain (mIndex, class_, -1, newLevel);
+
+                    if (progress>=1)
+                        progress = 0.999999999;
+
+                    stats.getSkill (mIndex).set (newLevel + progress);
+                    stats.getSkill (mIndex).setModifier (modifier);
                 }
         };
 
