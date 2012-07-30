2ccecd839bdac85672cd3aa861cfa95f25c27f46
Marc Zinnschlag <marc@zpages.de>
2012-07-30 12:43:23 +0200

    improved the help text for --script-run; updated readme.txt

--- apps/openmw/main.cpp
+++ apps/openmw/main.cpp
@@ -131,7 +131,7 @@ bool parseOptions (int argc, char** argv, OMW::Engine& engine, Files::Configurat
             ->default_value(false), "enable console-only script functionality")
 
         ("script-run", bpo::value<std::string>()->default_value(""),
-            "set a file that is execute in the console on startup\n\n"
+            "select a file that is executed in the console on startup\n\n"
             "Note: The file contains a list of script lines, but not a complete scripts. "
             "That means no begin/end and no variable declarations.")
 
