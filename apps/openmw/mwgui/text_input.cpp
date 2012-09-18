4d4ba6698abdd8e968fd98dc2386b748b19aa6a0
scrawl <scrawl@baseoftrash.de>
2012-09-18 19:00:20 +0200

    fix character preview destruction; don't allow empty names in name dialog

--- apps/openmw/mwgui/text_input.cpp
+++ apps/openmw/mwgui/text_input.cpp
@@ -47,10 +47,22 @@ void TextInputDialog::open()
 
 void TextInputDialog::onOkClicked(MyGUI::Widget* _sender)
 {
-    eventDone(this);
+    if (mTextEdit->getCaption() == "")
+    {
+        mWindowManager.messageBox ("#{sNotifyMessage37}", std::vector<std::string>());
+        MyGUI::InputManager::getInstance ().setKeyFocusWidget (mTextEdit);
+    }
+    else
+        eventDone(this);
 }
 
 void TextInputDialog::onTextAccepted(MyGUI::Edit* _sender)
 {
-    eventDone(this);
+    if (mTextEdit->getCaption() == "")
+    {
+        mWindowManager.messageBox ("#{sNotifyMessage37}", std::vector<std::string>());
+        MyGUI::InputManager::getInstance ().setKeyFocusWidget (mTextEdit);
+    }
+    else
+        eventDone(this);
 }
