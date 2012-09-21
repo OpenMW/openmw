1d012d1680c9d4521550a9859aa77e38809e1a29
scrawl <scrawl@baseoftrash.de>
2012-09-21 15:24:26 +0200

    don't update input during loading. Fixes the OSX crash, and doesn't suffer from the issue anymore that I had while implementing this, so let's accept it as a hotfix for now. Note this means we can't switch the window during loading anymore.

--- apps/openmw/mwgui/loadingscreen.cpp
+++ apps/openmw/mwgui/loadingscreen.cpp
@@ -126,7 +126,7 @@ namespace MWGui
 
             // always update input before rendering something, otherwise mygui goes crazy when something was entered in the frame before
             // (e.g. when using "coc" console command, it would enter an infinite loop and crash due to overflow)
-            MWBase::Environment::get().getInputManager()->update(0, true);
+            //MWBase::Environment::get().getInputManager()->update(0, true);
 
             Ogre::CompositorChain* chain = Ogre::CompositorManager::getSingleton().getCompositorChain(mWindow->getViewport(0));
 
