f907a6a86f17ffa37d0922e61b2f662a79da64b2
greye <greye@null.net>
2012-10-01 11:24:44 +0400

    updateListener() every frame

--- apps/openmw/mwrender/player.cpp
+++ apps/openmw/mwrender/player.cpp
@@ -94,7 +94,6 @@ namespace MWRender
         } else {
             mCameraNode->setOrientation(zr * xr);
         }
-        updateListener();
     }
 
     std::string Player::getHandle() const
@@ -121,6 +120,8 @@ namespace MWRender
 
     void Player::update(float duration)
     {
+        updateListener();
+
         // only show the crosshair in game mode and in first person mode.
         MWBase::Environment::get().getWindowManager ()->showCrosshair
                 (!MWBase::Environment::get().getWindowManager ()->isGuiMode () && (mFirstPersonView && !mVanity.enabled && !mPreviewMode));
