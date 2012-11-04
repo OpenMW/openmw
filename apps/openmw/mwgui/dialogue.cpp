6f7a621b6f4a6913950055411e1f747c8dd042f5
Marc Zinnschlag <marc@zpages.de>
2012-11-04 23:15:20 +0100

    disabled enchanting unfinished enchantment GUI for now

--- apps/openmw/mwgui/dialogue.cpp
+++ apps/openmw/mwgui/dialogue.cpp
@@ -192,8 +192,8 @@ void DialogueWindow::setKeywords(std::list<std::string> keyWords)
     if (mServices & Service_CreateSpells)
         mTopicsList->addItem(MWBase::Environment::get().getWorld()->getStore().gameSettings.find("sSpellmakingMenuTitle")->getString());
 
-    if (mServices & Service_Enchant)
-        mTopicsList->addItem(MWBase::Environment::get().getWorld()->getStore().gameSettings.find("sEnchanting")->getString());
+//    if (mServices & Service_Enchant)
+//        mTopicsList->addItem(MWBase::Environment::get().getWorld()->getStore().gameSettings.find("sEnchanting")->getString());
 
     if (mServices & Service_Training)
         mTopicsList->addItem(MWBase::Environment::get().getWorld()->getStore().gameSettings.find("sServiceTrainingTitle")->getString());
