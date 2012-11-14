e6c8e1f0d7e2149de4d980d617e504971aaa29ae
marcin <Gohan1989@gmail.com>
2012-11-14 18:42:04 +0100

    Feature #391 Dummy AI package classes

---
+++ apps/openmw/mwmechanics/aiescort.cpp
@@ -0,0 +1,56 @@
+#include "aiescort.hpp"
+
+
+MWMechanics::AiEscort::AiEscort(std::string ActorID,int Duration, float X, float Y, float Z, bool Reset)
+{
+    mActorID = ActorID;
+    mDuration = Duration;
+    mX = X;
+    mY = Y;
+    mZ = Z;
+    mReset = Reset;
+
+}
+MWMechanics::AiEscort *MWMechanics::AiEscort::clone() const
+{
+    AiEscort * temp = new AiEscort(*this);
+    return temp;
+}
+
+bool MWMechanics::AiEscort::execute (const MWWorld::Ptr& actor)
+{
+    std::cout << "AiEscort complted. \n";
+    return true;
+}
+
+int MWMechanics::AiEscort::getTypeId() const
+{
+    return 2;
+}
+
+float MWMechanics::AiEscort::getX()
+{
+    return mX;
+}
+float MWMechanics::AiEscort::getY()
+{
+    return mY;
+}
+float MWMechanics::AiEscort::getZ()
+{
+    return mZ;
+}
+bool MWMechanics::AiEscort::getReset()
+{
+    return mReset;
+}
+
+std::string MWMechanics::AiEscort::getActorID()
+{
+    return mActorID;
+}
+
+int MWMechanics::AiEscort::getDuration()
+{
+    return mDuration;
+}
---
+++ apps/openmw/mwmechanics/aiescort.hpp
@@ -0,0 +1,41 @@
+#ifndef AIESCORT_H
+#define AIESCORT_H
+
+#include "aipackage.hpp"
+#include <iostream>
+#include <vector>
+
+namespace MWWorld
+{
+class Ptr;
+}
+
+namespace MWMechanics
+{
+class AiEscort : public AiPackage
+{
+    public:
+     AiEscort(std::string ActorID,int Duration, float X, float Y, float Z, bool Reset = false);
+    virtual AiEscort *clone() const;
+
+    virtual bool execute (const MWWorld::Ptr& actor);
+            ///< \return Package completed?
+
+    virtual int getTypeId() const;
+    float getX();
+    float getY();
+    float getZ();
+    bool getReset();
+    std::string getActorID();
+    int getDuration();
+
+    private:
+    std::string mActorID;
+    float mX;
+    float mY;
+    float mZ;
+    int mDuration;
+    bool mReset;
+};
+}
+#endif // AIESCORT_H
