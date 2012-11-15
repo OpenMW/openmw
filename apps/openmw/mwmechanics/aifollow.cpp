99ddc63e2c4d165a8b9f9f8d3b47d4c585bf3b94
marcin <Gohan1989@gmail.com>
2012-11-15 22:32:15 +0100

    Feature #391 Dummy AI package classes

---
+++ apps/openmw/mwmechanics/aifollow.cpp
@@ -0,0 +1,22 @@
+#include "aifollow.hpp"
+#include <iostream>
+
+MWMechanics::AiFollow::AiFollow(const std::string &ActorID,float duration, float X, float Y, float Z):
+mActorID(ActorID), mDuration(duration), mX(X), mY(Y), mZ(Z)
+{
+}
+MWMechanics::AiFollow *MWMechanics::AiFollow::clone() const
+{
+    return new AiFollow(*this);
+}
+
+ bool MWMechanics::AiFollow::execute (const MWWorld::Ptr& actor)
+{
+    std::cout << "AiFollow completed.\n";
+    return true;
+}
+
+ int MWMechanics::AiFollow::getTypeId() const
+{
+    return 3;
+}
---
+++ apps/openmw/mwmechanics/aifollow.hpp
@@ -0,0 +1,27 @@
+#ifndef GAME_MWMECHANICS_AIFALLOW_H
+#define GAME_MWMECHANICS_AIFALLOW_H
+
+#include "aipackage.hpp"
+#include <string>
+
+namespace MWMechanics
+{
+
+class AiFollow : AiPackage
+{
+    public:
+    AiFollow(const std::string &ActorID,float duration, float X, float Y, float Z);
+    virtual AiFollow *clone() const;
+    virtual bool execute (const MWWorld::Ptr& actor);
+            ///< \return Package completed?
+    virtual int getTypeId() const;
+
+    private:
+    float mDuration;
+    float mX;
+    float mY;
+    float mZ;
+    std::string mActorID;
+};
+}
+#endif
