515419ae0beb762b68822cfb572ad840e7899e3a
marcin <Gohan1989@gmail.com>
2012-11-15 22:22:44 +0100

    Feature #391 Dummy AI package classes

---
+++ apps/openmw/mwmechanics/aiactivate.cpp
@@ -0,0 +1,21 @@
+#include "aiactivate.hpp"
+#include <iostream>
+
+MWMechanics::AiActivate::AiActivate(const MWWorld::Ptr& object)
+{
+    mObject = &object;
+}
+MWMechanics::AiActivate *MWMechanics::AiActivate::clone() const
+{
+    return new AiActivate(*this);
+}
+bool MWMechanics::AiActivate::execute (const MWWorld::Ptr& actor)
+{
+    std::cout << "AiActivate completed.\n";
+    return true;
+}
+
+int MWMechanics::AiActivate::getTypeId() const
+{
+    return 4;
+}
---
+++ apps/openmw/mwmechanics/aiactivate.hpp
@@ -0,0 +1,23 @@
+#ifndef GAME_MWMECHANICS_AIACTIVATE_H
+#define GAME_MWMECHANICS_AIACTIVATE_H
+
+#include "aipackage.hpp"
+
+
+namespace MWMechanics
+{
+
+class AiActivate : AiPackage
+{
+    public:
+    AiActivate(const MWWorld::Ptr& object);
+    virtual AiActivate *clone() const;
+    virtual bool execute (const MWWorld::Ptr& actor);
+            ///< \return Package completed?
+    virtual int getTypeId() const;
+
+    private:
+    const MWWorld::Ptr * mObject;
+};
+}
+#endif // GAME_MWMECHANICS_AIACTIVATE_H
