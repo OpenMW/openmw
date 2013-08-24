7e02c9acf229daad0a17f03c113dfda1c1b479ab
Marc Zinnschlag <marc@zpages.de>
2013-08-24 18:53:11 +0200

    added filter text column to filter table

--- apps/opencs/model/world/data.cpp
+++ apps/opencs/model/world/data.cpp
@@ -150,6 +150,7 @@ CSMWorld::Data::Data() : mRefs (mCells)
 
     mFilters.addColumn (new StringIdColumn<CSMFilter::Filter>);
     mFilters.addColumn (new RecordStateColumn<CSMFilter::Filter>);
+    mFilters.addColumn (new FilterColumn<CSMFilter::Filter>);
     mFilters.addColumn (new DescriptionColumn<CSMFilter::Filter>);
 
     addModel (new IdTable (&mGlobals), UniversalId::Type_Globals, UniversalId::Type_Global);
