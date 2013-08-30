515a865daafae550756a36f4787f79c65deb015d
Lukasz Gromanowski <lgromanowski@gmail.com>
2013-08-30 22:44:03 +0200

    Changed ASSERT_EQ to ASSERT_TRUE/FALSE.
    Signed-off-by: Lukasz Gromanowski <lgromanowski@gmail.com>

--- apps/openmw_test_suite/components/misc/test_stringops.cpp
+++ apps/openmw_test_suite/components/misc/test_stringops.cpp
@@ -15,65 +15,65 @@ struct StringOpsTest : public ::testing::Test
 
 TEST_F(StringOpsTest, begins_matching)
 {
-  ASSERT_EQ(true, Misc::begins("abc", "a"));
-  ASSERT_EQ(true, Misc::begins("abc", "ab"));
-  ASSERT_EQ(true, Misc::begins("abc", "abc"));
-  ASSERT_EQ(true, Misc::begins("abcd", "abc"));
+  ASSERT_TRUE(Misc::begins("abc", "a"));
+  ASSERT_TRUE(Misc::begins("abc", "ab"));
+  ASSERT_TRUE(Misc::begins("abc", "abc"));
+  ASSERT_TRUE(Misc::begins("abcd", "abc"));
 }
 
 TEST_F(StringOpsTest, begins_not_matching)
 {
-  ASSERT_EQ(false, Misc::begins("abc", "b"));
-  ASSERT_EQ(false, Misc::begins("abc", "bc"));
-  ASSERT_EQ(false, Misc::begins("abc", "bcd"));
-  ASSERT_EQ(false, Misc::begins("abc", "abcd"));
+  ASSERT_FALSE(Misc::begins("abc", "b"));
+  ASSERT_FALSE(Misc::begins("abc", "bc"));
+  ASSERT_FALSE(Misc::begins("abc", "bcd"));
+  ASSERT_FALSE(Misc::begins("abc", "abcd"));
 }
 
 TEST_F(StringOpsTest, ibegins_matching)
 {
-  ASSERT_EQ(true, Misc::ibegins("Abc", "a"));
-  ASSERT_EQ(true, Misc::ibegins("aBc", "ab"));
-  ASSERT_EQ(true, Misc::ibegins("abC", "abc"));
-  ASSERT_EQ(true, Misc::ibegins("abcD", "abc"));
+  ASSERT_TRUE(Misc::ibegins("Abc", "a"));
+  ASSERT_TRUE(Misc::ibegins("aBc", "ab"));
+  ASSERT_TRUE(Misc::ibegins("abC", "abc"));
+  ASSERT_TRUE(Misc::ibegins("abcD", "abc"));
 }
 
 TEST_F(StringOpsTest, ibegins_not_matching)
 {
-  ASSERT_EQ(false, Misc::ibegins("abc", "b"));
-  ASSERT_EQ(false, Misc::ibegins("abc", "bc"));
-  ASSERT_EQ(false, Misc::ibegins("abc", "bcd"));
-  ASSERT_EQ(false, Misc::ibegins("abc", "abcd"));
+  ASSERT_FALSE(Misc::ibegins("abc", "b"));
+  ASSERT_FALSE(Misc::ibegins("abc", "bc"));
+  ASSERT_FALSE(Misc::ibegins("abc", "bcd"));
+  ASSERT_FALSE(Misc::ibegins("abc", "abcd"));
 }
 
 TEST_F(StringOpsTest, ends_matching)
 {
-  ASSERT_EQ(true, Misc::ends("abc", "c"));
-  ASSERT_EQ(true, Misc::ends("abc", "bc"));
-  ASSERT_EQ(true, Misc::ends("abc", "abc"));
-  ASSERT_EQ(true, Misc::ends("abcd", "abcd"));
+  ASSERT_TRUE(Misc::ends("abc", "c"));
+  ASSERT_TRUE(Misc::ends("abc", "bc"));
+  ASSERT_TRUE(Misc::ends("abc", "abc"));
+  ASSERT_TRUE(Misc::ends("abcd", "abcd"));
 }
 
 TEST_F(StringOpsTest, ends_not_matching)
 {
-  ASSERT_EQ(false, Misc::ends("abc", "b"));
-  ASSERT_EQ(false, Misc::ends("abc", "ab"));
-  ASSERT_EQ(false, Misc::ends("abc", "bcd"));
-  ASSERT_EQ(false, Misc::ends("abc", "abcd"));
+  ASSERT_FALSE(Misc::ends("abc", "b"));
+  ASSERT_FALSE(Misc::ends("abc", "ab"));
+  ASSERT_FALSE(Misc::ends("abc", "bcd"));
+  ASSERT_FALSE(Misc::ends("abc", "abcd"));
 }
 
 TEST_F(StringOpsTest, iends_matching)
 {
-  ASSERT_EQ(true, Misc::iends("Abc", "c"));
-  ASSERT_EQ(true, Misc::iends("aBc", "bc"));
-  ASSERT_EQ(true, Misc::iends("abC", "abc"));
-  ASSERT_EQ(true, Misc::iends("abcD", "abcd"));
+  ASSERT_TRUE(Misc::iends("Abc", "c"));
+  ASSERT_TRUE(Misc::iends("aBc", "bc"));
+  ASSERT_TRUE(Misc::iends("abC", "abc"));
+  ASSERT_TRUE(Misc::iends("abcD", "abcd"));
 }
 
 TEST_F(StringOpsTest, iends_not_matching)
 {
-  ASSERT_EQ(false, Misc::iends("abc", "b"));
-  ASSERT_EQ(false, Misc::iends("abc", "ab"));
-  ASSERT_EQ(false, Misc::iends("abc", "bcd"));
-  ASSERT_EQ(false, Misc::iends("abc", "abcd"));
+  ASSERT_FALSE(Misc::iends("abc", "b"));
+  ASSERT_FALSE(Misc::iends("abc", "ab"));
+  ASSERT_FALSE(Misc::iends("abc", "bcd"));
+  ASSERT_FALSE(Misc::iends("abc", "abcd"));
 }
 
