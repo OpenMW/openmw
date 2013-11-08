0745a86039596ce29289c6fe88236df50c370ea2
Marc Zinnschlag <marc@zpages.de>
2013-11-08 11:51:59 +0100

    added InfoCreator

---
+++ apps/opencs/view/world/infocreator.hpp
@@ -0,0 +1,42 @@
+#ifndef CSV_WORLD_INFOCREATOR_H
+#define CSV_WORLD_INFOCREATOR_H
+
+#include "genericcreator.hpp"
+
+class QLineEdit;
+
+namespace CSMWorld
+{
+    class InfoCollection;
+}
+
+namespace CSVWorld
+{
+    class InfoCreator : public GenericCreator
+    {
+            Q_OBJECT
+
+            QLineEdit *mTopic;
+
+            virtual std::string getId() const;
+
+            virtual void configureCreateCommand (CSMWorld::CreateCommand& command) const;
+
+        public:
+
+            InfoCreator (CSMWorld::Data& data, QUndoStack& undoStack,
+                const CSMWorld::UniversalId& id);
+
+            virtual void reset();
+
+            virtual std::string getErrors() const;
+            ///< Return formatted error descriptions for the current state of the creator. if an empty
+            /// string is returned, there is no error.
+
+        private slots:
+
+            void topicChanged();
+    };
+}
+
+#endif
