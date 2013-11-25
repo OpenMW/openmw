58dce88c7d1a93d3bb784dc479eca845c7f1bf31
scrawl <scrawl@baseoftrash.de>
2013-11-25 14:03:51 +0100

    Handle the "tai" alias for ToggleAI

--- components/compiler/extensions0.cpp
+++ components/compiler/extensions0.cpp
@@ -60,6 +60,7 @@ namespace Compiler
             extensions.registerInstruction ("modflee", "l", opcodeModFlee, opcodeModFleeExplicit);
             extensions.registerInstruction ("modalarm", "l", opcodeModAlarm, opcodeModAlarmExplicit);
             extensions.registerInstruction ("toggleai", "", opcodeToggleAI, opcodeToggleAI);
+            extensions.registerInstruction ("tai", "", opcodeToggleAI, opcodeToggleAI);
             extensions.registerFunction ("gethello", 'l', "", opcodeGetHello, opcodeGetHelloExplicit);
             extensions.registerFunction ("getfight", 'l', "", opcodeGetFight, opcodeGetFightExplicit);
             extensions.registerFunction ("getflee", 'l', "", opcodeGetFlee, opcodeGetFleeExplicit);
