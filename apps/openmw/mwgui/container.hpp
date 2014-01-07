780bf5a2cdcc0bdb642901685165f6882c4bbec8
scrawl <scrawl@baseoftrash.de>
2014-01-07 03:01:33 +0100

    Implement pickpocket detection. Play a voiced dialogue entry when detected.

--- apps/openmw/mwgui/container.hpp
+++ apps/openmw/mwgui/container.hpp
@@ -52,9 +52,12 @@ namespace MWGui
         ContainerWindow(DragAndDrop* dragAndDrop);
 
         void open(const MWWorld::Ptr& container, bool loot=false);
+        virtual void close();
 
     private:
         DragAndDrop* mDragAndDrop;
+
+        bool mPickpocketDetected;
 
         MWGui::ItemView* mItemView;
         SortFilterItemModel* mSortModel;
@@ -72,6 +75,9 @@ namespace MWGui
         void onCloseButtonClicked(MyGUI::Widget* _sender);
         void onTakeAllButtonClicked(MyGUI::Widget* _sender);
         void onDisposeCorpseButtonClicked(MyGUI::Widget* sender);
+
+        /// @return is taking the item allowed?
+        bool onTakeItem(const ItemStack& item, int count);
 
         virtual void onReferenceUnavailable();
     };
