a36cc264349584a1c0f6abfa1669d109843aadb3
Marek Kochanowicz <sirherrbatka@gmail.com>
2014-02-01 19:06:41 +0100

    Created new files for TableMimeData class.

---
+++ apps/opencs/model/world/tablemimedata.cpp
@@ -0,0 +1,1 @@
+#include "tablemimedata.hpp"
---
+++ apps/opencs/model/world/tablemimedata.hpp
@@ -0,0 +1,10 @@
+#ifndef TABLEMIMEDATA_H
+#define TABLEMIMEDATA_H
+
+#include <qt4/QtCore/QMimeData>
+
+class TableMimeData : public QMimeData
+{
+};
+
+#endif // TABLEMIMEDATA_H
