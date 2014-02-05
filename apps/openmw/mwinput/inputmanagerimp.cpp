4a26909172d0584acd344590410b6d1ebac05f7f
scrawl <scrawl@baseoftrash.de>
2014-02-05 12:15:07 +0100

    Fixes #1159: Don't allow quick keys menu in chargen

--- apps/openmw/mwinput/inputmanagerimp.cpp
+++ apps/openmw/mwinput/inputmanagerimp.cpp
@@ -748,7 +748,8 @@ namespace MWInput
 
     void InputManager::showQuickKeysMenu()
     {
-        if (!MWBase::Environment::get().getWindowManager()->isGuiMode ())
+        if (!MWBase::Environment::get().getWindowManager()->isGuiMode ()
+                && MWBase::Environment::get().getWorld()->getGlobalFloat ("chargenstate")==-1)
             MWBase::Environment::get().getWindowManager()->pushGuiMode (MWGui::GM_QuickKeysMenu);
         else if (MWBase::Environment::get().getWindowManager()->getMode () == MWGui::GM_QuickKeysMenu)
             MWBase::Environment::get().getWindowManager()->removeGuiMode (MWGui::GM_QuickKeysMenu);
