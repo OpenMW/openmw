0313876d88f9eb55c8ed43a9b79787ec17866b05
Marc Zinnschlag <marc@zpages.de>
2014-02-13 08:49:40 +0100

    allow leaving out if in a top-level if-statement

--- components/compiler/scriptparser.cpp
+++ components/compiler/scriptparser.cpp
@@ -71,6 +71,12 @@ namespace Compiler
         if (code==Scanner::S_newline) // empty line
             return true;
 
+        if (code==Scanner::S_open) /// \todo Option to switch this off
+        {
+            scanner.putbackSpecial (code, loc);
+            return parseKeyword (Scanner::K_if, loc, scanner);
+        }
+
         mLineParser.reset();
         if (mLineParser.parseSpecial (code, loc, scanner))
             scanner.scan (mLineParser);
