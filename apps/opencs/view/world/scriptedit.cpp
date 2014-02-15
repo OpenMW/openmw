930b77e60a1d7afb00a6a5766e009d0e49d28e56
Marek Kochanowicz <sirherrbatka@gmail.com>
2014-02-15 19:52:40 +0100

    allow drag and drop into the script edit window.

--- apps/opencs/view/world/scriptedit.cpp
+++ apps/opencs/view/world/scriptedit.cpp
@@ -1,26 +1,17 @@
-/*
- * <one line to give the library's name and an idea of what it does.>
- * Copyright 2014  Marek Kochanowicz <sirherrbatka@gmail.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 #include "scriptedit.hpp"
+
+#include <vector>
+
+#include <QDragEnterEvent>
+
+#include "../../model/world/universalid.hpp"
+#include "../../model/world/tablemimedata.hpp"
+
+CSVWorld::ScriptEdit::ScriptEdit (QWidget* parent) :
+QTextEdit(parent)
+{
+
+}
 
 void CSVWorld::ScriptEdit::dragEnterEvent (QDragEnterEvent* event)
 {
@@ -34,6 +25,13 @@ void CSVWorld::ScriptEdit::dragMoveEvent (QDragMoveEvent* event)
 
 void CSVWorld::ScriptEdit::dropEvent (QDropEvent* event)
 {
+    const CSMWorld::TableMimeData* mime = dynamic_cast<const CSMWorld::TableMimeData*> (event->mimeData());
 
+    std::vector<CSMWorld::UniversalId> records (mime->getData());
+
+    for (std::vector<CSMWorld::UniversalId>::iterator it = records.begin(); it != records.end(); ++it)
+    {
+        insertPlainText (QString::fromStdString (it->getId()));
+    }
 }
 // kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
