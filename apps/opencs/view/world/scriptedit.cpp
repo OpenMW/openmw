97fe5465bf4bf6ec2c72a622eb5c226f1b7a547d
Marek Kochanowicz <sirherrbatka@gmail.com>
2014-02-15 17:55:18 +0100

    created new files

---
+++ apps/opencs/view/world/scriptedit.cpp
@@ -0,0 +1,39 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright 2014  Marek Kochanowicz <sirherrbatka@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "scriptedit.hpp"
+
+void CSVWorld::ScriptEdit::dragEnterEvent (QDragEnterEvent* event)
+{
+    event->acceptProposedAction();
+}
+
+void CSVWorld::ScriptEdit::dragMoveEvent (QDragMoveEvent* event)
+{
+    event->accept();
+}
+
+void CSVWorld::ScriptEdit::dropEvent (QDropEvent* event)
+{
+
+}
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
