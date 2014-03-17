128a47570e07e7d862f3f59bb66f8153e34481e0
scrawl <scrawl@baseoftrash.de>
2014-03-17 14:15:52 +0100

    One more fix

--- apps/openmw/mwgui/journalbooks.hpp
+++ apps/openmw/mwgui/journalbooks.hpp
@@ -6,14 +6,7 @@
 
 namespace MWGui
 {
-    MWGui::BookTypesetter::Utf8Span to_utf8_span (char const * text)
-    {
-        typedef MWGui::BookTypesetter::Utf8Point point;
-
-        point begin = reinterpret_cast <point> (text);
-
-        return MWGui::BookTypesetter::Utf8Span (begin, begin + strlen (text));
-    }
+    MWGui::BookTypesetter::Utf8Span to_utf8_span (char const * text);
 
     struct JournalBooks
     {
