7697b9e868b13f611d67ef5355d797f106735ad8
Thomas <tluppi@thomas-GE60.(none)>
2014-05-26 01:48:24 -0400

    Ensures nothing bad will happen if we try to lock a door that never had a lock level

--- apps/openmw/mwclass/door.cpp
+++ apps/openmw/mwclass/door.cpp
@@ -192,8 +192,12 @@ namespace MWClass
     {
         if(lockLevel!=0)
             ptr.getCellRef().setLockLevel(abs(lockLevel)); //Changes lock to locklevel, in positive
-        else
-            ptr.getCellRef().setLockLevel(abs(ptr.getCellRef().getLockLevel())); //No locklevel given, just flip the origional one
+        else {
+            if(abs(ptr.getCellRef().getLockLevel())!=0)
+                ptr.getCellRef().setLockLevel(abs(ptr.getCellRef().getLockLevel())); //No locklevel given, just flip the origional one
+            else
+                ptr.getCellRef().setLockLevel(100);
+        }
     }
 
     void Door::unlock (const MWWorld::Ptr& ptr) const
