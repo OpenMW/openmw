01283f531eeb71cbef1a01b7e1a0da41a895520e
Hallfaer Tuilinn <gijsbertth@gmail.com>
2014-06-06 15:13:25 +0200

    Fix for Bug #1409

--- apps/openmw/mwmechanics/actors.cpp
+++ apps/openmw/mwmechanics/actors.cpp
@@ -1088,10 +1088,10 @@ namespace MWMechanics
 
         CreatureStats& stats = ptr.getClass().getCreatureStats(ptr);
 
-        float healthHours  = healthPerHour >= 0
+        float healthHours  = healthPerHour > 0
                              ? (stats.getHealth().getModified() - stats.getHealth().getCurrent()) / healthPerHour
                              : 1.0f;
-        float magickaHours = magickaPerHour >= 0
+        float magickaHours = magickaPerHour > 0
                               ? (stats.getMagicka().getModified() - stats.getMagicka().getCurrent()) / magickaPerHour
                               : 1.0f;
 
