0aba1088a1ff249baa85d57d84f8a5c46f84c8cc
scrawl <scrawl@baseoftrash.de>
2014-06-16 00:51:58 +0200

    Attempt to open companion window if no greetings are found (Bug #1507)

--- apps/openmw/mwdialogue/dialoguemanagerimp.cpp
+++ apps/openmw/mwdialogue/dialoguemanagerimp.cpp
@@ -182,9 +182,19 @@ namespace MWDialogue
                     win->addResponse (Interpreter::fixDefinesDialog(info->mResponse, interpreterContext));
                     executeScript (info->mResultScript);
                     mLastTopic = Misc::StringUtils::lowerCase(it->mId);
-                    break;
+                    return;
                 }
             }
+        }
+
+        // No greetings found. The dialogue window should not be shown.
+        // If this is a companion, we must show the companion window directly (used by BM_bear_be_unique).
+        bool isCompanion = !mActor.getClass().getScript(mActor).empty()
+                && mActor.getRefData().getLocals().getIntVar(mActor.getClass().getScript(mActor), "companion");
+        if (isCompanion)
+        {
+            MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Companion);
+            MWBase::Environment::get().getWindowManager()->showCompanionWindow(mActor);
         }
     }
 
