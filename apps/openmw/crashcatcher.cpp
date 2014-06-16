47bd170d7eab68fea72b219e00b5636492f940d6
scrawl <scrawl@baseoftrash.de>
2014-06-16 04:13:36 +0200

    Crashcatcher: create temp file in /tmp, not working directory (which may not have write access)

--- apps/openmw/crashcatcher.cpp
+++ apps/openmw/crashcatcher.cpp
@@ -128,7 +128,7 @@ static void gdb_info(pid_t pid)
     int fd;
 
     /* Create a temp file to put gdb commands into */
-    strcpy(respfile, "gdb-respfile-XXXXXX");
+    strcpy(respfile, "/tmp/gdb-respfile-XXXXXX");
     if((fd=mkstemp(respfile)) >= 0 && (f=fdopen(fd, "w")) != NULL)
     {
         fprintf(f, "attach %d\n"
