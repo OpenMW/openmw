b259c5def26cc371b13fdb048591f3a257981f59
scrawl <scrawl@baseoftrash.de>
2014-06-25 01:12:57 +0200

    Make GetFactionReaction garbage argument optional

--- components/compiler/extensions0.cpp
+++ components/compiler/extensions0.cpp
@@ -180,7 +180,7 @@ namespace Compiler
             extensions.registerFunction("samefaction", 'l', "", opcodeSameFaction,
                 opcodeSameFactionExplicit);
             extensions.registerInstruction("modfactionreaction", "ccl", opcodeModFactionReaction);
-            extensions.registerFunction("getfactionreaction", 'l', "ccl", opcodeGetFactionReaction);
+            extensions.registerFunction("getfactionreaction", 'l', "ccX", opcodeGetFactionReaction);
             extensions.registerInstruction("clearinfoactor", "", opcodeClearInfoActor, opcodeClearInfoActorExplicit);
         }
     }
