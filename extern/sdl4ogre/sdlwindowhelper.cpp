adcf3b6a3fea415447cd0ef5472e2352bfc3c84f
cc9cii <cc9c@iinet.net.au>
2014-07-05 13:26:50 +1000

    Go back to original formatting for preexisting code.  Also tidy up some comments.

--- extern/sdl4ogre/sdlwindowhelper.cpp
+++ extern/sdl4ogre/sdlwindowhelper.cpp
@@ -20,17 +20,22 @@ bool checkMinGLVersion(std::ostream& error)
 {
 #ifdef WIN32 /* FIXME: Windows only until linux and mac calls to GetDC() are tested */
 
+    // Similar issues with old OpenGL versions reported here:
+    //
+    //   https://github.com/mono/MonoGame/issues/998
+    //   http://www.ogre3d.org/forums/viewtopic.php?f=1&t=79698
+    //
     // If using OpenGL we must ensure version 3.0 or higher for Ogre 1.9.
     //
-    // Root::createRenderWindow() ends up calling GLStateCacheManagerImp::initializeCache()
-    // which uses OpenGL 3.0 core functions glBindFramebuffer() and glBindRenderbuffer().
+    // Root::createRenderWindow() ends up with GLStateCacheManagerImp::initializeCache()
+    // which uses OpenGL 3.0 core functions glBindFramebuffer() & glBindRenderbuffer().
     // If these are not available then a null pointer exception can occur.
     //
     // Ogre 1.9 introduced StateCacheManager/OgreGLNullStateCacheManagerImp.cpp and
     // StateCacheManager/OgreGLStateCacheManagerImp.cpp.  Details from mercurial log:
     //
-    //   Changeset: 4934 (fdc9a9a081f6) Many updates to the GL state cache. Also use
-    //     the same CMake config to enable the state cache regardless of GL render system.
+    //   Changeset: 4934 (fdc9a9a081f6) Many updates to the GL state cache. Also use the
+    //     same CMake config to enable the state cache regardless of GL render system.
     //   Branch:    v1-9
     //   User:      David Rogers <masterfalcon@ogre3d.org>
     //   Date:      2013-09-02 00:06:32 -0500 (9 months)
@@ -41,12 +46,10 @@ bool checkMinGLVersion(std::ostream& error)
     //   http://www.opengl.org/wiki/GLAPI/glBindRenderbuffer
     //   http://www.opengl.org/wiki/GLAPI/glBindFramebuffer
     //
-    // Ogre recently a change to use the extension methods instead (because some earlier
-    // hardware/drivers had them).  https://ogre3d.atlassian.net/browse/OGRE-402
-    //
-    // However, even though equivalent extensions may be available they were deprecated
-    // in v3.0 and removed in v3.1 and therefore not recommended for use
-    // (see: http://www.opengl.org/wiki/Framebuffer_Object). Mercurial log:
+    // Ogre recently made a change to use the extension methods instead (because some
+    // earlier hardware/drivers had them) in OgreGLStateCacheManagerImp. But these
+    // changes were not made to OgreGLNullStateCacheManagerImp (used by default).
+    // See: https://ogre3d.atlassian.net/browse/OGRE-402.  Mercurial log:
     //
     //   Changeset: 6346 (499ae0d1273b) [OGRE-402] Use extension function names to
     //     maintain compatibility with some drivers when using the GL state cache
@@ -54,10 +57,9 @@ bool checkMinGLVersion(std::ostream& error)
     //   User:      David Rogers <masterfalcon@ogre3d.org>
     //   Date:      2014-03-17 23:41:36 -0500 (2 months)
     //
-    // Similar issues with old OpenGL versions reported here:
-    //
-    //   https://github.com/mono/MonoGame/issues/998
-    //   http://www.ogre3d.org/forums/viewtopic.php?f=1&t=79698
+    // Note that even though equivalent extensions may be available they were deprecated
+    // in v3.0 and removed in v3.1 and therefore not recommended for use.
+    // See: http://www.opengl.org/wiki/Framebuffer_Object.
     //
     typedef HGLRC (* WGL_CreateContext_Func) (HDC);
     typedef BOOL (* WGL_MakeCurrent_Func) (HDC, HGLRC);
@@ -199,115 +201,115 @@ namespace SFO
 {
 
 SDLWindowHelper::SDLWindowHelper (SDL_Window* window, int w, int h,
-        const std::string& title, bool fullscreen, Ogre::NameValuePairList params)
-    : mSDLWindow(window)
+		const std::string& title, bool fullscreen, Ogre::NameValuePairList params)
+	: mSDLWindow(window)
 {
-    //get the native whnd
-    struct SDL_SysWMinfo wmInfo;
-    SDL_VERSION(&wmInfo.version);
+	//get the native whnd
+	struct SDL_SysWMinfo wmInfo;
+	SDL_VERSION(&wmInfo.version);
 
-    if (SDL_GetWindowWMInfo(mSDLWindow, &wmInfo) == SDL_FALSE)
-        throw std::runtime_error("Couldn't get WM Info!");
+	if (SDL_GetWindowWMInfo(mSDLWindow, &wmInfo) == SDL_FALSE)
+		throw std::runtime_error("Couldn't get WM Info!");
 
-    Ogre::String winHandle;
+	Ogre::String winHandle;
 
-    switch (wmInfo.subsystem)
-    {
+	switch (wmInfo.subsystem)
+	{
 #ifdef WIN32
-    case SDL_SYSWM_WINDOWS:
-        // Windows code
-        winHandle = Ogre::StringConverter::toString((unsigned long)wmInfo.info.win.window);
-        break;
+	case SDL_SYSWM_WINDOWS:
+		// Windows code
+		winHandle = Ogre::StringConverter::toString((unsigned long)wmInfo.info.win.window);
+		break;
 #elif __MACOSX__
-    case SDL_SYSWM_COCOA:
-        //required to make OGRE play nice with our window
-        params.insert(std::make_pair("macAPI", "cocoa"));
-        params.insert(std::make_pair("macAPICocoaUseNSView", "true"));
+	case SDL_SYSWM_COCOA:
+		//required to make OGRE play nice with our window
+		params.insert(std::make_pair("macAPI", "cocoa"));
+		params.insert(std::make_pair("macAPICocoaUseNSView", "true"));
 
-        winHandle  = Ogre::StringConverter::toString(WindowContentViewHandle(wmInfo));
-        break;
+		winHandle  = Ogre::StringConverter::toString(WindowContentViewHandle(wmInfo));
+		break;
 #else
-    case SDL_SYSWM_X11:
-        winHandle = Ogre::StringConverter::toString((unsigned long)wmInfo.info.x11.window);
-        break;
+	case SDL_SYSWM_X11:
+		winHandle = Ogre::StringConverter::toString((unsigned long)wmInfo.info.x11.window);
+		break;
 #endif
-    default:
-        throw std::runtime_error("Unexpected WM!");
-        break;
-    }
+	default:
+		throw std::runtime_error("Unexpected WM!");
+		break;
+	}
 
-    /// \todo externalWindowHandle is deprecated according to the source code. Figure out a way to get parentWindowHandle
-    /// to work properly. On Linux/X11 it causes an occasional GLXBadDrawable error.
-    params.insert(std::make_pair("externalWindowHandle",  winHandle));
+	/// \todo externalWindowHandle is deprecated according to the source code. Figure out a way to get parentWindowHandle
+	/// to work properly. On Linux/X11 it causes an occasional GLXBadDrawable error.
+	params.insert(std::make_pair("externalWindowHandle",  winHandle));
 
-    Ogre::RenderSystem* rs = Ogre::Root::getSingleton().getRenderSystem();
-    if (rs && (rs->getName() == "OpenGL Rendering Subsystem")
-           && (OGRE_VERSION_MAJOR >= 1) && (OGRE_VERSION_MINOR >= 9))
-    {
-        std::stringstream error;
-        if (!checkMinGLVersion(error))
-            throw std::runtime_error(error.str());
-    }
+	Ogre::RenderSystem* rs = Ogre::Root::getSingleton().getRenderSystem();
+	if (rs && (rs->getName() == "OpenGL Rendering Subsystem")
+		&& (OGRE_VERSION_MAJOR >= 1) && (OGRE_VERSION_MINOR >= 9))
+	{
+		std::stringstream error;
+		if (!checkMinGLVersion(error))
+			throw std::runtime_error(error.str());
+	}
 
-    mWindow = Ogre::Root::getSingleton().createRenderWindow(title, w, h, fullscreen, &params);
+	mWindow = Ogre::Root::getSingleton().createRenderWindow(title, w, h, fullscreen, &params);
 }
 
 void SDLWindowHelper::setWindowIcon(const std::string &name)
 {
-    Ogre::TexturePtr texture = Ogre::TextureManager::getSingleton().load(name, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME);
-    if (texture.isNull())
-    {
-        std::stringstream error;
-        error << "Window icon not found: " << name;
-        throw std::runtime_error(error.str());
-    }
-    Ogre::Image image;
-    texture->convertToImage(image);
+	Ogre::TexturePtr texture = Ogre::TextureManager::getSingleton().load(name, Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME);
+	if (texture.isNull())
+	{
+		std::stringstream error;
+		error << "Window icon not found: " << name;
+		throw std::runtime_error(error.str());
+	}
+	Ogre::Image image;
+	texture->convertToImage(image);
 
-    SDL_Surface* surface = SDL_CreateRGBSurface(0,texture->getWidth(),texture->getHeight(),32,0xFF000000,0x00FF0000,0x0000FF00,0x000000FF);
+	SDL_Surface* surface = SDL_CreateRGBSurface(0,texture->getWidth(),texture->getHeight(),32,0xFF000000,0x00FF0000,0x0000FF00,0x000000FF);
 
-    //copy the Ogre texture to an SDL surface
-    for(size_t x = 0; x < texture->getWidth(); ++x)
-    {
-        for(size_t y = 0; y < texture->getHeight(); ++y)
-        {
-            Ogre::ColourValue clr = image.getColourAt(x, y, 0);
+	//copy the Ogre texture to an SDL surface
+	for(size_t x = 0; x < texture->getWidth(); ++x)
+	{
+		for(size_t y = 0; y < texture->getHeight(); ++y)
+		{
+			Ogre::ColourValue clr = image.getColourAt(x, y, 0);
 
-            //set the pixel on the SDL surface to the same value as the Ogre texture's
-            int bpp = surface->format->BytesPerPixel;
-            /* Here p is the address to the pixel we want to set */
-            Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
-            Uint32 pixel = SDL_MapRGBA(surface->format, clr.r*255, clr.g*255, clr.b*255, clr.a*255);
-            switch(bpp) {
-            case 1:
-                *p = pixel;
-                break;
+			//set the pixel on the SDL surface to the same value as the Ogre texture's
+			int bpp = surface->format->BytesPerPixel;
+			/* Here p is the address to the pixel we want to set */
+			Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+			Uint32 pixel = SDL_MapRGBA(surface->format, clr.r*255, clr.g*255, clr.b*255, clr.a*255);
+			switch(bpp) {
+			case 1:
+				*p = pixel;
+				break;
 
-            case 2:
-                *(Uint16 *)p = pixel;
-                break;
+			case 2:
+				*(Uint16 *)p = pixel;
+				break;
 
-            case 3:
-                if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-                    p[0] = (pixel >> 16) & 0xff;
-                    p[1] = (pixel >> 8) & 0xff;
-                    p[2] = pixel & 0xff;
-                } else {
-                    p[0] = pixel & 0xff;
-                    p[1] = (pixel >> 8) & 0xff;
-                    p[2] = (pixel >> 16) & 0xff;
-                }
-                break;
+			case 3:
+				if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+					p[0] = (pixel >> 16) & 0xff;
+					p[1] = (pixel >> 8) & 0xff;
+					p[2] = pixel & 0xff;
+				} else {
+					p[0] = pixel & 0xff;
+					p[1] = (pixel >> 8) & 0xff;
+					p[2] = (pixel >> 16) & 0xff;
+				}
+				break;
 
-            case 4:
-                *(Uint32 *)p = pixel;
-                break;
-            }
-        }
-    }
+			case 4:
+				*(Uint32 *)p = pixel;
+				break;
+			}
+		}
+	}
 
-    SDL_SetWindowIcon(mSDLWindow, surface);
-    SDL_FreeSurface(surface);
+	SDL_SetWindowIcon(mSDLWindow, surface);
+	SDL_FreeSurface(surface);
 }
 
 }
