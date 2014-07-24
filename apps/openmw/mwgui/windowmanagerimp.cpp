4b3e12bfb4fe5450a3ad22bdb12e04760ed8e558
scrawl <scrawl@baseoftrash.de>
2014-07-24 18:03:11 +0200

    Fix pinned widgets receiving mouse clicks in gamemode (Fixes #1686)

--- apps/openmw/mwgui/windowmanagerimp.cpp
+++ apps/openmw/mwgui/windowmanagerimp.cpp
@@ -268,7 +268,7 @@ namespace MWGui
         mCompanionWindow = new CompanionWindow(mDragAndDrop, mMessageBoxManager);
         trackWindow(mCompanionWindow, "companion");
 
-        mInputBlocker = mGui->createWidget<MyGUI::Widget>("",0,0,w,h,MyGUI::Align::Default,"Windows");
+        mInputBlocker = mGui->createWidget<MyGUI::Widget>("",0,0,w,h,MyGUI::Align::Default,"Overlay");
 
         mHud->setVisible(mHudEnabled);
 
