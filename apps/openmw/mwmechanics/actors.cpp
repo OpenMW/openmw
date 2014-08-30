180a831e96d5811505ca6c9533e9bfebce380a73
scrawl <scrawl@baseoftrash.de>
2014-08-30 18:37:51 +0200

    Fix typo in combat distance

--- apps/openmw/mwmechanics/actors.cpp
+++ apps/openmw/mwmechanics/actors.cpp
@@ -280,7 +280,7 @@ namespace MWMechanics
                 || actor1.getClass().getCreatureStats(actor1).isDead())
             return;
 
-        const ESM::Position& actor1Pos = actor2.getRefData().getPosition();
+        const ESM::Position& actor1Pos = actor1.getRefData().getPosition();
         const ESM::Position& actor2Pos = actor2.getRefData().getPosition();
         float sqrDist = Ogre::Vector3(actor1Pos.pos).squaredDistance(Ogre::Vector3(actor2Pos.pos));
         if (sqrDist > 7168*7168)
