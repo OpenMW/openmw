ff11745429bee0d7b1468d210e1856fd64c5f70d
scrawl <scrawl@baseoftrash.de>
2014-10-05 14:47:23 +0200

    Fix magic projectile sound disappearing after multiple save/load cycles

--- apps/openmw/mwworld/projectilemanager.cpp
+++ apps/openmw/mwworld/projectilemanager.cpp
@@ -409,6 +409,7 @@ namespace MWWorld
             MWBase::SoundManager *sndMgr = MWBase::Environment::get().getSoundManager();
             state.mSound = sndMgr->playManualSound3D(esm.mPosition, esm.mSound, 1.0f, 1.0f,
                                                      MWBase::SoundManager::Play_TypeSfx, MWBase::SoundManager::Play_Loop);
+            state.mSoundId = esm.mSound;
 
             mMagicBolts.push_back(state);
             return true;
