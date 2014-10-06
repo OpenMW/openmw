6d687993b85666a7565afe060b9dc4bb7877c250
cc9cii <cc9c@iinet.net.au>
2014-10-06 14:08:35 +1100

    Remove C++11 stuff.

--- apps/opencs/view/render/scenewidget.cpp
+++ apps/opencs/view/render/scenewidget.cpp
@@ -300,14 +300,14 @@ namespace CSVRender
             //FIXME: for testing only
             case Qt::Key_N:
                 setVisibilityMask((uint32_t)mViewport->getVisibilityMask()
-                        & ~(uint32_t)CSVRender::Elements::Element_CellMarker);
+                        & ~(uint32_t)CSVRender::Element_CellMarker);
                 updateOverlay();
                 break;
 
             //FIXME: for testing only
             case Qt::Key_M:
                 setVisibilityMask((uint32_t)mViewport->getVisibilityMask()
-                        | (uint32_t)CSVRender::Elements::Element_CellMarker);
+                        | (uint32_t)CSVRender::Element_CellMarker);
                 updateOverlay();
                 break;
 
