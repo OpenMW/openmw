OpenMW/openmw

commit d55fe43fc95bfaec681c65b57aff378e6703f894

Support animation groups for Light and Door objects (Fixes #2039)

apps/openmw/mwrender/actors.cpp
@@ -83,10 +83,19 @@ void Actors::insertCreature (const MWWorld::Ptr& ptr, bool weaponsShields)
     mAllActors[ptr] = anim;
     mRendering->addWaterRippleEmitter (ptr);
 }
-void Actors::insertActivator (const MWWorld::Ptr& ptr)
+void Actors::insertActivator (const MWWorld::Ptr& ptr, bool addLight)
 {
     insertBegin(ptr);
     ActivatorAnimation* anim = new ActivatorAnimation(ptr);
+
+    if(ptr.getTypeName() == typeid(ESM::Light).name())
+    {
+        if (addLight)
+            anim->addLight(ptr.get<ESM::Light>()->mBase);
+        else
+            anim->removeParticles();
+    }
+
     delete mAllActors[ptr];
     mAllActors[ptr] = anim;
 }

apps/openmw/mwrender/actors.hpp
@@ -41,7 +41,7 @@ namespace MWRender
 
         void insertNPC(const MWWorld::Ptr& ptr);
         void insertCreature (const MWWorld::Ptr& ptr, bool weaponsShields);
-        void insertActivator (const MWWorld::Ptr& ptr);
+        void insertActivator (const MWWorld::Ptr& ptr, bool addLight=false);
          bool deleteObject (const MWWorld::Ptr& ptr);
         ///< \return found?
 

apps/openmw/mwrender/objects.cpp
@@ -73,19 +73,11 @@ void Objects::insertBegin(const MWWorld::Ptr& ptr)
     ptr.getRefData().setBaseNode(insert);
 }
 
-void Objects::insertModel(const MWWorld::Ptr &ptr, const std::string &mesh, bool batch, bool addLight)
+void Objects::insertModel(const MWWorld::Ptr &ptr, const std::string &mesh, bool batch)
 {
     insertBegin(ptr);
 
     std::auto_ptr<ObjectAnimation> anim(new ObjectAnimation(ptr, mesh));
-
-    if(ptr.getTypeName() == typeid(ESM::Light).name())
-    {
-        if (addLight)
-            anim->addLight(ptr.get<ESM::Light>()->mBase);
-        else
-            anim->removeParticles();
-    }
 
     if (!mesh.empty())
     {

apps/openmw/mwrender/objects.hpp
@@ -41,7 +41,7 @@ public:
         , mRootNode(NULL)
     {}
     ~Objects(){}
-    void insertModel(const MWWorld::Ptr& ptr, const std::string &model, bool batch=false, bool addLight=false);
+    void insertModel(const MWWorld::Ptr& ptr, const std::string &model, bool batch=false);
 
     ObjectAnimation* getAnimation(const MWWorld::Ptr &ptr);
 

apps/openmw/mwclass/door.cpp
@@ -8,6 +8,7 @@
 #include "../mwbase/world.hpp"
 #include "../mwbase/windowmanager.hpp"
 #include "../mwbase/soundmanager.hpp"
+#include "../mwbase/mechanicsmanager.hpp"
 
 #include "../mwworld/ptr.hpp"
 #include "../mwworld/nullaction.hpp"
@@ -22,7 +23,7 @@
 
 #include "../mwgui/tooltips.hpp"
 
-#include "../mwrender/objects.hpp"
+#include "../mwrender/actors.hpp"
 #include "../mwrender/renderinginterface.hpp"
 
 namespace
@@ -51,7 +52,8 @@ namespace MWClass
     {
         const std::string model = getModel(ptr);
         if (!model.empty()) {
-            renderingInterface.getObjects().insertModel(ptr, model);
+            MWRender::Actors& actors = renderingInterface.getActors();
+            actors.insertActivator(ptr);
         }
     }
 
@@ -70,6 +72,8 @@ namespace MWClass
                 MWBase::Environment::get().getWorld()->activateDoor(ptr, customData.mDoorState);
             }
         }
+
+        MWBase::Environment::get().getMechanicsManager()->add(ptr);
     }
 
     std::string Door::getModel(const MWWorld::Ptr &ptr) const

apps/openmw/mwclass/light.cpp
@@ -8,6 +8,7 @@
 #include "../mwbase/world.hpp"
 #include "../mwbase/soundmanager.hpp"
 #include "../mwbase/windowmanager.hpp"
+#include "../mwbase/mechanicsmanager.hpp"
 
 #include "../mwworld/ptr.hpp"
 #include "../mwworld/actiontake.hpp"
@@ -22,6 +23,7 @@
 #include "../mwgui/tooltips.hpp"
 
 #include "../mwrender/objects.hpp"
+#include "../mwrender/actors.hpp"
 #include "../mwrender/renderinginterface.hpp"
 
 namespace
@@ -54,13 +56,12 @@ namespace MWClass
 
     void Light::insertObjectRendering (const MWWorld::Ptr& ptr, MWRender::RenderingInterface& renderingInterface) const
     {
-        const std::string model = getModel(ptr);
-
         MWWorld::LiveCellRef<ESM::Light> *ref =
             ptr.get<ESM::Light>();
 
         // Insert even if model is empty, so that the light is added
-        renderingInterface.getObjects().insertModel(ptr, model, false, !(ref->mBase->mData.mFlags & ESM::Light::OffDefault));
+        MWRender::Actors& actors = renderingInterface.getActors();
+        actors.insertActivator(ptr, !(ref->mBase->mData.mFlags & ESM::Light::OffDefault));
     }
 
     void Light::insertObject(const MWWorld::Ptr& ptr, MWWorld::PhysicsSystem& physics) const
@@ -78,6 +79,8 @@ namespace MWClass
             MWBase::Environment::get().getSoundManager()->playSound3D(ptr, ref->mBase->mSound, 1.0, 1.0,
                                                                       MWBase::SoundManager::Play_TypeSfx,
                                                                       MWBase::SoundManager::Play_Loop);
+
+        MWBase::Environment::get().getMechanicsManager()->add(ptr);
     }
 
     std::string Light::getModel(const MWWorld::Ptr &ptr) const

apps/openmw/mwrender/activatoranimation.cpp
@@ -1,5 +1,8 @@
 #include "activatoranimation.hpp"
 
+#include <OgreSceneNode.h>
+#include <OgreParticleSystem.h>
+
 #include <components/esm/loadacti.hpp>
 
 #include "../mwbase/world.hpp"
@@ -26,6 +29,28 @@ ActivatorAnimation::ActivatorAnimation(const MWWorld::Ptr &ptr)
         setRenderProperties(mObjectRoot, RV_Misc, RQG_Main, RQG_Alpha);
 
         addAnimSource(model);
+    }
+    else
+    {
+        // No model given. Create an object root anyway, so that lights can be added to it if needed.
+        mObjectRoot = NifOgre::ObjectScenePtr (new NifOgre::ObjectScene(mInsert->getCreator()));
+    }
+}
+
+void ActivatorAnimation::addLight(const ESM::Light *light)
+{
+    addExtraLight(mInsert->getCreator(), mObjectRoot, light);
+}
+
+void ActivatorAnimation::removeParticles()
+{
+    for (unsigned int i=0; i<mObjectRoot->mParticles.size(); ++i)
+    {
+        // Don't destroyParticleSystem, the ParticleSystemController is still holding a pointer to it.
+        // Don't setVisible, this could conflict with a VisController.
+        // The following will remove all spawned particles, then set the speed factor to zero so that no new ones will be spawned.
+        mObjectRoot->mParticles[i]->setSpeedFactor(0.f);
+        mObjectRoot->mParticles[i]->clear();
     }
 }
 

apps/openmw/mwrender/activatoranimation.hpp
@@ -15,6 +15,9 @@ namespace MWRender
     public:
         ActivatorAnimation(const MWWorld::Ptr& ptr);
         virtual ~ActivatorAnimation();
+
+        void addLight(const ESM::Light *light);
+        void removeParticles();
     };
 }
 

commit 5d7eb11596be16f9426f40535b4e422774b0e36c

Fix lights being rendered on the map (broken by d55fe43fc95bf)

apps/openmw/mwrender/actors.cpp
@@ -198,4 +198,18 @@ void Actors::updateObjectCell(const MWWorld::Ptr &old, const MWWorld::Ptr &cur)
     mRendering->updateWaterRippleEmitterPtr (old, cur);
 }
 
+void Actors::enableLights()
+{
+    PtrAnimationMap::const_iterator it = mAllActors.begin();
+    for(;it != mAllActors.end();++it)
+        it->second->enableLights(true);
+}
+
+void Actors::disableLights()
+{
+    PtrAnimationMap::const_iterator it = mAllActors.begin();
+    for(;it != mAllActors.end();++it)
+        it->second->enableLights(false);
+}
+
 }

apps/openmw/mwrender/actors.hpp
@@ -42,8 +42,11 @@ namespace MWRender
         void insertNPC(const MWWorld::Ptr& ptr);
         void insertCreature (const MWWorld::Ptr& ptr, bool weaponsShields);
         void insertActivator (const MWWorld::Ptr& ptr, bool addLight=false);
-         bool deleteObject (const MWWorld::Ptr& ptr);
+        bool deleteObject (const MWWorld::Ptr& ptr);
         ///< \return found?
+
+        void enableLights();
+        void disableLights();
 
         void removeCell(MWWorld::CellStore* store);
 

apps/openmw/mwrender/objects.cpp
@@ -237,20 +237,6 @@ Ogre::AxisAlignedBox Objects::getDimensions(MWWorld::CellStore* cell)
     return mBounds[cell];
 }
 
-void Objects::enableLights()
-{
-    PtrAnimationMap::const_iterator it = mObjects.begin();
-    for(;it != mObjects.end();++it)
-        it->second->enableLights(true);
-}
-
-void Objects::disableLights()
-{
-    PtrAnimationMap::const_iterator it = mObjects.begin();
-    for(;it != mObjects.end();++it)
-        it->second->enableLights(false);
-}
-
 void Objects::update(float dt, Ogre::Camera* camera)
 {
     PtrAnimationMap::const_iterator it = mObjects.begin();

apps/openmw/mwrender/objects.hpp
@@ -45,9 +45,6 @@ public:
 
     ObjectAnimation* getAnimation(const MWWorld::Ptr &ptr);
 
-    void enableLights();
-    void disableLights();
-
     void update (float dt, Ogre::Camera* camera);
     ///< per-frame update
 

apps/openmw/mwrender/renderingmanager.cpp
@@ -677,13 +677,13 @@ void RenderingManager::writeFog(MWWorld::CellStore* cell)
 
 void RenderingManager::disableLights(bool sun)
 {
-    mObjects->disableLights();
+    mActors->disableLights();
     sunDisable(sun);
 }
 
 void RenderingManager::enableLights(bool sun)
 {
-    mObjects->enableLights();
+    mActors->enableLights();
     sunEnable(sun);
 }
 