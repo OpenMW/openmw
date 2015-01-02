a8621e62308abafae018e30c76f3f2feddd5edcb
mrcheko <cheko@sevas.ua>
2015-01-02 16:49:22 +0300

    defaults are set to 1.0; remove contrast setting
    (can be changed in config though); disable gamma control for not Windows
    OSs

--- apps/openmw/mwrender/renderingmanager.cpp
+++ apps/openmw/mwrender/renderingmanager.cpp
@@ -757,7 +757,7 @@ void RenderingManager::processChangedSettings(const Settings::CategorySettingVec
             changeRes = true;
         else if (it->second == "field of view" && it->first == "General")
             mRendering.setFov(Settings::Manager::getFloat("field of view", "General"));
-        else if ((it->second == "gamma" || it->second == "contrast") && it->first == "General")
+        else if (it->second == "gamma" && it->first == "General")
         {
             mRendering.setWindowGammaContrast(Settings::Manager::getFloat("gamma", "General"), Settings::Manager::getFloat("contrast", "General"));
         }
