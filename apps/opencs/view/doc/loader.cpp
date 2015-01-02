ce7cef924e80ab551066e779446dcb86c25cb4c9
Scott Howard <showard314@gmail.com>
2015-01-01 22:27:08 -0500

    when loading a file fails, pop-up critical window and highlight error text

--- apps/opencs/view/doc/loader.cpp
+++ apps/opencs/view/doc/loader.cpp
@@ -8,6 +8,8 @@
 #include <QDialogButtonBox>
 #include <QCloseEvent>
 #include <QListWidget>
+#include <QMessageBox>
+#include <QApplication>
 
 #include "../../model/doc/document.hpp"
 
@@ -104,8 +106,10 @@ void CSVDoc::LoadingDocument::nextRecord (int records)
 void CSVDoc::LoadingDocument::abort (const std::string& error)
 {
     mAborted = true;
-    mError->setText (QString::fromUtf8 (("Loading failed: " + error).c_str()));
+    mError->setText (QString::fromUtf8 (("<font color=red>Loading failed: " + error + "</font>").c_str()));
     mButtons->setStandardButtons (QDialogButtonBox::Close);
+    QMessageBox::critical(this, tr("OpenCS Loading Failed"),
+        QString::fromUtf8 (("Loading failed:\n" + error).c_str()));
 }
 
 void CSVDoc::LoadingDocument::addMessage (const std::string& message)
