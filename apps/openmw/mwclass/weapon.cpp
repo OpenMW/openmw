bc686c93b50485bc6e3bb4e94d83c7b46def6dca
scrawl <scrawl@baseoftrash.de>
2015-01-04 01:36:57 +0100

    Potential fix for thrown weapons being regarded as broken

--- apps/openmw/mwclass/weapon.cpp
+++ apps/openmw/mwclass/weapon.cpp
@@ -386,7 +386,7 @@ namespace MWClass
 
     std::pair<int, std::string> Weapon::canBeEquipped(const MWWorld::Ptr &ptr, const MWWorld::Ptr &npc) const
     {
-        if (ptr.getCellRef().getCharge() == 0)
+        if (hasItemHealth(ptr) && ptr.getCellRef().getCharge() == 0)
             return std::make_pair(0, "#{sInventoryMessage1}");
 
         std::pair<std::vector<int>, bool> slots_ = ptr.getClass().getEquipmentSlots(ptr);
