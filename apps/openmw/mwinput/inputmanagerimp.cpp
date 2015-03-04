4aee6e74c66097722c0aa8716ba6d043db3edae6
Antifleischwolf <Antifleischwolf@gmail.com>
2015-03-04 13:05:43 +0100

    Should fix bugs #2409 and #2351 by ignoring keypresses whenever textinput is active.

--- apps/openmw/mwinput/inputmanagerimp.cpp
+++ apps/openmw/mwinput/inputmanagerimp.cpp
@@ -665,11 +665,9 @@ namespace MWInput
                 && MWBase::Environment::get().getWindowManager()->getMode() == MWGui::GM_Console)
             SDL_StopTextInput();
 
-        bool consumed = false;
+        bool consumed = SDL_IsTextInputActive();
         if (kc != OIS::KC_UNASSIGNED)
         {
-            consumed = SDL_IsTextInputActive() &&
-                    ( !(SDLK_SCANCODE_MASK & arg.keysym.sym) && std::isprint(arg.keysym.sym)); // Little trick to check if key is printable
             bool guiFocus = MyGUI::InputManager::getInstance().injectKeyPress(MyGUI::KeyCode::Enum(kc), 0);
             setPlayerControlsEnabled(!guiFocus);
         }
