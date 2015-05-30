445f8280143b6a0286d602b5df43820940700044
cc9cii <cc9c@iinet.net.au>
2015-05-30 18:22:58 +1000

    Refresh filter only if one exists. Fixes Qt warning message "QSortFilterProxyModel: inconsistent changes reported by source model" when a record is deleted.

--- apps/opencs/model/world/idtableproxymodel.cpp
+++ apps/opencs/model/world/idtableproxymodel.cpp
@@ -56,6 +56,9 @@ bool CSMWorld::IdTableProxyModel::lessThan(const QModelIndex &left, const QModel
 
 void CSMWorld::IdTableProxyModel::refreshFilter()
 {
-    updateColumnMap();
-    invalidateFilter();
+    if (mFilter)
+    {
+        updateColumnMap();
+        invalidateFilter();
+    }
 }
