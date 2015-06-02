cd4042109a5046707dfa1f07d93e41f9a34d569b
Alexander "Ace" Olofsson <ace@haxalot.com>
2015-06-02 23:29:47 +0200

    Fix the script issues

--- CI/build.msvc.sh
+++ CI/build.msvc.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-while [ $# -gt 0 ]; do
-	ARG=$1
-	shift
+case $1 in
+	x86|i686|win32 )
+		BITS=32 ;;
 
-	case $ARG in
-		x86|i686|win32 )
-			BITS=32 ;;
+	x64_64|x64|win64 )
+		BITS=64 ;;
 
-		x64_64|x64|win64 )
-			BITS=64 ;;
-
-		* )
-			echo "Unknown arg $ARG."
-			exit 1 ;;
-	esac
-done
+	* )
+		echo "Unknown platform $ARG."
+		exit 1 ;;
+esac
 
 cd $(dirname $0)/../build_$BITS
 
-msbuild OpenMW.sln //t:Build //p:Configuration=Release //m:8 //logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild OpenMW.sln //t:Build //p:Configuration=$2 //m:8 //logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
