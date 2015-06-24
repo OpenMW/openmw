e29d9bcc8eac923ecf90c73733302033e01c2700
Marc Zinnschlag <marc@zpages.de>
2015-06-24 17:29:47 +0200

    use a namespace instead of a class with only static functions

--- apps/opencs/view/world/dragdroputils.hpp
+++ apps/opencs/view/world/dragdroputils.hpp
@@ -13,18 +13,17 @@ namespace CSMWorld
 
 namespace CSVWorld
 {
-    class DragDropUtils
+    namespace DragDropUtils
     {
-        public:
-            static const CSMWorld::TableMimeData *getTableMimeData(const QDropEvent &event);
+        const CSMWorld::TableMimeData *getTableMimeData(const QDropEvent &event);
 
-            static bool canAcceptData(const QDropEvent &event, CSMWorld::ColumnBase::Display type);
-            ///< Checks whether the \a event contains a valid CSMWorld::TableMimeData that holds the \a type
+        bool canAcceptData(const QDropEvent &event, CSMWorld::ColumnBase::Display type);
+        ///< Checks whether the \a event contains a valid CSMWorld::TableMimeData that holds the \a type
 
-            static CSMWorld::UniversalId getAcceptedData(const QDropEvent &event, CSMWorld::ColumnBase::Display type);
-            ///< Gets the accepted data from the \a event using the \a type
-            ///< \return Type_None if the \a event data doesn't holds the \a type
-    };
+        CSMWorld::UniversalId getAcceptedData(const QDropEvent &event, CSMWorld::ColumnBase::Display type);
+        ///< Gets the accepted data from the \a event using the \a type
+        ///< \return Type_None if the \a event data doesn't holds the \a type
+    }
 }
 
 #endif
