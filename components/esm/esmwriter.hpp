502cc852da0f10dae95ba10ec363a2be66707304
scrawl <scrawl@baseoftrash.de>
2015-06-26 20:16:32 +0200

    Handle encoding conversions when saving TES3 header (Fixes #2727)

--- components/esm/esmwriter.hpp
+++ components/esm/esmwriter.hpp
@@ -120,6 +120,7 @@ public:
         void startSubRecord(const std::string& name);
         void endRecord(const std::string& name);
         void endRecord(uint32_t name);
+        void writeFixedSizeString(const std::string& data, int size);
         void writeHString(const std::string& data);
         void writeHCString(const std::string& data);
         void writeName(const std::string& data);
