47317df33298bfd4126ae42b7802d8d8fa268b60
dteviot <dteviot@gmail.com>
2015-06-28 12:04:46 +1200

    Made PathFinder::buildPath() private.
    Now all paths are built by calling PathFinder::buildSyncedPath().
    Also removed useless comment.

--- apps/openmw/mwmechanics/pathfinding.hpp
+++ apps/openmw/mwmechanics/pathfinding.hpp
@@ -38,9 +38,6 @@ namespace MWMechanics
 
             void clearPath();
 
-            void buildPath(const ESM::Pathgrid::Point &startPoint, const ESM::Pathgrid::Point &endPoint,
-                           const MWWorld::CellStore* cell, bool allowShortcuts = true);
-
             bool checkPathCompleted(float x, float y, float tolerance=32.f);
             ///< \Returns true if we are within \a tolerance units of the last path point.
 
@@ -95,6 +92,8 @@ namespace MWMechanics
             }
 
         private:
+            void buildPath(const ESM::Pathgrid::Point &startPoint, const ESM::Pathgrid::Point &endPoint,
+                const MWWorld::CellStore* cell, bool allowShortcuts = true);
 
             std::list<ESM::Pathgrid::Point> mPath;
 
