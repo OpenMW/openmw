973c65bc822945b69e405f55f8608b29c4a7d256
Marc Zinnschlag <marc@zpages.de>
2015-08-21 14:02:32 +0200

    merge id collections
    (cherry picked from commit 3902513e65206908c0f0f54db213a7c9ea7f2fd0)

--- apps/opencs/model/tools/mergestages.hpp
+++ apps/opencs/model/tools/mergestages.hpp
@@ -5,10 +5,12 @@
 
 #include "../doc/stage.hpp"
 
+#include "../world/data.hpp"
+
+#include "mergestate.hpp"
+
 namespace CSMTools
 {
-    struct MergeState;
-
     class FinishMergedDocumentStage : public CSMDoc::Stage
     {
             MergeState& mState;
@@ -24,6 +26,53 @@ namespace CSMTools
             virtual void perform (int stage, CSMDoc::Messages& messages);
             ///< Messages resulting from this stage will be appended to \a messages.
     };
+
+    template<typename RecordType>
+    class MergeIdCollectionStage : public CSMDoc::Stage
+    {
+            typedef typename CSMWorld::IdCollection<RecordType> Collection;
+
+            MergeState& mState;
+            Collection& (CSMWorld::Data::*mAccessor)();
+
+        public:
+
+            MergeIdCollectionStage (MergeState& state, Collection& (CSMWorld::Data::*accessor)());
+
+            virtual int setup();
+            ///< \return number of steps
+
+            virtual void perform (int stage, CSMDoc::Messages& messages);
+            ///< Messages resulting from this stage will be appended to \a messages.
+    };
+
+    template<typename RecordType>
+    MergeIdCollectionStage<RecordType>::MergeIdCollectionStage (MergeState& state, Collection& (CSMWorld::Data::*accessor)())
+    : mState (state), mAccessor (accessor)
+    {}
+
+    template<typename RecordType>
+    int MergeIdCollectionStage<RecordType>::setup()
+    {
+        return 1;
+    }
+
+    template<typename RecordType>
+    void MergeIdCollectionStage<RecordType>::perform (int stage, CSMDoc::Messages& messages)
+    {
+        const Collection& source = (mState.mSource.getData().*mAccessor)();
+        Collection& target = (mState.mTarget->getData().*mAccessor)();
+
+        int size = source.getSize();
+
+        for (int i=0; i<size; ++i)
+        {
+            const CSMWorld::Record<RecordType>& record = source.getRecord (i);
+
+            if (!record.isDeleted())
+                target.appendRecord (CSMWorld::Record<RecordType> (CSMWorld::RecordBase::State_BaseOnly, &record.get()));
+        }
+    }
 }
 
 #endif
