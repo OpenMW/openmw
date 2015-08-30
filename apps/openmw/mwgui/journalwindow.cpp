OpenMW/openmw

commit 7dd09dd637c317d3d94497b9d1cca4e1c4e54420

Fix being able to flip journal pages with the mousewheel when the options overlay is active (Fixes #2855)

apps/openmw/mwgui/journalwindow.cpp
@@ -61,6 +61,7 @@ namespace
         DisplayStateStack mStates;
         Book mTopicIndexBook;
         bool mQuestMode;
+        bool mOptionsMode;
         bool mAllQuests;
 
         template <typename T>
@@ -182,6 +183,7 @@ namespace
 
             mQuestMode = false;
             mAllQuests = false;
+            mOptionsMode = false;
         }
 
         void adjustButton (char const * name, bool optional = false)
@@ -244,6 +246,7 @@ namespace
 
         void setBookMode ()
         {
+            mOptionsMode = false;
             setVisible (OptionsBTN, true);
             setVisible (OptionsOverlay, false);
 
@@ -253,6 +256,8 @@ namespace
 
         void setOptionsMode ()
         {
+            mOptionsMode = true;
+
             setVisible (OptionsBTN, false);
             setVisible (OptionsOverlay, true);
 
@@ -508,6 +513,8 @@ namespace
 
         void notifyNextPage(MyGUI::Widget* _sender)
         {
+            if (mOptionsMode)
+                return;
             if (!mStates.empty ())
             {
                 unsigned int  & page = mStates.top ().mPage;
@@ -523,6 +530,8 @@ namespace
 
         void notifyPrevPage(MyGUI::Widget* _sender)
         {
+            if (mOptionsMode)
+                return;
             if (!mStates.empty ())
             {
                 unsigned int & page = mStates.top ().mPage;

commit 8aaba0af6fac116f09092bbc1f88241988b7706e

Fix journal page navigation bug introduced by 7dd09dd637c317d3d94 (Fixes #2899)

apps/openmw/mwgui/journalwindow.cpp
@@ -356,6 +356,8 @@ namespace
             setVisible (OptionsOverlay, false);
             setVisible (OptionsBTN, true);
             setVisible (JournalBTN, true);
+
+            mOptionsMode = false;
         }
 
         void notifyTopicSelected (const std::string& topic, int id)
@@ -383,6 +385,8 @@ namespace
             setVisible (OptionsOverlay, false);
             setVisible (OptionsBTN, true);
             setVisible (JournalBTN, true);
+
+            mOptionsMode = false;
         }
 
         void notifyOptions(MyGUI::Widget* _sender)