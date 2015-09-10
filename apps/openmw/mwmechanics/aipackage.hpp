573a14993ac1d41979c805cd6298bd9f82227a64
dteviot <dteviot@gmail.com>
2015-09-10 21:53:31 +1200

    Moved isActorNearInactiveCell() logic to own function.
    Also, triggers when actor is near edge of cell, not when less than 1/2 way to edge.

--- apps/openmw/mwmechanics/aipackage.hpp
+++ apps/openmw/mwmechanics/aipackage.hpp
@@ -88,6 +88,7 @@ namespace MWMechanics
 
         private:
             void evadeObstacles(const MWWorld::Ptr& actor, float duration, ESM::Position& pos);
+            bool isActorNearInactiveCell(const ESM::Position& actorPos);
 
     };
 }
