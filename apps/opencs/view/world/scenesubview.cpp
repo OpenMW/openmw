b3b4fb3efbaf84a31b9a62203f9767b08328b671
Marc Zinnschlag <marc@zpages.de>
2015-09-17 11:27:55 +0200

    removed some left-over signal slot stuff that shouldn't have been there in the first place

--- apps/opencs/view/world/scenesubview.cpp
+++ apps/opencs/view/world/scenesubview.cpp
@@ -247,8 +247,6 @@ void CSVWorld::SceneSubView::replaceToolbarAndWorldspace (CSVRender::WorldspaceW
     mToolbar = toolbar;
 
     connect (mScene, SIGNAL (focusToolbarRequest()), mToolbar, SLOT (setFocus()));
-    connect (this, SIGNAL (updateSceneUserSetting(const QString &, const QStringList &)),
-            mScene, SLOT (updateUserSetting(const QString &, const QStringList &)));
     connect (mToolbar, SIGNAL (focusSceneRequest()), mScene, SLOT (setFocus()));
 
     mLayout->addWidget (mToolbar, 0);
@@ -256,9 +254,4 @@ void CSVWorld::SceneSubView::replaceToolbarAndWorldspace (CSVRender::WorldspaceW
 
     mScene->selectDefaultNavigationMode();
     setFocusProxy (mScene);
-}
-
-void CSVWorld::SceneSubView::updateUserSetting (const QString &key, const QStringList &list)
-{
-    emit updateSceneUserSetting(key, list);
 }
