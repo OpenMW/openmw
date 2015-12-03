06efd72a89117658351870c3d01b5b32b6f62f73
Marc Zinnschlag <marc@zpages.de>
2015-12-03 11:14:58 +0100

    allow keywords as strings in messagebox instruction (Fixes #2991)

--- components/compiler/scanner.hpp
+++ components/compiler/scanner.hpp
@@ -37,6 +37,7 @@ namespace Compiler
             float mPutbackFloat;
             std::string mPutbackName;
             TokenLoc mPutbackLoc;
+            bool mStrictKeywords;
 
         public:
 
@@ -116,13 +117,18 @@ namespace Compiler
             ///< put back a float token
 
             void putbackName (const std::string& name, const TokenLoc& loc);
-            ///< put back a name toekn
+            ///< put back a name token
 
             void putbackKeyword (int keyword, const TokenLoc& loc);
             ///< put back a keyword token
 
             void listKeywords (std::vector<std::string>& keywords);
-            ///< Append all known keywords to \a kaywords.
+            ///< Append all known keywords to \a keywords.
+
+            /// Do not accept keywords in quotation marks anymore.
+            ///
+            /// \attention This mode lasts only until the next newline is reached.
+            void enableStrictKeywords();
     };
 }
 
