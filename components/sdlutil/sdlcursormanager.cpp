1c902d156bb39504288499af010eb6cf8005ee55
sandstranger <sandstranger@gmail.com>
2015-12-10 21:13:11 +0300

    fix building with Opengl ES1 on linux

--- components/sdlutil/sdlcursormanager.cpp
+++ components/sdlutil/sdlcursormanager.cpp
@@ -217,7 +217,7 @@ namespace SDLUtil
 
     void SDLCursorManager::_createCursorFromResource(const std::string& name, int rotDegrees, osg::Image* image, Uint8 size_x, Uint8 size_y, Uint8 hotspot_x, Uint8 hotspot_y)
     {
-         #ifdef ANDROID
+         #ifdef OPENGL_ES
          return;
          #endif
 
