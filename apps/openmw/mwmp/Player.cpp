4be6b362c5fc045b54d7e140e120379fa813a77a
Koncord <koncord@rwa.su>
2016-01-04 20:15:55 +0800

    Add OpenMW-mp target

---
+++ apps/openmw/mwmp/Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by koncord on 02.01.16.
+//
+
+#include "Player.hpp"
---
+++ apps/openmw/mwmp/Player.hpp
@@ -0,0 +1,15 @@
+//
+// Created by koncord on 02.01.16.
+//
+
+#ifndef OPENMW_PLAYER_HPP
+#define OPENMW_PLAYER_HPP
+
+
+class Player
+{
+
+};
+
+
+#endif //OPENMW_PLAYER_HPP
