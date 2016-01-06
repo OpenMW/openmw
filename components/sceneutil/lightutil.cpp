3089aeccc4f82aebd9d5ae1801b3d9adcf5621eb
scrawl <scrawl@baseoftrash.de>
2016-01-06 13:37:55 +0100

    Factor out SceneUtil::addLight

--- components/sceneutil/lightutil.cpp
+++ components/sceneutil/lightutil.cpp
@@ -1,6 +1,16 @@
 #include "lightutil.hpp"
 
 #include <osg/Light>
+#include <osg/Group>
+#include <osg/ComputeBoundsVisitor>
+
+#include <components/esm/loadligh.hpp>
+
+#include "lightmanager.hpp"
+#include "lightcontroller.hpp"
+#include "util.hpp"
+#include "visitor.hpp"
+#include "positionattitudetransform.hpp"
 
 namespace SceneUtil
 {
@@ -27,6 +37,71 @@ namespace SceneUtil
         light->setQuadraticAttenuation(quadraticAttenuation);
         light->setConstantAttenuation(0.f);
 
+    }
+
+    void addLight (osg::Group* node, const ESM::Light* esmLight, int partsysMask, int lightMask, bool isExterior, bool outQuadInLin, bool useQuadratic,
+                   float quadraticValue, float quadraticRadiusMult, bool useLinear, float linearRadiusMult,
+                   float linearValue)
+    {
+        SceneUtil::FindByNameVisitor visitor("AttachLight");
+        node->accept(visitor);
+
+        osg::Group* attachTo = NULL;
+        if (visitor.mFoundNode)
+        {
+            attachTo = visitor.mFoundNode;
+        }
+        else
+        {
+            osg::ComputeBoundsVisitor computeBound;
+            computeBound.setTraversalMask(~partsysMask);
+            node->accept(computeBound);
+
+            // PositionAttitudeTransform seems to be slightly faster than MatrixTransform
+            osg::ref_ptr<SceneUtil::PositionAttitudeTransform> trans(new SceneUtil::PositionAttitudeTransform);
+            trans->setPosition(computeBound.getBoundingBox().center());
+
+            node->addChild(trans);
+
+            attachTo = trans;
+        }
+
+        osg::ref_ptr<SceneUtil::LightSource> lightSource (new SceneUtil::LightSource);
+        osg::ref_ptr<osg::Light> light (new osg::Light);
+        lightSource->setNodeMask(lightMask);
+
+        float radius = esmLight->mData.mRadius;
+        lightSource->setRadius(radius);
+
+        configureLight(light, radius, isExterior, outQuadInLin, useQuadratic, quadraticValue,
+                                  quadraticRadiusMult, useLinear, linearRadiusMult, linearValue);
+
+        osg::Vec4f diffuse = SceneUtil::colourFromRGB(esmLight->mData.mColor);
+        if (esmLight->mData.mFlags & ESM::Light::Negative)
+        {
+            diffuse *= -1;
+            diffuse.a() = 1;
+        }
+        light->setDiffuse(diffuse);
+        light->setAmbient(osg::Vec4f(0,0,0,1));
+        light->setSpecular(osg::Vec4f(0,0,0,0));
+
+        lightSource->setLight(light);
+
+        osg::ref_ptr<SceneUtil::LightController> ctrl (new SceneUtil::LightController);
+        ctrl->setDiffuse(light->getDiffuse());
+        if (esmLight->mData.mFlags & ESM::Light::Flicker)
+            ctrl->setType(SceneUtil::LightController::LT_Flicker);
+        if (esmLight->mData.mFlags & ESM::Light::FlickerSlow)
+            ctrl->setType(SceneUtil::LightController::LT_FlickerSlow);
+        if (esmLight->mData.mFlags & ESM::Light::Pulse)
+            ctrl->setType(SceneUtil::LightController::LT_Pulse);
+        if (esmLight->mData.mFlags & ESM::Light::PulseSlow)
+            ctrl->setType(SceneUtil::LightController::LT_PulseSlow);
+
+        lightSource->addUpdateCallback(ctrl);
+
+        attachTo->addChild(lightSource);
     }
 
 }
--- components/sceneutil/lightutil.hpp
+++ components/sceneutil/lightutil.hpp
@@ -3,16 +3,29 @@
 
 namespace osg
 {
+    class Group;
+}
+
+namespace ESM
+{
     class Light;
 }
 
 namespace SceneUtil
 {
 
-    /// @brief Configures a light's attenuation according to vanilla Morrowind attenuation settings.
-    void configureLight(osg::Light* light, float radius, bool isExterior, bool outQuadInLin, bool useQuadratic,
-                        float quadraticValue, float quadraticRadiusMult, bool useLinear, float linearRadiusMult,
-                        float linearValue);
+    /// @brief Convert an ESM::Light to a SceneUtil::LightSource, and add it to a sub graph.
+    /// @note If the sub graph contains a node named "AttachLight" (case insensitive), then the light is added to that.
+    /// Otherwise, the light is added in the center of the node's bounds.
+    /// @param node The sub graph to add a light to
+    /// @param esmLight The light definition coming from the game files containing radius, color, flicker, etc.
+    /// @param partsysMask Node mask to ignore when computing the sub graph's bounding box.
+    /// @param lightMask Mask to assign to the newly created LightSource.
+    /// @param isExterior Is the light outside? May be used for deciding which attenuation settings to use.
+    /// @par Attenuation parameters come from the game INI file.
+    void addLight (osg::Group* node, const ESM::Light* esmLight, int partsysMask, int lightMask, bool isExterior, bool outQuadInLin, bool useQuadratic,
+                   float quadraticValue, float quadraticRadiusMult, bool useLinear, float linearRadiusMult,
+                   float linearValue);
 
 }
 
