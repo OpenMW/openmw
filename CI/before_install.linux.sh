b77ed829b963179e4848035b55eb71ce06625040
scrawl <scrawl@baseoftrash.de>
2016-02-15 15:17:23 +0100

    Revert "Use Qt5 on travis to match the PPA's OSG build"
    This reverts commit 3c717a63603b53b301370bdf7cdef12441582748.

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -11,11 +11,11 @@ sudo apt-get update -qq
 sudo apt-get install -qq libgtest-dev google-mock
 sudo apt-get install -qq libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev
 sudo apt-get install -qq libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev
-sudo apt-get install -qq libbullet-dev libopenscenegraph-dev libmygui-dev libsdl2-dev libunshield-dev libtinyxml-dev libopenal-dev qtbase5-dev
+sudo apt-get install -qq libbullet-dev libopenscenegraph-dev libmygui-dev libsdl2-dev libunshield-dev libtinyxml-dev libopenal-dev libqt4-dev
 if [ "${ANALYZE}" ]; then sudo apt-get install -qq clang-3.6; fi
 sudo mkdir /usr/src/gtest/build
 cd /usr/src/gtest/build
-sudo cmake .. -DBUILD_SHARED_LIBS=1 -DDESIRED_QT_VERSION=5
+sudo cmake .. -DBUILD_SHARED_LIBS=1
 sudo make -j4
 sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
 sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
