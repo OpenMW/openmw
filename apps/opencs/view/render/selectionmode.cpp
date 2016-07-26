05888e7925904549241a19903dc618a793fca5fc
Aesylwinn <kyleacooley@gmail.com>
2016-07-25 22:43:37 -0400

    Updated more tool tips to show shortcut configs.

--- apps/opencs/view/render/selectionmode.cpp
+++ apps/opencs/view/render/selectionmode.cpp
@@ -15,22 +15,28 @@ namespace CSVRender
     {
         addButton(":placeholder", "cube-centre",
             "Centred cube"
-            "<ul><li>Drag with primary (make instances the selection) or secondary (invert selection state) select button from the centre of the selection cube outwards</li>"
+            "<ul><li>Drag with {scene-select-primary} (make instances the selection) or {scene-select-secondary} "
+                "(invert selection state) from the centre of the selection cube outwards</li>"
             "<li>The selection cube is aligned to the word space axis</li>"
-            "<li>If context selection mode is enabled, a drag with primary/secondary edit not starting on an instance will have the same effect</li>"
+            "<li>If context selection mode is enabled, a drag with {scene-edit-primary} or {scene-edit-secondary} not "
+                "starting on an instance will have the same effect</li>"
             "</ul>"
             "<font color=Red>Not implemented yet</font color>");
         addButton(":placeholder", "cube-corner",
             "Cube corner to corner"
-            "<ul><li>Drag with primary (make instances the selection) or secondary (invert selection state) select button from one corner of the selection cube to the opposite corner</li>"
+            "<ul><li>Drag with {scene-select-primary} (make instances the selection) or {scene-select-secondary} "
+                "(invert selection state) from one corner of the selection cube to the opposite corner</li>"
             "<li>The selection cube is aligned to the word space axis</li>"
-            "<li>If context selection mode is enabled, a drag with primary/secondary edit not starting on an instance will have the same effect</li>"
+            "<li>If context selection mode is enabled, a drag with {scene-edit-primary} or {scene-edit-secondary} not "
+                "starting on an instance will have the same effect</li>"
             "</ul>"
             "<font color=Red>Not implemented yet</font color>");
         addButton(":placeholder", "sphere",
             "Centred sphere"
-            "<ul><li>Drag with primary (make instances the selection) or secondary (invert selection state) select button from the centre of the selection sphere outwards</li>"
-            "<li>If context selection mode is enabled, a drag with primary/secondary edit not starting on an instance will have the same effect</li>"
+            "<ul><li>Drag with {scene-select-primary} (make instances the selection) or {scene-select-secondary} "
+                "(invert selection state) from the centre of the selection sphere outwards</li>"
+            "<li>If context selection mode is enabled, a drag with {scene-edit-primary} or {scene-edit-secondary} not "
+                "starting on an instance will have the same effect</li>"
             "</ul>"
             "<font color=Red>Not implemented yet</font color>");
 
