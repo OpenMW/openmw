448c9d5a5bf598b938b6720df1db2fbaa3d360ec
Aesylwinn <kyleacooley@gmail.com>
2016-08-09 00:17:41 -0400

    Fix globals not being initialized with a valid type.

---
+++ apps/opencs/view/world/globalcreator.cpp
@@ -0,0 +1,26 @@
+#include "globalcreator.hpp"
+
+#include <components/esm/variant.hpp>
+
+#include "../../model/world/data.hpp"
+#include "../../model/world/commands.hpp"
+#include "../../model/world/columns.hpp"
+#include "../../model/world/idtable.hpp"
+
+namespace CSVWorld
+{
+    void GlobalCreator::configureCreateCommand (CSMWorld::CreateCommand& command) const
+    {
+        CSMWorld::IdTable* table = static_cast<CSMWorld::IdTable*>(getData().getTableModel(getCollectionId()));
+
+        int index = table->findColumnIndex(CSMWorld::Columns::ColumnId_ValueType);
+        int type = (int)ESM::VT_Float;
+
+        command.addValue(index, type);
+    }
+
+    GlobalCreator::GlobalCreator(CSMWorld::Data& data, QUndoStack& undoStack, const CSMWorld::UniversalId& id)
+        : GenericCreator (data, undoStack, id, true)
+    {
+    }
+}
