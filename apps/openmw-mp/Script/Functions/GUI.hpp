5e6a37468770f8efc70334ed830c614984e8a2cd
David Cernat <davidcernat@gmail.com>
2016-08-30 08:13:08 +0300

    Add comment about MesssageBox to prevent future confusion

--- apps/openmw-mp/Script/Functions/GUI.hpp
+++ apps/openmw-mp/Script/Functions/GUI.hpp
@@ -15,7 +15,9 @@
 class GUIFunctions
 {
 public:
+    /* Do not rename into MessageBox to not conflict with WINAPI's MessageBox */
     static void _MessageBox(unsigned short pid, int id, const char *label) noexcept;
+    
     static void CustomMessageBox(unsigned short pid, int id, const char *label, const char *buttons) noexcept;
     static void InputDialog(unsigned short pid, int id, const char *label) noexcept;
 
