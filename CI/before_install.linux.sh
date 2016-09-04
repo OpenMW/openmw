31c372a0e7c981a6ed4f02035fff735dac6aa0d6
Koncord <koncord@rwa.su>
2016-09-04 14:47:21 +0800

    resolve depends for travis

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -9,3 +9,15 @@ sudo cmake .. -DBUILD_SHARED_LIBS=1
 sudo make -j4
 sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
 sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
+
+cd ~/
+git clone https://github.com/TES3MP/RakNet
+cd RakNet
+cmake . -DRAKNET_ENABLE_DLL=OFF -DRAKNET_ENABLE_SAMPLES=OFF -DCMAKE_BUILD_TYPE=Release
+mkdir ./lib
+cp ./Lib/RakNetLibStatic/libRakNetLibStatic.a ./lib
+make -j3 install
+cd ..
+
+wget https://github.com/zdevito/terra/releases/download/release-2016-03-25/terra-Linux-x86_64-332a506.zip
+unzip terra-Linux-x86_64-332a506.zip
--- CI/before_script.linux.sh
+++ CI/before_script.linux.sh
@@ -4,5 +4,7 @@ free -m
 mkdir build
 cd build
 export CODE_COVERAGE=1
+export RAKNET_ROOT=~/RakNet
+export Terra_ROOT=~/terra-Linux-x86_64-332a506
 if [ "${CC}" = "clang" ]; then export CODE_COVERAGE=0; fi
-${ANALYZE}cmake .. -DBUILD_WITH_CODE_COVERAGE=${CODE_COVERAGE} -DBUILD_UNITTESTS=1 -DCMAKE_INSTALL_PREFIX=/usr -DBINDIR=/usr/games  -DCMAKE_BUILD_TYPE="None" -DUSE_SYSTEM_TINYXML=TRUE
+${ANALYZE}cmake .. -DBUILD_WITH_CODE_COVERAGE=${CODE_COVERAGE} -DBUILD_UNITTESTS=1 -DCMAKE_INSTALL_PREFIX=/usr -DBINDIR=/usr/games  -DCMAKE_BUILD_TYPE="None" -DUSE_SYSTEM_TINYXML=TRUE -DRakNet_LIBRARY_RELEASE=~/RakNet/lib/libRakNetLibStatic.a -DRakNet_LIBRARY_DEBUG=~/RakNet/lib/libRakNetLibStatic.a
