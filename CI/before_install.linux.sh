9abd4e5a162f43887e7bfe9a965bf7efc000e632
Koncord <koncord@rwa.su>
2016-09-04 17:03:07 +0800

    Fail :D

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -15,8 +15,8 @@ git clone https://github.com/TES3MP/RakNet
 cd RakNet
 cmake . -DRAKNET_ENABLE_DLL=OFF -DRAKNET_ENABLE_SAMPLES=OFF -DCMAKE_BUILD_TYPE=Release
 mkdir ./lib
-cp ./Lib/RakNetLibStatic/libRakNetLibStatic.a ./lib
 make -j3 install
+cp ./Lib/RakNetLibStatic/libRakNetLibStatic.a ./lib
 cd ..
 
 wget https://github.com/zdevito/terra/releases/download/release-2016-03-25/terra-Linux-x86_64-332a506.zip
