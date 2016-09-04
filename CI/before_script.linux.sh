b2f1bb06f2bd0d363eb758b9b599d09ab7609fca
Koncord <koncord@rwa.su>
2016-09-04 16:51:16 +0800

    fix travis 2

--- CI/before_script.linux.sh
+++ CI/before_script.linux.sh
@@ -6,5 +6,10 @@ cd build
 export CODE_COVERAGE=1
 export RAKNET_ROOT=~/RakNet
 export Terra_ROOT=~/terra-Linux-x86_64-332a506
-if [ "${CC}" = "clang" ]; then export CODE_COVERAGE=0; fi
+if [ "${CC}" = "clang" ]; then export CODE_COVERAGE=0; 
+else 
+    export COMPILER_NAME=gcc
+    export CXX=g++-6
+    export CC=gcc-6
+fi
 ${ANALYZE}cmake .. -DBUILD_WITH_CODE_COVERAGE=${CODE_COVERAGE} -DBUILD_BSATOOL=OFF -DBUILD_ESMTOOL=OFF -DBUILD_ESSIMPORTER=OFF -DBUILD_LAUNCHER=OFF -DBUILD_MWINIIMPORTER=OFF -DBUILD_MYGUI_PLUGIN=OFF -DBUILD_OPENCS=OFF -DBUILD_WIZARD=OFF -DBUILD_UNITTESTS=1 -DCMAKE_INSTALL_PREFIX=/usr -DBINDIR=/usr/games  -DCMAKE_BUILD_TYPE="None" -DUSE_SYSTEM_TINYXML=TRUE -DRakNet_LIBRARY_RELEASE=~/RakNet/lib/libRakNetLibStatic.a -DRakNet_LIBRARY_DEBUG=~/RakNet/lib/libRakNetLibStatic.a
