f31342894a449a71c82560ea85037cf3a4a7821f
Allofich <rickywd@gmail.com>
2016-09-09 00:02:24 +0900

    Put simpler condition first

--- apps/openmw/mwworld/worldimp.cpp
+++ apps/openmw/mwworld/worldimp.cpp
@@ -3162,7 +3162,7 @@ namespace MWWorld
         {
             const ESM::MagicEffect* effect = getStore().get<ESM::MagicEffect>().find(effectIt->mEffectID);
 
-            if ((effectIt->mArea <= 0 && !ignore.isEmpty() && ignore.getClass().isActor()) || effectIt->mRange != rangeType)
+            if (effectIt->mRange != rangeType || (effectIt->mArea <= 0 && !ignore.isEmpty() && ignore.getClass().isActor()))
                 continue; // Not right range type, or not area effect and hit an actor
 
             // Spawn the explosion orb effect
