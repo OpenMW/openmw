231908d9cfcf962194d29dec94fd8b8e22f0ba68
Nikolay Kasyanov <nikolaykasyanov@users.noreply.github.com>
2016-09-17 13:01:04 +0200

    [OS X, CI] Use dependencies build against libc++

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -6,5 +6,5 @@ brew outdated cmake || brew upgrade cmake
 brew outdated pkgconfig || brew upgrade pkgconfig
 brew install $macos_qt_formula
 
-curl https://downloads.openmw.org/osx/dependencies/openmw-deps-c79172d.zip -o ~/openmw-deps.zip
+curl http://downloads.openmw.org/osx/dependencies/openmw-deps-c++11.zip -o ~/openmw-deps.zip
 unzip ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
