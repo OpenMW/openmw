7c7b87a43e523a9846add22ea18220841310be8d
sandstranger <sandstranger@gmail.com>
2016-10-05 21:24:15 +0400

    fix gui crash on gles1

--- components/myguiplatform/myguirendermanager.cpp
+++ components/myguiplatform/myguirendermanager.cpp
@@ -128,8 +128,11 @@ public:
             osg::Texture2D* texture = batch.mTexture;
             if(texture)
                 state->applyTextureAttribute(0, texture);
-
+#if defined (ANDROID) || defined (OPENGL_ES)
+            osg::GLBufferObject* bufferobject = 0;
+#else
             osg::GLBufferObject* bufferobject = state->isVertexBufferObjectSupported() ? vbo->getOrCreateGLBufferObject(state->getContextID()) : 0;
+#endif
             if (bufferobject)
             {
                 state->bindVertexBufferObject(bufferobject);
