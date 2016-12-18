6d8d9e5dbc7c4af6db78d2db9129e7c5461fa323
Nikolay Kasyanov <corrmage@gmail.com>
2016-12-18 14:13:46 +0100

    [macOS] Take Qt prefix path from Homebrew for CI resilience
    Recent CI failure was caused by path changed by Homebrew.

--- CI/before_script.osx.sh
+++ CI/before_script.osx.sh
@@ -4,7 +4,7 @@ export CXX=clang++
 export CC=clang
 
 DEPENDENCIES_ROOT="/private/tmp/openmw-deps/openmw-deps"
-QT_PATH="/usr/local/opt/qt55"
+QT_PATH=`brew --prefix qt@5.5`
 
 mkdir build
 cd build
