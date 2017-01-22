86d8b0630fb98466712af59a84c1359bebf1be51
Koncord <stas5978@gmail.com>
2017-01-22 15:07:42 +0800

    Do not allow override ServerModel

--- apps/browser/ServerModel.hpp
+++ apps/browser/ServerModel.hpp
@@ -31,16 +31,16 @@ class ServerModel: public QAbstractTableModel
 public:
     explicit ServerModel(QObject *parent = 0);
     ~ServerModel();
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    int columnCount(const QModelIndex &parent) const;
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_FINAL;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_FINAL;
+    int columnCount(const QModelIndex &parent) const Q_DECL_FINAL;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) Q_DECL_FINAL;
 
-    bool insertRows(int row, int count, const QModelIndex &index = QModelIndex());
-    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex());
+    bool insertRows(int row, int count, const QModelIndex &index = QModelIndex()) Q_DECL_FINAL;
+    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_FINAL;
 
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const Q_DECL_FINAL;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const Q_DECL_FINAL;
 
 
 public:
