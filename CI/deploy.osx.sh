376448094bd30a6cab7dbf5f7fb94593fc5ff067
Nikolay Kasyanov <corrmage@gmail.com>
2017-03-05 17:47:21 +0100

    Use short commit for nightly filename

--- CI/deploy.osx.sh
+++ CI/deploy.osx.sh
@@ -3,6 +3,7 @@
 cd build
 
 DATE=`date +'%d%m%Y'`
-TARGET_FILENAME="openmw-${DATE}-${TRAVIS_COMMIT}.dmg"
+SHORT_COMMIT=`git rev-parse --short ${TRAVIS_COMMIT}`
+TARGET_FILENAME="openmw-${DATE}-${SHORT_COMMIT}.dmg"
 
 curl --ftp-create-dirs -T *.dmg -u $OSX_FTP_USER:$OSX_FTP_PASSWORD "ftp://s3.mydevil.net:21/nightly/${TARGET_FILENAME}"
