a7b820c956a6057576dac1ffbcd2adccc947c064
Nikolay Kasyanov <corrmage@gmail.com>
2017-03-05 17:52:53 +0100

    Make curl use FTPS

--- CI/deploy.osx.sh
+++ CI/deploy.osx.sh
@@ -6,4 +6,4 @@ DATE=`date +'%d%m%Y'`
 SHORT_COMMIT=`git rev-parse --short ${TRAVIS_COMMIT}`
 TARGET_FILENAME="openmw-${DATE}-${SHORT_COMMIT}.dmg"
 
-curl --ftp-create-dirs -T *.dmg -u $OSX_FTP_USER:$OSX_FTP_PASSWORD "ftp://s3.mydevil.net:21/nightly/${TARGET_FILENAME}"
+curl --ssl --ftp-create-dirs -T *.dmg -u $OSX_FTP_USER:$OSX_FTP_PASSWORD "ftp://s3.mydevil.net:21/nightly/${TARGET_FILENAME}"
