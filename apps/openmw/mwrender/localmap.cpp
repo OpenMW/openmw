ec0b74312362e2937f6b3f3f52407f3e0d4f2dd0
scrawl <scrawl@baseoftrash.de>
2017-03-09 19:44:39 +0100

    Revert "Change ordering of LocalMap nodes to make sure they are traversed before the CompositeMapRenderer."
    This reverts commit 7d72c70c93ee3c0cc3d00d37b22c339d0103cd19.

--- apps/openmw/mwrender/localmap.cpp
+++ apps/openmw/mwrender/localmap.cpp
@@ -222,7 +222,7 @@ void LocalMap::setupRenderToTexture(osg::ref_ptr<osg::Camera> camera, int x, int
     camera->attach(osg::Camera::COLOR_BUFFER, texture);
 
     camera->addChild(mSceneRoot);
-    mRoot->insertChild(0, camera);
+    mRoot->addChild(camera);
     mActiveCameras.push_back(camera);
 
     MapSegment& segment = mSegments[std::make_pair(x, y)];
