2397afc6cecf8de1bc1465cecc25c18b877394a6
David Cernat <davidcernat@gmail.com>
2017-04-17 15:12:11 +0300

    [General] Move mwmp::Attack to BaseStructs

--- components/openmw-mp/Base/BasePlayer.hpp
+++ components/openmw-mp/Base/BasePlayer.hpp
@@ -11,30 +11,13 @@
 #include <components/esm/creaturestats.hpp>
 #include <components/esm/loadclas.hpp>
 #include <components/esm/loadspel.hpp>
+
+#include <components/openmw-mp/Base/BaseStructs.hpp>
+
 #include <RakNetTypes.h>
 
 namespace mwmp
 {
-    class Attack
-    {
-    public:
-        RakNet::RakNetGUID target;
-        RakNet::RakNetGUID attacker;
-        char type; // 0 - melee, 1 - magic, 2 - throwable
-        enum TYPE
-        {
-            MELEE = 0,
-            MAGIC,
-            THROWABLE
-        };
-        std::string refId; // id of spell (e.g. "fireball")
-        char success;
-        char block;
-        float damage;
-        char pressed;
-        char knockdown;
-    };
-
     struct CurrentContainer
     {
         std::string refId;
--- components/openmw-mp/Base/BaseStructs.hpp
+++ components/openmw-mp/Base/BaseStructs.hpp
@@ -1,8 +1,30 @@
 #ifndef OPENMW_BASESTRUCTS_HPP
 #define OPENMW_BASESTRUCTS_HPP
 
+#include <RakNetTypes.h>
+
 namespace mwmp
 {
+    class Attack
+    {
+    public:
+        RakNet::RakNetGUID target;
+        RakNet::RakNetGUID attacker;
+        char type; // 0 - melee, 1 - magic, 2 - throwable
+        enum TYPE
+        {
+            MELEE = 0,
+            MAGIC,
+            THROWABLE
+        };
+        std::string refId; // id of spell (e.g. "fireball")
+        char success;
+        char block;
+        float damage;
+        char pressed;
+        char knockdown;
+    };
+
     struct Animation
     {
         std::string groupname;
