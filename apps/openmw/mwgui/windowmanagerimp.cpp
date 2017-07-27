6ab36c05399ebcefceb5bec1bffbcc56c8a71697
Andrei Kortunov <andrei.kortunov@yandex.ru>
2017-07-27 13:20:18 +0400

    Spellbuying menu improvements

--- apps/openmw/mwgui/windowmanagerimp.cpp
+++ apps/openmw/mwgui/windowmanagerimp.cpp
@@ -2001,7 +2001,7 @@ namespace MWGui
     void WindowManager::startSpellBuying(const MWWorld::Ptr &actor)
     {
         pushGuiMode(GM_SpellBuying);
-        mSpellBuyingWindow->startSpellBuying(actor);
+        mSpellBuyingWindow->startSpellBuying(actor, 0);
     }
 
     void WindowManager::startTrade(const MWWorld::Ptr &actor)
