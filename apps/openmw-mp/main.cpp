92553b8e292b911029bee04fa9a9ca1d23a6c82d
David Cernat <davidcernat@gmail.com>
2017-08-29 19:30:37 +0300

    [Server] Log startup errors by not deleting logger too early

--- apps/openmw-mp/main.cpp
+++ apps/openmw-mp/main.cpp
@@ -332,6 +332,8 @@ int main(int argc, char *argv[])
     if (code == 0)
         LOG_MESSAGE_SIMPLE(Log::LOG_INFO, "Quitting peacefully.");
 
+    LOG_QUIT();
+
     if (!variables["no-logs"].as<bool>())
     {
         // Restore cout and cerr
