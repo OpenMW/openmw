08df8f8d836bfcb2216ffbafae22c30210642bbe
David Cernat <davidcernat@gmail.com>
2017-08-31 18:45:24 +0300

    [General] Update version to 0.6.1

--- components/openmw-mp/Version.hpp
+++ components/openmw-mp/Version.hpp
@@ -5,7 +5,7 @@
 #ifndef OPENMW_VERSION_HPP
 #define OPENMW_VERSION_HPP
 
-#define TES3MP_VERSION "0.6.0"
+#define TES3MP_VERSION "0.6.1"
 #define TES3MP_PROTO_VERSION 7
 
 #define TES3MP_DEFAULT_PASSW "SuperPassword"
