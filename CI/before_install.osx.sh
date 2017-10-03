c598f1313c4233af2aec6dcb92e4924408120961
Nikolay Kasyanov <nikolaykasyanov@users.noreply.github.com>
2017-10-03 17:34:45 +0200

    [macOS, CI] Update dependencies
    The following changes are included:
    - https://github.com/OpenMW/openmw-deps-mac/pull/37
    - https://github.com/OpenMW/openmw-deps-mac/pull/38

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -6,5 +6,5 @@ brew outdated cmake || brew upgrade cmake
 brew outdated pkgconfig || brew upgrade pkgconfig
 brew install $macos_qt_formula
 
-curl https://downloads.openmw.org/osx/dependencies/openmw-deps-5e144e2.zip -o ~/openmw-deps.zip
+curl https://downloads.openmw.org/osx/dependencies/openmw-deps-c40905f.zip -o ~/openmw-deps.zip
 unzip ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
