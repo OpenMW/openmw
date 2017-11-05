f98a821482e51b2c1bbb5a6c7c43393608dee2da
Miloslav Číž <tastyfish@seznam.cz>
2017-11-05 23:46:15 +0100

    fix possible bug in aipursue

--- apps/openmw/mwmechanics/aipursue.cpp
+++ apps/openmw/mwmechanics/aipursue.cpp
@@ -55,7 +55,7 @@ bool AiPursue::execute (const MWWorld::Ptr& actor, CharacterController& characte
     float pathTolerance = 100.0;
 
     if (pathTo(actor, dest, duration, pathTolerance) &&
-        MWMechanics::distance(dest,aPos.pos[0],aPos.pos[1],aPos.pos[2]) < pathTolerance)  // check the true distance in case the target is far away in Z-direction
+        abs(dest.mZ - aPos.pos[2]) < pathTolerance)      // check the true distance in case the target is far away in Z-direction
     {
         target.getClass().activate(target,actor).get()->execute(actor); //Arrest player when reached
         return true;
