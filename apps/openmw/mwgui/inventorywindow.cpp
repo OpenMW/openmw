f0b28a66fd6537b0a99985eef99a9cc9ebe930f3
Capostrophic <Capostrophic@users.noreply.github.com>
2018-02-24 13:32:03 +0300

    Round up encumbrance value in the inventory window (Feature #1786)

--- apps/openmw/mwgui/inventorywindow.cpp
+++ apps/openmw/mwgui/inventorywindow.cpp
@@ -1,5 +1,6 @@
 #include "inventorywindow.hpp"
 
+#include <cmath>
 #include <stdexcept>
 
 #include <MyGUI_Window.h>
@@ -598,7 +599,10 @@ namespace MWGui
         float capacity = player.getClass().getCapacity(player);
         float encumbrance = player.getClass().getEncumbrance(player);
         mTradeModel->adjustEncumbrance(encumbrance);
-        mEncumbranceBar->setValue(static_cast<int>(encumbrance), static_cast<int>(capacity));
+        if (encumbrance != 0.f)
+            mEncumbranceBar->setValue(static_cast<int>(std::round(encumbrance+0.5)), static_cast<int>(capacity));
+        else
+            mEncumbranceBar->setValue(0, static_cast<int>(capacity));
     }
 
     void InventoryWindow::onFrame(float dt)
