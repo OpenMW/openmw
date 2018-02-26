97b607fc663afc964db8b6e69cd74516a495636c
AnyOldName3 <krizdjali+github@gmail.com>
2018-02-26 21:10:06 +0000

    Make whitespace match upstream OSG

--- components/sceneutil/mwshadowtechnique.hpp
+++ components/sceneutil/mwshadowtechnique.hpp
@@ -4,16 +4,16 @@
  */
 
 /* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2011 Robert Osfield
-*
-* This library is open source and may be redistributed and/or modified under
-* the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
-* (at your option) any later version.  The full license is in LICENSE file
-* included with this distribution, and on the openscenegraph.org website.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* OpenSceneGraph Public License for more details.
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
 */
 
 #ifndef COMPONENTS_SCENEUTIL_MWSHADOWTECHNIQUE_H
