fdcfbdbdd1ef376cba88bd2aeca0780c284087a8
Ilya Zhuravlev <whatever@xyz.is>
2018-03-24 21:26:58 -0400

    android: Update for sdl 2.0.8

--- apps/openmw/android_main.c
+++ apps/openmw/android_main.c
@@ -35,28 +35,11 @@ int Java_org_libsdl_app_SDLActivity_isMouseShown(JNIEnv *env, jclass cls, jobjec
     return SDL_ShowCursor(SDL_QUERY);
 }
 
-
-int Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls,
-        jobject obj) {
-
+int Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject obj) {
     setenv("OPENMW_DECOMPRESS_TEXTURES", "1", 1);
-
-    SDL_Android_Init(env, cls);
-
-    SDL_SetMainReady();
 
     // On Android, we use a virtual controller with guid="Virtual"
     SDL_GameControllerAddMapping("5669727475616c000000000000000000,Virtual,a:b0,b:b1,back:b15,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b16,leftshoulder:b6,leftstick:b13,lefttrigger:a5,leftx:a0,lefty:a1,rightshoulder:b7,rightstick:b14,righttrigger:a4,rightx:a2,righty:a3,start:b11,x:b3,y:b4");
 
-    /* Run the application code! */
-
-    int status;
-
-    status = main(argcData+1, argvData);
-    releaseArgv();
-    /* Do not issue an exit or the whole application will terminate instead of just the SDL thread */
-    /* exit(status); */
-
-    return status;
+    return 0;
 }
-
