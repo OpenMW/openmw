d22b2b6be84e8a4e9db655085f8a107a3dd8b1c8
Ondřej Súkup <mimi.vx@gmail.com>
2018-05-02 15:50:51 +0200

    Fix undefined behaviour in CreatureAttackRefIdAdapter::getNestedData
    bug #4409

--- apps/opencs/model/world/refidadapterimp.cpp
+++ apps/opencs/model/world/refidadapterimp.cpp
@@ -1368,13 +1368,15 @@ QVariant CSMWorld::CreatureAttackRefIdAdapter::getNestedData (const RefIdColumn 
 
     const ESM::Creature& creature = record.get();
 
-    if (subRowIndex < 0 || subRowIndex > 2 || subColIndex < 0 || subColIndex > 2)
-        throw std::runtime_error ("index out of range");
-
     if (subColIndex == 0)
         return subRowIndex + 1;
     else if (subColIndex < 3) // 1 or 2
         return creature.mData.mAttack[(subRowIndex * 2) + (subColIndex - 1)];
+
+    if (subRowIndex < 0 || subRowIndex > 2 || subColIndex < 0 || subColIndex > 2)
+        throw std::runtime_error ("index out of range");
+    
+    return 0;
 }
 
 void CSMWorld::CreatureAttackRefIdAdapter::setNestedData (const RefIdColumn *column,
