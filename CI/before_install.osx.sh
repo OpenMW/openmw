308d78e3dafab71ee8ba173e8ed7fb35a7a41e2f
Bret Curtis <psi29a@gmail.com>
2018-06-26 13:54:37 +0000

    only download via curl if file on server is different than what is currently cached on VM.

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -6,5 +6,5 @@ brew outdated cmake || brew upgrade cmake
 brew outdated pkgconfig || brew upgrade pkgconfig
 brew install qt
 
-curl https://downloads.openmw.org/osx/dependencies/openmw-deps-100d2e0.zip -o ~/openmw-deps.zip
+curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-100d2e0.zip -o ~/openmw-deps.zip
 unzip ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
