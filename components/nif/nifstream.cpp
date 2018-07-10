c921d1c7e91bd37048f33b491fadcaede007cf2c
Andrei Kortunov <andrei.kortunov@yandex.ru>
2018-07-10 13:32:52 +0400

    Refactor NifStream class (eliminate LNK4221 MSVC warning)

--- components/nif/nifstream.cpp
+++ components/nif/nifstream.cpp
@@ -4,10 +4,24 @@
 
 namespace Nif
 {
+    osg::Quat NIFStream::getQuaternion()
+    {
+        float f[4];
+        readLittleEndianBufferOfType<4, float,uint32_t>(inp, (float*)&f);
+        osg::Quat quat;
+        quat.w() = f[0];
+        quat.x() = f[1];
+        quat.y() = f[2];
+        quat.z() = f[3];
+        return quat;
+    }
 
-//Private functions
-
-
-//Public functions
-
+    Transformation NIFStream::getTrafo()
+    {
+        Transformation t;
+        t.pos = getVector3();
+        t.rotation = getMatrix3();
+        t.scale = getFloat();
+        return t;
+    }
 }
