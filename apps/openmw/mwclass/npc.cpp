23d917df9c48fd1616037ac1cade28d639480d32
Andrei Kortunov <andrei.kortunov@yandex.ru>
2018-07-26 18:06:36 +0400

    Do not use magic numbers in capacity calculations

--- apps/openmw/mwclass/npc.cpp
+++ apps/openmw/mwclass/npc.cpp
@@ -1079,7 +1079,7 @@ namespace MWClass
     {
         const MWMechanics::CreatureStats& stats = getCreatureStats (ptr);
         static const float fEncumbranceStrMult = MWBase::Environment::get().getWorld()->getStore().get<ESM::GameSetting>().find("fEncumbranceStrMult")->getFloat();
-        return stats.getAttribute(0).getModified()*fEncumbranceStrMult;
+        return stats.getAttribute(ESM::Attribute::Strength).getModified()*fEncumbranceStrMult;
     }
 
     float Npc::getEncumbrance (const MWWorld::Ptr& ptr) const
