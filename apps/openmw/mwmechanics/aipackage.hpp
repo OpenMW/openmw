0cfdf0c7b6cf4a5997d3911491152bf176b93c4e
elsid <elsid.mail@gmail.com>
2018-08-18 12:52:17 +0300

    Remove unused virtual

--- apps/openmw/mwmechanics/aipackage.hpp
+++ apps/openmw/mwmechanics/aipackage.hpp
@@ -121,7 +121,7 @@ namespace MWMechanics
             /// Check if the way to the destination is clear, taking into account actor speed
             bool checkWayIsClearForActor(const ESM::Pathgrid::Point& startPoint, const ESM::Pathgrid::Point& endPoint, const MWWorld::Ptr& actor);
 
-            virtual bool doesPathNeedRecalc(const ESM::Pathgrid::Point& newDest, const MWWorld::CellStore* currentCell);
+            bool doesPathNeedRecalc(const ESM::Pathgrid::Point& newDest, const MWWorld::CellStore* currentCell);
 
             void evadeObstacles(const MWWorld::Ptr& actor, float duration, const ESM::Position& pos);
             void openDoors(const MWWorld::Ptr& actor);
