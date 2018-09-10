dcc1a122f79161d926148b1526e9b8923432097e
Andrei Kortunov <andrei.kortunov@yandex.ru>
2018-09-10 15:18:07 +0400

    Use drag and drop for ActionTake when InventoryWindow is active (bug #4543)

--- apps/openmw/mwworld/actiontake.cpp
+++ apps/openmw/mwworld/actiontake.cpp
@@ -5,6 +5,8 @@
 #include "../mwbase/windowmanager.hpp"
 #include "../mwbase/mechanicsmanager.hpp"
 
+#include "../mwgui/inventorywindow.hpp"
+
 #include "class.hpp"
 #include "containerstore.hpp"
 
@@ -14,6 +16,17 @@ namespace MWWorld
 
     void ActionTake::executeImp (const Ptr& actor)
     {
+        // When in GUI mode, we should use drag and drop
+        if (actor == MWBase::Environment::get().getWorld()->getPlayerPtr())
+        {
+            MWGui::GuiMode mode = MWBase::Environment::get().getWindowManager()->getMode();
+            if (mode == MWGui::GM_Inventory || mode == MWGui::GM_Container)
+            {
+                MWBase::Environment::get().getWindowManager()->getInventoryWindow()->pickUpObject(getTarget());
+                return;
+            }
+        }
+
         MWBase::Environment::get().getMechanicsManager()->itemTaken(
                     actor, getTarget(), MWWorld::Ptr(), getTarget().getRefData().getCount());
         MWWorld::Ptr newitem = *actor.getClass().getContainerStore (actor).add (getTarget(), getTarget().getRefData().getCount(), actor);
