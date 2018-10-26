f62ca24356a16c7148bef07bdaf2ba19464f2236
Nikolay Kasyanov <corrmage@gmail.com>
2018-10-26 19:33:04 +0200

    [macOS, CI] Use dependencies with downgraded MyGUI, fixing #4665

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -6,5 +6,5 @@ brew outdated cmake || brew upgrade cmake
 brew outdated pkgconfig || brew upgrade pkgconfig
 brew install qt
 
-curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-100d2e0.zip -o ~/openmw-deps.zip
+curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-4eec887.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
