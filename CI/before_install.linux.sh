8c21893d06c4d7ac84444faf372d2ef227793136
Bret Curtis <psi29a@gmail.com>
2018-11-06 21:44:56 +0100

    for Ubuntu, use gcc5 instead of 4.8

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
-sudo ln -s /usr/bin/clang-3.6 /usr/local/bin/clang
-sudo ln -s /usr/bin/clang++-3.6 /usr/local/bin/clang++
+sudo ln -sf /usr/bin/clang-3.6 /usr/local/bin/clang
+sudo ln -sf /usr/bin/clang++-3.6 /usr/local/bin/clang++
+sudo ln -sf /usr/bin/gcc-5 /usr/bin/gcc
+sudo ln -sf /usr/bin/g++-5 /usr/bin/g++
