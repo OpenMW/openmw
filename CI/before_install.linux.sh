d8e7ee193ee449148fa40df95bc1a1d61a3f7bf3
Bret Curtis <psi29a@gmail.com>
2018-11-07 08:59:08 +0100

    replace precise with trusty llvm toolchain, because we have been using trusty for awhile now

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -e
-sudo ln -sf /usr/bin/clang-3.6 /usr/local/bin/clang
-sudo ln -sf /usr/bin/clang++-3.6 /usr/local/bin/clang++
-
-eval "${MATRIX_EVAL}"
+sudo ln -sf /usr/bin/clang-6.0 /usr/local/bin/clang
+sudo ln -sf /usr/bin/clang++-6.0 /usr/local/bin/clang++
