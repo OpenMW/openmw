f858b0c27e2b2512e93f868079d6f3fb2a49fdbc
Bret Curtis <psi29a@gmail.com>
2018-11-07 11:33:55 +0100

    travis.yml cleanup, have output of scripts go to stdout, make search for substring a regex
    use double []
    
    fix missing ,
    
    use bash to use regex
    
    black spaces matter

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,3 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -ex
+
 sudo ln -sf /usr/bin/clang-6.0 /usr/local/bin/clang
 sudo ln -sf /usr/bin/clang++-6.0 /usr/local/bin/clang++
--- CI/before_script.linux.sh
+++ CI/before_script.linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -ex
 
 free -m
 
@@ -8,8 +8,8 @@ GOOGLETEST_DIR="$(pwd)/googletest/build"
 mkdir build
 cd build
 export CODE_COVERAGE=1
-if [ "${CC}" = "clang" ]; then export CODE_COVERAGE=0; fi
-${ANALYZE}cmake \
+if [[ "${CC}" =~ "clang" ]]; then export CODE_COVERAGE=0; fi
+${ANALYZE} cmake \
     -DBUILD_WITH_CODE_COVERAGE=${CODE_COVERAGE} \
     -DBUILD_UNITTESTS=1 \
     -DCMAKE_INSTALL_PREFIX=/usr \
