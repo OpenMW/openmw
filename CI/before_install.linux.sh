ca3e4c3a62023467dd6c7f5590dbe18be213496c
Bret Curtis <psi29a@gmail.com>
2018-11-08 12:19:41 +0100

    what does clang7 have to say?

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-sudo ln -sf /usr/bin/clang-6.0 /usr/local/bin/clang
-sudo ln -sf /usr/bin/clang++-6.0 /usr/local/bin/clang++
+sudo ln -sf /usr/bin/clang-7.0 /usr/local/bin/clang
+sudo ln -sf /usr/bin/clang++-7.0 /usr/local/bin/clang++
