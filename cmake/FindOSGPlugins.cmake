9067731a96cb83702b25f69677afba1d209cb9d4
AnyOldName3 <krizdjali+github@gmail.com>
2018-12-13 02:08:35 +0000

    Adapt to CMake 3.13's new meaning of OSGDB_LIBRARY (i.e. that it can now be a list) while allowing for the possibility that the found libraries may be in different directories when debug and optimised versions exist.

--- cmake/FindOSGPlugins.cmake
+++ cmake/FindOSGPlugins.cmake
@@ -1,5 +1,6 @@
 # This module accepts the following env variable
 #  OSGPlugins_LIB_DIR - <OpenSceneGraph>/lib/osgPlugins-<X.X.X> , path to search plugins
+#  OSGPlugins_DONT_FIND_DEPENDENCIES - Set to skip also finding png, zlib and jpeg
 #
 # Once done this will define
 #  OSGPlugins_FOUND         - System has the all required components.
@@ -27,10 +28,10 @@ foreach(_library ${OSGPlugins_FIND_COMPONENTS})
     set(_component OSGPlugins_${_library})
 
     set(${_library_uc}_DIR ${OSGPlugins_LIB_DIR}) # to help function osg_find_library
-    set(_saved_lib_prefix ${CMAKE_FIND_LIBRARY_PREFIXES}) # save CMAKE_FIND_LIBRARY_PREFIXES
-    set(CMAKE_FIND_LIBRARY_PREFIXES "") # search libraries with no prefix
+    #set(_saved_lib_prefix ${CMAKE_FIND_LIBRARY_PREFIXES}) # save CMAKE_FIND_LIBRARY_PREFIXES
+    #set(CMAKE_FIND_LIBRARY_PREFIXES "") # search libraries with no prefix
     osg_find_library(${_library_uc} ${_library}) # find it into ${_library_uc}_LIBRARIES
-    set(CMAKE_FIND_LIBRARY_PREFIXES ${_saved_lib_prefix}) # restore prefix
+    #set(CMAKE_FIND_LIBRARY_PREFIXES ${_saved_lib_prefix}) # restore prefix
 
     if (${_library_uc}_LIBRARIES)
         set(${_component}_LIBRARY ${${_library_uc}_LIBRARIES}) # fake as if we call find_library
@@ -41,10 +42,12 @@ foreach(_library ${OSGPlugins_FIND_COMPONENTS})
     list(APPEND OSGPlugins_PROCESS_LIBS ${_component}_LIBRARY)
 endforeach()
 
-foreach(_dependency PNG ZLIB JPEG) # needed by osgdb_png or osgdb_jpeg
-    libfind_package(OSGPlugins ${_dependency})
-    set(${_dependency}_LIBRARY_OPTS ${_dependency}_LIBRARY)
-    #list(APPEND OSGPlugins_PROCESS_LIBS ${_dependency}_LIBRARY)
-endforeach()
+if(NOT DEFINED OSGPlugins_DONT_FIND_DEPENDENCIES)
+    foreach(_dependency PNG ZLIB JPEG) # needed by osgdb_png or osgdb_jpeg
+        libfind_package(OSGPlugins ${_dependency})
+        set(${_dependency}_LIBRARY_OPTS ${_dependency}_LIBRARY)
+        #list(APPEND OSGPlugins_PROCESS_LIBS ${_dependency}_LIBRARY)
+    endforeach()
+endif()
 
 libfind_process(OSGPlugins)
