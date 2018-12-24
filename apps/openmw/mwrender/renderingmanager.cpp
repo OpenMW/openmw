OpenMW/openmw

commit 2e1786f7223937a81d3a93a9d9af20a183e55569

fix screenshot at saving introducing the unshadowed uniform(unlit and unshadowed) and shadowed stateset (to reuse computed shadowmap)

apps/openmw/mwrender/renderingmanager.cpp
@@ -231,7 +231,8 @@ namespace MWRender
 
         osg::ref_ptr<osg::Group> scenegroup=new osg::Group;
         osg::ref_ptr<SceneUtil::LightManager> sceneRoot = new SceneUtil::LightManager;
-        mSceneRoot = scenegroup;
+        mSceneRoot = sceneRoot;
+        mShadoweSceneRoot = scenegroup;
         sceneRoot->setLightingMask(Mask_Lighting);
 
         mRootNode->addChild(sceneRoot);
@@ -268,7 +269,6 @@ namespace MWRender
         int indoorShadowCastingTraversalMask =   Mask_Actor| Mask_Player ;
 
         mShadowManager.reset(new ShadowManager( sceneRoot, scenegroup , shadowCastingTraversalMask, indoorShadowCastingTraversalMask, mResourceSystem->getSceneManager()->getShaderManager()));
-
         mNavMesh.reset(new NavMesh(mRootNode, Settings::Manager::getBool("enable nav mesh render", "Navigator")));
         mActorsPaths.reset(new ActorsPaths(mRootNode, Settings::Manager::getBool("enable agents paths render", "Navigator")));
         mPathgrid.reset(new Pathgrid(mRootNode));
@@ -947,8 +947,21 @@ namespace MWRender
         rttCamera->setViewMatrix(mViewer->getCamera()->getViewMatrix() * cameraTransform);
 
         rttCamera->setUpdateCallback(new NoTraverseCallback);
-        rttCamera->addChild(mSceneRoot);
 
+        ///remove shadowedscene from scenegraph to avoid infinite loop
+        osg::ref_ptr<osg::Group> sceneclone=(osg::Group* )mSceneRoot-> clone(osg::CopyOp::SHALLOW_COPY);
+        sceneclone->removeChild(mShadoweSceneRoot->getParent(0));
+        sceneclone->addChild(mShadoweSceneRoot);
+
+        osg::ref_ptr<osg::StateSet> stateset=new osg::StateSet();
+        stateset->merge(*mSceneRoot->getOrCreateStateSet());
+        if(mShadowManager->isShadowEnable())
+            stateset->merge(*mShadowManager->getShadowedStateSet());
+        else
+            stateset->addUniform(ShadowManager::getUnshadowedUniform());
+        sceneclone->setStateSet(stateset);
+
+        rttCamera->addChild(sceneclone);
         rttCamera->addChild(mWater->getReflectionCamera());
         rttCamera->addChild(mWater->getRefractionCamera());
 
@@ -1124,7 +1137,7 @@ namespace MWRender
             mPlayerNode = new SceneUtil::PositionAttitudeTransform;
             mPlayerNode->setNodeMask(Mask_Player);
             mPlayerNode->setName("Player Root");
-            mSceneRoot->addChild(mPlayerNode);
+            mShadoweSceneRoot->addChild(mPlayerNode);
         }
 
         mPlayerNode->setUserDataContainer(new osg::DefaultUserDataContainer);

apps/openmw/mwrender/characterpreview.cpp
@@ -24,6 +24,7 @@
 
 #include "../mwmechanics/actorutil.hpp"
 
+#include "shadow.hpp"
 #include "npcanimation.hpp"
 #include "vismask.hpp"
 
@@ -151,6 +152,7 @@ namespace MWRender
         defaultMat->setDiffuse(osg::Material::FRONT_AND_BACK, osg::Vec4f(1,1,1,1));
         defaultMat->setSpecular(osg::Material::FRONT_AND_BACK, osg::Vec4f(0.f, 0.f, 0.f, 0.f));
         stateset->setAttribute(defaultMat);
+        stateset->addUniform(ShadowManager::getUnshadowedUniform());
 
         // assign large value to effectively turn off fog
         // shaders don't respect glDisable(GL_FOG)

apps/openmw/mwrender/localmap.cpp
@@ -25,6 +25,7 @@
 #include "../mwworld/cellstore.hpp"
 
 #include "vismask.hpp"
+#include "shadow.hpp"
 
 namespace
 {
@@ -183,7 +184,6 @@ osg::ref_ptr<osg::Camera> LocalMap::createOrthographicCamera(float x, float y, f
     osg::ref_ptr<osg::StateSet> stateset = new osg::StateSet;
     stateset->setAttribute(new osg::PolygonMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::FILL), osg::StateAttribute::OVERRIDE);
 
-    stateset->addUniform(new osg::Uniform("ambientBias",osg::Vec2(1,1)));
     // assign large value to effectively turn off fog
     // shaders don't respect glDisable(GL_FOG)
     osg::ref_ptr<osg::Fog> fog (new osg::Fog);
@@ -194,6 +194,7 @@ osg::ref_ptr<osg::Camera> LocalMap::createOrthographicCamera(float x, float y, f
     osg::ref_ptr<osg::LightModel> lightmodel = new osg::LightModel;
     lightmodel->setAmbientIntensity(osg::Vec4(0.3f, 0.3f, 0.3f, 1.f));
     stateset->setAttributeAndModes(lightmodel, osg::StateAttribute::ON|osg::StateAttribute::OVERRIDE);
+    stateset->addUniform(ShadowManager::getUnshadowedUniform());
 
     osg::ref_ptr<osg::Light> light = new osg::Light;
     light->setPosition(osg::Vec4(-0.3f, -0.3f, 0.7f, 0.f));

apps/openmw/mwrender/renderingmanager.hpp
@@ -248,6 +248,8 @@ namespace MWRender
         osg::ref_ptr<osgViewer::Viewer> mViewer;
         osg::ref_ptr<osg::Group> mRootNode;
         osg::ref_ptr<osg::Group> mSceneRoot;
+        osg::ref_ptr<osg::Group> mShadoweSceneRoot;
+
         Resource::ResourceSystem* mResourceSystem;
 
         osg::ref_ptr<SceneUtil::WorkQueue> mWorkQueue;

apps/openmw/mwrender/shadow.cpp
@@ -1,5 +1,5 @@
 #include "shadow.hpp"
-
+#include "parallelsplitshadowmap.hpp"
 #include <osgShadow/ShadowSettings>
 #include <osgShadow/ViewDependentShadowMap>
 
@@ -9,6 +9,13 @@ using namespace osgShadow;
 
 namespace MWRender
 {
+
+/*static */osg::ref_ptr<osg::Uniform> ShadowManager::_unshadowedAmbientBias=0;
+/*static */osg::Uniform* ShadowManager::getUnshadowedUniform()
+{
+    if(!_unshadowedAmbientBias.valid()) _unshadowedAmbientBias = new osg::Uniform("ambientBias",osg::Vec2(1,1));
+    return _unshadowedAmbientBias ;
+}
 
 Shader::ShaderManager::DefineMap ShadowManager::getShadowDefines()
 {
@@ -30,15 +37,10 @@ Shader::ShaderManager::DefineMap getShadowsDisabledDefines()
     return definesWithShadows;
 }
 
+/*useless (for the moment current pssm don't use shadowsettings for the moment)*/
 void ShadowManager::setupShadowSettings()
 {
-    /*useless (for the moment current pssm don't use shadowsettings for the moment)*/
     mShadowSettings->setLightNum(0);
-    mShadowSettings->setReceivesShadowTraversalMask(~0u);
-
-    int numberOfShadowMapsPerLight = Settings::Manager::getInt("number of shadow maps", "Shadows");
-    mShadowSettings->setNumShadowMapsPerLight(numberOfShadowMapsPerLight);
-
 }
 
 ShadowManager::ShadowManager(osg::Group* parent, osg::Group* sceneRoot,

apps/openmw/mwrender/shadow.hpp
@@ -17,11 +17,22 @@ namespace MWRender
                       Shader::ShaderManager &shaderManager);
         ~ShadowManager() = default;
 
+        /// @return true if Shadowing is enabled
+        inline bool isShadowEnable() const { return mEnableShadows;}
+
+        /// @return current shadowStateSet (to reuse shadowmap result map&texgen)
+        osg::StateSet * getShadowedStateSet(){ return mOutdoorShadowTechnique->getShadowedStateSet(); }
+
+        /// @return an uniform to nullify shadow
+        static osg::Uniform* getUnshadowedUniform();
+
         void enableIndoorMode();
 
         void enableOutdoorMode();
 
+        osgShadow::ShadowSettings* getShadowSettings(){return mShadowSettings;}
     protected:
+        static osg::ref_ptr<osg::Uniform> _unshadowedAmbientBias;
         void setupShadowSettings();
         Shader::ShaderManager::DefineMap getShadowDefines();
 

commit e9a47421c22a40955292322f8479fa3fbf07614d

fix a bug introduced in 2e1786f7223937a81d3a93a9d9af20a183e55569

apps/openmw/mwrender/renderingmanager.cpp
@@ -952,6 +952,8 @@ namespace MWRender
         ///remove shadowedscene from scenegraph to avoid infinite loop
         osg::ref_ptr<osg::Group> sceneclone=(osg::Group* )mSceneRoot-> clone(osg::CopyOp::SHALLOW_COPY);
         sceneclone->removeChild(mShadoweSceneRoot->getParent(0));
+        osgUtil::UpdateVisitor fakecv;
+        mStateUpdater->operator ()(sceneclone,&fakecv);
         sceneclone->addChild(mShadoweSceneRoot);
 
         osg::ref_ptr<osg::StateSet> stateset=new osg::StateSet();
@@ -962,6 +964,7 @@ namespace MWRender
             stateset->addUniform(ShadowManager::getUnshadowedUniform());
         sceneclone->setStateSet(stateset);
 
+
         rttCamera->addChild(sceneclone);
         rttCamera->addChild(mWater->getReflectionCamera());
         rttCamera->addChild(mWater->getRefractionCamera());
@@ -971,6 +974,8 @@ namespace MWRender
         rttCamera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         renderCameraToImage(rttCamera.get(),image,w,h);
+
+
     }
 
     osg::Vec4f RenderingManager::getScreenBounds(const MWWorld::Ptr& ptr)