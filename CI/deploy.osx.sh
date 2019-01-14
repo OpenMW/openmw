8e8f04fb52da7f0425f889020a07c6ebdc732c82
Nikolay Kasyanov <corrmage@gmail.com>
2019-01-14 19:47:08 +0100

    [macOS] Deploy using scp

--- CI/deploy.osx.sh
+++ CI/deploy.osx.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo "$OSX_DEPLOY_KEY" > ~/.ssh/openmw_deploy
+
 cd build
 
 DATE=`date +'%d%m%Y'`
 SHORT_COMMIT=`git rev-parse --short ${TRAVIS_COMMIT}`
 TARGET_FILENAME="OpenMW-${DATE}-${SHORT_COMMIT}.dmg"
 
-if ! curl --ssl -u $OSX_FTP_USER:$OSX_FTP_PASSWORD "${OSX_FTP_URL}" --silent | grep $SHORT_COMMIT > /dev/null; then
-    curl --ssl --ftp-create-dirs -T *.dmg -u $OSX_FTP_USER:$OSX_FTP_PASSWORD "${OSX_FTP_URL}${TARGET_FILENAME}"
+if ! ssh -p "$OSX_DEPLOY_PORT" -i ~/.ssh/openmw_deploy "$OSX_DEPLOY_HOST" sh -c "ls ~/nightly" | grep $SHORT_COMMIT > /dev/null; then
+    scp -P "$OSX_DEPLOY_PORT" -i ~/.ssh/openmw_deploy *.dmg "$OSX_DEPLOY_HOST:~/nightly"
 fi
