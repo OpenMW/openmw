12bbecacfd2293729b0d34aeb3f69c6eb792247f
Vladimir Panteleev <git@thecybershadow.net>
2019-03-04 16:23:57 +0000

    Reuse a previously-saved value

--- apps/openmw/mwclass/container.cpp
+++ apps/openmw/mwclass/container.cpp
@@ -240,8 +240,8 @@ namespace MWClass
         std::string text;
         int lockLevel = ptr.getCellRef().getLockLevel();
         if (lockLevel > 0 && lockLevel != ESM::UnbreakableLock)
-            text += "\n#{sLockLevel}: " + MWGui::ToolTips::toString(ptr.getCellRef().getLockLevel());
-        else if (ptr.getCellRef().getLockLevel() < 0)
+            text += "\n#{sLockLevel}: " + MWGui::ToolTips::toString(lockLevel);
+        else if (lockLevel < 0)
             text += "\n#{sUnlocked}";
         if (ptr.getCellRef().getTrap() != "")
             text += "\n#{sTrapped}";
