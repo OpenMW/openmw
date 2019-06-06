e9bef8d8a19d4528a075d00568e2981f4c68fc2a
fgsfds <pvt.fgsfds@gmail.com>
2019-06-06 05:42:18 +0300

    switch: unswap A/B and X/Y via controller mapping

--- apps/openmw/switch_startup.cpp
+++ apps/openmw/switch_startup.cpp
@@ -1,5 +1,6 @@
 #include <switch.h>
 #include <stdarg.h>
+#include <SDL.h>
 
 #include <components/files/configurationmanager.hpp>
 #include <components/debug/debuglog.hpp>
@@ -114,6 +115,16 @@ void Switch::startup()
     setenv("OPENMW_OPTIMIZE", "MERGE_GEOMETRY", 1);
     setenv("OPENMW_DONT_PRECOMPILE", "1", 1);
     setenv("OPENMW_DECOMPRESS_TEXTURES", "1", 1);
+
+    // swap A, B and X, Y to correct positions
+    SDL_GameControllerAddMapping(
+        "53776974636820436F6E74726F6C6C65,Switch Controller,"
+        "a:b0,b:b1,back:b11,"
+        "dpdown:b15,dpleft:b12,dpright:b14,dpup:b13,"
+        "leftshoulder:b6,leftstick:b4,lefttrigger:b8,leftx:a0,lefty:a1,"
+        "rightshoulder:b7,rightstick:b5,righttrigger:b9,rightx:a2,righty:a3,"
+        "start:b10,x:b2,y:b3"
+    );
 
     // unlocked in Switch::shutdown()
     appletLockExit();
