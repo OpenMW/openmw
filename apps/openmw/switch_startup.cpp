2674b4ea6b6c135c66326206c5889fa8b1e30e0d
terrabyte25 <23338617+terabyte25@users.noreply.github.com>
2019-06-11 18:25:35 -0500

    switch: return blank by default for getUsername, and fix indentation

--- apps/openmw/switch_startup.cpp
+++ apps/openmw/switch_startup.cpp
@@ -30,170 +30,169 @@ extern "C" void userAppExit(void)
 
 namespace Switch
 {
+    std::string username = "";
 
-std::string username = "global";
-
-void readUsername()
-{
-    // get the name of the current user
-    auto rc = accountInitialize();
-    if (R_FAILED(rc))
-        return;
-
-    u128 userId = 0;
-    bool accountSelected = 0;
-
-    rc = accountGetActiveUser(&userId, &accountSelected);
-
-    if (R_SUCCEEDED(rc) && accountSelected)
+    void readUsername()
     {
-        AccountProfile profile;
-        rc = accountGetProfile(&profile, userId);
+        // get the name of the current user
+        auto rc = accountInitialize();
+        if (R_FAILED(rc))
+            return;
 
-        if (R_SUCCEEDED(rc))
+        u128 userId = 0;
+        bool accountSelected = 0;
+
+        rc = accountGetActiveUser(&userId, &accountSelected);
+
+        if (R_SUCCEEDED(rc) && accountSelected)
         {
-            AccountProfileBase pb;
-            rc = accountProfileGet(&profile, nullptr, &pb);
+            AccountProfile profile;
+            rc = accountGetProfile(&profile, userId);
 
             if (R_SUCCEEDED(rc))
             {
-                // HACK: check if the username is printable ascii
-                // otherwise throw it out for the time being, since fs seems
-                // allergic to certain UTF-8 characters 
-                bool clean = true;
-                for (size_t i = 0; i < sizeof(pb.username) && pb.username[i]; ++i)
+                AccountProfileBase pb;
+                rc = accountProfileGet(&profile, nullptr, &pb);
+
+                if (R_SUCCEEDED(rc))
                 {
-                    if (!std::isprint(pb.username[i]))
+                    // HACK: check if the username is printable ascii
+                    // otherwise throw it out for the time being, since fs seems
+                    // allergic to certain UTF-8 characters 
+                    bool clean = true;
+                    for (size_t i = 0; i < sizeof(pb.username) && pb.username[i]; ++i)
                     {
-                        clean = false;
-                        break;
+                        if (!std::isprint(pb.username[i]))
+                        {
+                            clean = false;
+                            break;
+                        }
                     }
+
+                    if (clean)
+                        username = std::string(pb.username);
                 }
 
-                if (clean)
-                    username = std::string(pb.username);
+                accountProfileClose(&profile);
             }
-
-            accountProfileClose(&profile);
         }
+
+        accountExit();
     }
 
-    accountExit();
-}
-
-void fatal(const char *fmt, ...)
-{
-    FILE *f = fopen("fatal.log", "w");
-
-    if (f)
+    void fatal(const char *fmt, ...)
     {
-        fprintf(f, "FATAL ERROR:\n");
-        va_list args;
-        va_start(args, fmt);
-        vfprintf(f, fmt, args);
-        va_end(args);
-        fclose(f);
+        FILE *f = fopen("fatal.log", "w");
+
+        if (f)
+        {
+            fprintf(f, "FATAL ERROR:\n");
+            va_list args;
+            va_start(args, fmt);
+            vfprintf(f, fmt, args);
+            va_end(args);
+            fclose(f);
+        }
+
+        Log(Debug::Error) << "FATAL ERROR! Check fatal.log.";
+        exit(1);
     }
 
-    Log(Debug::Error) << "FATAL ERROR! Check fatal.log.";
-    exit(1);
-}
+    void importIni(Files::ConfigurationManager& cfgMgr)
+    {
+        // if the cfg already exists, we don't need to perform conversion
+        auto cfgPath = cfgMgr.getUserConfigPath() / "openmw.cfg";
+        if (boost::filesystem::exists(cfgPath))
+            return;
 
-void importIni(Files::ConfigurationManager& cfgMgr)
-{
-    // if the cfg already exists, we don't need to perform conversion
-    auto cfgPath = cfgMgr.getUserConfigPath() / "openmw.cfg";
-    if (boost::filesystem::exists(cfgPath))
-        return;
+        // if the ini does not exist, we have nothing to convert
+        auto dataPath = cfgMgr.getGlobalDataPath();
+        auto iniPath = dataPath / "Morrowind.ini";
+        if (!boost::filesystem::exists(iniPath))
+            fatal(
+                "Could not find `%s` or `%s`.\n"
+                "Either manually place your data files into `%s` and your `openmw.cfg` into `%s`,\n"
+                "or copy the `Data Files` directory and `Morrowind.ini` from your TES3 installation\n"
+                "into `%s`.\n",
+                cfgPath.c_str(),
+                iniPath.c_str(),
+                dataPath.c_str(),
+                cfgMgr.getUserConfigPath().c_str(),
+                dataPath.c_str()
+            );
 
-    // if the ini does not exist, we have nothing to convert
-    auto dataPath = cfgMgr.getGlobalDataPath();
-    auto iniPath = dataPath / "Morrowind.ini";
-    if (!boost::filesystem::exists(iniPath))
-        fatal(
-            "Could not find `%s` or `%s`.\n"
-            "Either manually place your data files into `%s` and your `openmw.cfg` into `%s`,\n"
-            "or copy the `Data Files` directory and `Morrowind.ini` from your TES3 installation\n"
-            "into `%s`.\n",
-            cfgPath.c_str(),
-            iniPath.c_str(),
-            dataPath.c_str(),
-            cfgMgr.getUserConfigPath().c_str(),
-            dataPath.c_str()
+        Log(Debug::Info) << "Found Morrowind.ini, converting.";
+
+        // perform ini => cfg conversion and save the cfg
+        try
+        {
+            MwIniImporter importer;
+            // TODO: figure out how to detect this
+            importer.setInputEncoding(ToUTF8::WINDOWS_1252);
+
+            MwIniImporter::multistrmap ini = importer.loadIniFile(iniPath);
+            MwIniImporter::multistrmap cfg;
+
+            // add a default searchpath for the importer just in case
+            cfg.insert( std::make_pair( "data", std::vector<std::string>{"\"" + dataPath.string() + "\""} ) );
+
+            importer.merge(cfg, ini);
+            importer.mergeFallback(cfg, ini);
+            importer.importGameFiles(cfg, ini, iniPath);
+            importer.importArchives(cfg, ini);
+
+            // add the Data Files folder if it exists
+            auto dataDir = dataPath / "Data Files";
+            if (boost::filesystem::is_directory(dataDir))
+                cfg["data"].push_back("\"" + dataDir.string() + "\"");
+
+            boost::filesystem::ofstream file(cfgPath);
+            importer.writeToFile(file, cfg);
+        }
+        catch (std::exception& e)
+        {
+            fatal("While converting INI file:\n%s\n", e.what());
+        }
+
+        Log(Debug::Info) << "INI conversion done.";
+    }
+
+    void startup()
+    {
+        // some env vars for optimization purposes
+        setenv("__GL_THREADED_OPTIMIZATIONS", "1", 1);
+        setenv("__GL_ALLOW_UNOFFICIAL_PROTOCOL", "0", 1);
+        setenv("__GL_NextGenCompiler", "0", 1);
+        setenv("MESA_NO_ERROR", "1", 1);
+        setenv("OPENMW_OPTIMIZE", "MERGE_GEOMETRY", 1);
+        setenv("OPENMW_DONT_PRECOMPILE", "1", 1);
+        setenv("OPENMW_DECOMPRESS_TEXTURES", "1", 1);
+
+        // swap A, B and X, Y to correct positions
+        SDL_GameControllerAddMapping(
+            "53776974636820436F6E74726F6C6C65,Switch Controller,"
+            "a:b0,b:b1,back:b11,"
+            "dpdown:b15,dpleft:b12,dpright:b14,dpup:b13,"
+            "leftshoulder:b6,leftstick:b4,lefttrigger:b8,leftx:a0,lefty:a1,"
+            "rightshoulder:b7,rightstick:b5,righttrigger:b9,rightx:a2,righty:a3,"
+            "start:b10,x:b2,y:b3"
         );
 
-    Log(Debug::Info) << "Found Morrowind.ini, converting.";
+        // get and save the username for getUserDataPath
+        readUsername();
 
-    // perform ini => cfg conversion and save the cfg
-    try
-    {
-        MwIniImporter importer;
-        // TODO: figure out how to detect this
-        importer.setInputEncoding(ToUTF8::WINDOWS_1252);
-
-        MwIniImporter::multistrmap ini = importer.loadIniFile(iniPath);
-        MwIniImporter::multistrmap cfg;
-
-        // add a default searchpath for the importer just in case
-        cfg.insert( std::make_pair( "data", std::vector<std::string>{"\"" + dataPath.string() + "\""} ) );
-
-        importer.merge(cfg, ini);
-        importer.mergeFallback(cfg, ini);
-        importer.importGameFiles(cfg, ini, iniPath);
-        importer.importArchives(cfg, ini);
-
-        // add the Data Files folder if it exists
-        auto dataDir = dataPath / "Data Files";
-        if (boost::filesystem::is_directory(dataDir))
-            cfg["data"].push_back("\"" + dataDir.string() + "\"");
-
-        boost::filesystem::ofstream file(cfgPath);
-        importer.writeToFile(file, cfg);
-    }
-    catch (std::exception& e)
-    {
-        fatal("While converting INI file:\n%s\n", e.what());
+        // unlocked in shutdown()
+        appletLockExit();
     }
 
-    Log(Debug::Info) << "INI conversion done.";
-}
+    std::string getUsername()
+    {
+        return username;
+    }
 
-void startup()
-{
-    // some env vars for optimization purposes
-    setenv("__GL_THREADED_OPTIMIZATIONS", "1", 1);
-    setenv("__GL_ALLOW_UNOFFICIAL_PROTOCOL", "0", 1);
-    setenv("__GL_NextGenCompiler", "0", 1);
-    setenv("MESA_NO_ERROR", "1", 1);
-    setenv("OPENMW_OPTIMIZE", "MERGE_GEOMETRY", 1);
-    setenv("OPENMW_DONT_PRECOMPILE", "1", 1);
-    setenv("OPENMW_DECOMPRESS_TEXTURES", "1", 1);
-
-    // swap A, B and X, Y to correct positions
-    SDL_GameControllerAddMapping(
-        "53776974636820436F6E74726F6C6C65,Switch Controller,"
-        "a:b0,b:b1,back:b11,"
-        "dpdown:b15,dpleft:b12,dpright:b14,dpup:b13,"
-        "leftshoulder:b6,leftstick:b4,lefttrigger:b8,leftx:a0,lefty:a1,"
-        "rightshoulder:b7,rightstick:b5,righttrigger:b9,rightx:a2,righty:a3,"
-        "start:b10,x:b2,y:b3"
-    );
-
-    // get and save the username for getUserDataPath
-    readUsername();
-
-    // unlocked in shutdown()
-    appletLockExit();
-}
-
-std::string getUsername()
-{
-    return username;
-}
-
-void shutdown()
-{
-    appletUnlockExit();
-}
+    void shutdown()
+    {
+        appletUnlockExit();
+    }
 
 } /* namespace Switch */
