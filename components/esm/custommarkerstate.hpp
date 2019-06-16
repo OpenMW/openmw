698c90b3ee01bb4e8be7f8ac72ce2029bb681dae
Evil Eye <malusluminis@hotmail.com>
2019-06-16 14:41:34 +0200

    fix builds against MyGUI master

--- components/esm/custommarkerstate.hpp
+++ components/esm/custommarkerstate.hpp
@@ -16,7 +16,7 @@ struct CustomMarker
 
     std::string mNote;
 
-    bool operator == (const CustomMarker& other)
+    bool operator == (const CustomMarker& other) const
     {
         return mNote == other.mNote && mCell == other.mCell && mWorldX == other.mWorldX && mWorldY == other.mWorldY;
     }
