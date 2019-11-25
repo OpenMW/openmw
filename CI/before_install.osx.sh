5a72ebe2c9d9a0ccb23aa08e8c4e764be422911f
Bret Curtis <psi29a@gmail.com>
2019-11-25 16:17:57 +0100

    go right to bottle

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -5,11 +5,10 @@ brew outdated pkgconfig || brew upgrade pkgconfig
 brew install qt
 brew install ccache
 
-# fall back to and older cmake that likes new libboost
+# fall back to and older cmake that likes new libboost; this will break once we upgrade to MacOS 10.15
 brew unlink cmake
-cd "$(brew --repo homebrew/core)" && git log master -- Formula/cmake.rb
-cd "$(brew --repo homebrew/core)" && git checkout 74d54460d0af8e21deacee8dcced325cfc191141  # 3.15.4
-HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake
+curl -fSL -R -J https://mirrors.aliyun.com/homebrew/homebrew-bottles/bottles/cmake-3.14.6.mojave.bottle.tar.gz -o ~/cmake-3.14.6.mojave.bottle.tar.gz
+brew install -f ~/cmake-3.14.6.mojave.bottle.tar.gz
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-110f3d3.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
