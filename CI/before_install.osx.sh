eddfe370b8a858c6a736bf6fe2a75132ff46a5c7
Bret Curtis <psi29a@gmail.com>
2019-11-25 15:41:51 +0100

    try to list cmakes

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -7,7 +7,8 @@ brew install ccache
 
 # fall back to and older cmake that likes new libboost
 brew unlink cmake
-cd "$(brew --repo homebrew/core)" && git checkout 787d9349e5bf9b2791d17e47e3f93aadb3df3ad0  # 3.15.4
+cd "$(brew --repo homebrew/core)" && git log master -- Formula/cmake.rb
+cd "$(brew --repo homebrew/core)" && git checkout 74d54460d0af8e21deacee8dcced325cfc191141  # 3.15.4
 HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-110f3d3.zip -o ~/openmw-deps.zip
