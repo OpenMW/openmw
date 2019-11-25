bc8f3f72d617f07230a665bc952e0820228a1b8f
Bret Curtis <psi29a@gmail.com>
2019-11-25 15:11:23 +0100

    do some brew trickery

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -2,10 +2,13 @@
 
 brew update
 brew outdated pkgconfig || brew upgrade pkgconfig
-brew unlink cmake
-brew install cmake@3.14
 brew install qt
 brew install ccache
+
+# fall back to and older cmake that likes new libboost
+brew unlink cmake
+cd "$(brew --repo homebrew/core)" && git checkout 787d9349e5bf9b2791d17e47e3f93aadb3df3ad0  # 3.15.4
+HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-110f3d3.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
