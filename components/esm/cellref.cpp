24044b08e0cfd603b4f897958a4971549e960523
Andrei Kortunov <andrei.kortunov@yandex.ru>
2019-12-21 08:47:31 +0400

    Do not store gold-specific value for everything

--- components/esm/cellref.cpp
+++ components/esm/cellref.cpp
@@ -168,9 +168,8 @@ void ESM::CellRef::save (ESMWriter &esm, bool wideRefNum, bool inInventory, bool
     if (mChargeInt != -1)
         esm.writeHNT("INTV", mChargeInt);
 
-    if (mGoldValue != 1) {
+    if (mGoldValue > 1)
         esm.writeHNT("NAM9", mGoldValue);
-    }
 
     if (!inInventory && mTeleport)
     {
@@ -208,7 +207,7 @@ void ESM::CellRef::blank()
     mChargeInt = -1;
     mChargeIntRemainder = 0.0f;
     mEnchantmentCharge = -1;
-    mGoldValue = 0;
+    mGoldValue = 1;
     mDestCell.clear();
     mLockLevel = 0;
     mKey.clear();
