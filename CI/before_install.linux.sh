5091e2c371178396eecc8391993339a8adb27bb0
Bret Curtis <psi29a@gmail.com>
2020-03-22 20:56:48 +0100

    bump to bionic; gcc-9 and clang-10

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-sudo ln -sf /usr/bin/clang-7 /usr/local/bin/clang
-sudo ln -sf /usr/bin/clang++-7 /usr/local/bin/clang++
+sudo ln -sf /usr/bin/clang-10 /usr/local/bin/clang
+sudo ln -sf /usr/bin/clang++-10 /usr/local/bin/clang++
