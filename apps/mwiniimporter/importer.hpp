3663bd786fe60df8a234fe56e0bad15f25cc0f49
Bret Curtis <psi29a@gmail.com>
2020-03-23 23:18:57 +0100

    go allman

--- apps/mwiniimporter/importer.hpp
+++ apps/mwiniimporter/importer.hpp
@@ -7,36 +7,33 @@
 #include <exception>
 #include <iosfwd>
 #include <boost/filesystem/path.hpp>
-
 #include <components/to_utf8/to_utf8.hpp>
 
 class MwIniImporter {
-  public:
+public:
     typedef std::map<std::string, std::string> strmap;
-    typedef std::map<std::string, std::vector<std::string> > multistrmap;
-    typedef std::vector< std::pair< std::string, std::vector<std::string> > > dependencyList;
+    typedef std::map<std::string, std::vector<std::string>> multistrmap;
+    typedef std::vector<std::pair<std::string, std::vector<std::string>>> dependencyList;
 
     MwIniImporter();
-    void    setInputEncoding(const ToUTF8::FromType& encoding);
-    void    setVerbose(bool verbose);
-    multistrmap  loadIniFile(const boost::filesystem::path& filename) const;
-    multistrmap  loadCfgFile(const boost::filesystem::path& filename) const;
-    void merge(multistrmap &cfg, const multistrmap &ini) const;
-    void mergeFallback(multistrmap &cfg, const multistrmap &ini) const;
-    void importGameFiles(multistrmap &cfg, const multistrmap &ini,
+    void setInputEncoding(const ToUTF8::FromType& encoding);
+    void setVerbose(bool verbose);
+    multistrmap loadIniFile(const boost::filesystem::path& filename) const;
+    multistrmap loadCfgFile(const boost::filesystem::path& filename) const;
+    void merge(multistrmap& cfg, const multistrmap& ini) const;
+    void mergeFallback(multistrmap& cfg, const multistrmap& ini) const;
+    void importGameFiles(multistrmap& cfg, const multistrmap& ini,
             const boost::filesystem::path& iniFilename) const;
-    void importArchives(multistrmap &cfg, const multistrmap &ini) const;
-    void writeToFile(std::ostream &out, const multistrmap &cfg) const;
 
+    void importArchives(multistrmap& cfg, const multistrmap& ini) const;
+    void writeToFile(std::ostream& out, const multistrmap& cfg) const;
     static std::vector<std::string> dependencySort(MwIniImporter::dependencyList source);
 
-  private:
+private:
     static void dependencySortStep(std::string& element, MwIniImporter::dependencyList& source, std::vector<std::string>& result);
     static std::vector<std::string>::iterator findString(std::vector<std::string>& source, const std::string& string);
-
-    static void insertMultistrmap(multistrmap &cfg, const std::string& key, const std::string& value);
+    static void insertMultistrmap(multistrmap& cfg, const std::string& key, const std::string& value);
     static void addPaths(std::vector<boost::filesystem::path>& output, std::vector<std::string> input);
-
     /// \return file's "last modified time", used in original MW to determine plug-in load order
     static std::time_t lastWriteTime(const boost::filesystem::path& filename, std::time_t defaultTime);
 
@@ -45,5 +42,4 @@ class MwIniImporter {
     std::vector<std::string> mMergeFallback;
     ToUTF8::FromType mEncoding;
 };
-
 #endif
--- apps/niftest/niftest.cpp
+++ apps/niftest/niftest.cpp
@@ -16,7 +16,8 @@ namespace bpo = boost::program_options;
 namespace bfs = boost::filesystem;
 
 ///See if the file has the named extension
-bool hasExtension(const std::string &filename, std::string extensionToFind) {
+bool hasExtension(const std::string& filename, std::string extensionToFind)
+{
     std::string extension = filename.substr(filename.find_last_of('.') + 1);
 
     //Convert strings to lower case for comparison
@@ -27,46 +28,57 @@ bool hasExtension(const std::string &filename, std::string extensionToFind) {
 }
 
 ///See if the file has the "nif" extension.
-bool isNIF(const std::string &filename) {
+bool isNIF(const std::string& filename)
+{
     return hasExtension(filename, "nif");
 }
 
 ///See if the file has the "bsa" extension.
-bool isBSA(const std::string &filename) {
+bool isBSA(const std::string& filename)
+{
     return hasExtension(filename, "bsa");
 }
 
 /// Check all the nif files in a given VFS::Archive
 /// \note Takes ownership!
 /// \note Can not read a bsa file inside of a bsa file.
-void readVFS(VFS::Archive *anArchive, const std::string &archivePath = "") {
+void readVFS(VFS::Archive* anArchive, const std::string& archivePath = "")
+{
     VFS::Manager myManager(true);
     myManager.addArchive(anArchive);
     myManager.buildIndex();
 
-    std::map<std::string, VFS::File *> files = myManager.getIndex();
-    for (std::map<std::string, VFS::File *>::const_iterator it = files.begin(); it != files.end(); ++it) {
+    std::map<std::string, VFS::File*> files = myManager.getIndex();
+    for (std::map<std::string, VFS::File*>::const_iterator it = files.begin(); it != files.end(); ++it)
+    {
         std::string name = it->first;
 
-        try {
-            if (isNIF(name)) {
-                //           std::cout << "Decoding: " << name << std::endl;
+        try
+        {
+            if (isNIF(name))
+            {
+                // std::cout << "Decoding: " << name << std::endl;
                 Nif::NIFFile temp_nif(myManager.get(name), archivePath + name);
-            } else if (isBSA(name)) {
-                if (!archivePath.empty() && !isBSA(archivePath)) {
-//                     std::cout << "Reading BSA File: " << name << std::endl;
+            }
+            else if (isBSA(name))
+            {
+                if (!archivePath.empty() && !isBSA(archivePath))
+                {
+                    // std::cout << "Reading BSA File: " << name << std::endl;
                     readVFS(new VFS::BsaArchive(archivePath + name), archivePath + name + "/");
-//                     std::cout << "Done with BSA File: " << name << std::endl;
+                    // std::cout << "Done with BSA File: " << name << std::endl;
                 }
             }
         }
-        catch (std::exception &e) {
+        catch (std::exception& e)
+        {
             std::cerr << "ERROR, an exception has occurred:  " << e.what() << std::endl;
         }
     }
 }
 
-bool parseOptions(int argc, char **argv, std::vector<std::string> &files) {
+bool parseOptions(int argc, char** argv, std::vector<std::string>& files)
+{
     bpo::options_description desc("Ensure that OpenMW can use the provided NIF and BSA files\n\n"
                                   "Usages:\n"
                                   "  niftool <nif files, BSA files, or directories>\n"
@@ -74,28 +86,32 @@ bool parseOptions(int argc, char **argv, std::vector<std::string> &files) {
                                   "Allowed options");
     desc.add_options()
             ("help,h", "print help message.")
-            ("input-file", bpo::value<std::vector<std::string> >(), "input file");
+            ("input-file", bpo::value<std::vector<std::string>>(), "input file");
 
     //Default option if none provided
     bpo::positional_options_description p;
     p.add("input-file", -1);
 
     bpo::variables_map variables;
-    try {
+    try
+    {
         bpo::parsed_options valid_opts = bpo::command_line_parser(argc, argv).
                 options(desc).positional(p).run();
         bpo::store(valid_opts, variables);
         bpo::notify(variables);
-        if (variables.count("help")) {
+        if (variables.count("help"))
+        {
             std::cout << desc << std::endl;
             return false;
         }
-        if (variables.count("input-file")) {
-            files = variables["input-file"].as<std::vector<std::string> >();
+        if (variables.count("input-file"))
+        {
+            files = variables["input-file"].as<std::vector<std::string>>();
             return true;
         }
     }
-    catch (std::exception &e) {
+    catch (std::exception& e)
+    {
         std::cout << "ERROR parsing arguments: " << e.what() << "\n\n"
                   << desc << std::endl;
         return false;
@@ -106,28 +122,39 @@ bool parseOptions(int argc, char **argv, std::vector<std::string> &files) {
     return false;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     std::vector<std::string> files;
     if (!parseOptions(argc, argv, files))
         return 1;
 
-//     std::cout << "Reading Files" << std::endl;
-    for (auto name : files) {
-        try {
-            if (isNIF(name)) {
-                //std::cout << "Decoding: " << name << std::endl;
+    // std::cout << "Reading Files" << std::endl;
+    for (auto name : files)
+    {
+        try
+        {
+            if (isNIF(name))
+            {
+                // std::cout << "Decoding: " << name << std::endl;
                 Nif::NIFFile temp_nif(Files::openConstrainedFileStream(name.c_str()), name);
-            } else if (isBSA(name)) {
-//                 std::cout << "Reading BSA File: " << name << std::endl;
+            }
+            else if (isBSA(name))
+            {
+                // std::cout << "Reading BSA File: " << name << std::endl;
                 readVFS(new VFS::BsaArchive(name));
-            } else if (bfs::is_directory(bfs::path(name))) {
-//                 std::cout << "Reading All Files in: " << name << std::endl;
+            }
+            else if (bfs::is_directory(bfs::path(name)))
+            {
+                // std::cout << "Reading All Files in: " << name << std::endl;
                 readVFS(new VFS::FileSystemArchive(name), name);
-            } else {
+            }
+            else
+            {
                 std::cerr << "ERROR:  \"" << name << "\" is not a nif file, bsa file, or directory!" << std::endl;
             }
         }
-        catch (std::exception &e) {
+        catch (std::exception& e)
+        {
             std::cerr << "ERROR, an exception has occurred:  " << e.what() << std::endl;
         }
     }
