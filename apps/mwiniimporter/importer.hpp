ab977593dc8ce135e93ee4aef01d544769585a35
Bret Curtis <psi29a@gmail.com>
2020-03-27 07:52:47 +0100

    be consistent

--- apps/mwiniimporter/importer.hpp
+++ apps/mwiniimporter/importer.hpp
@@ -16,26 +16,26 @@ public:
     typedef std::vector<std::pair<std::string, std::vector<std::string>>> dependencyList;
 
     MwIniImporter();
-    void setInputEncoding(const ToUTF8::FromType& encoding);
+    void setInputEncoding(const ToUTF8::FromType &encoding);
     void setVerbose(bool verbose);
-    multistrmap loadIniFile(const boost::filesystem::path& filename) const;
-    multistrmap loadCfgFile(const boost::filesystem::path& filename) const;
-    void merge(multistrmap& cfg, const multistrmap& ini) const;
-    void mergeFallback(multistrmap& cfg, const multistrmap& ini) const;
-    void importGameFiles(multistrmap& cfg, const multistrmap& ini,
-            const boost::filesystem::path& iniFilename) const;
+    multistrmap loadIniFile(const boost::filesystem::path &filename) const;
+    multistrmap loadCfgFile(const boost::filesystem::path &filename) const;
+    void merge(multistrmap &cfg, const multistrmap &ini) const;
+    void mergeFallback(multistrmap &cfg, const multistrmap &ini) const;
+    void importGameFiles(multistrmap &cfg, const multistrmap &ini,
+            const boost::filesystem::path &iniFilename) const;
 
-    void importArchives(multistrmap& cfg, const multistrmap& ini) const;
-    void writeToFile(std::ostream& out, const multistrmap& cfg) const;
+    void importArchives(multistrmap &cfg, const multistrmap &ini) const;
+    void writeToFile(std::ostream &out, const multistrmap &cfg) const;
     static std::vector<std::string> dependencySort(MwIniImporter::dependencyList source);
 
 private:
-    static void dependencySortStep(std::string& element, MwIniImporter::dependencyList& source, std::vector<std::string>& result);
-    static std::vector<std::string>::iterator findString(std::vector<std::string>& source, const std::string& string);
-    static void insertMultistrmap(multistrmap& cfg, const std::string& key, const std::string& value);
-    static void addPaths(std::vector<boost::filesystem::path>& output, std::vector<std::string> input);
+    static void dependencySortStep(std::string &element, MwIniImporter::dependencyList &source, std::vector<std::string> &result);
+    static std::vector<std::string>::iterator findString(std::vector<std::string> &source, const std::string &string);
+    static void insertMultistrmap(multistrmap &cfg, const std::string &key, const std::string &value);
+    static void addPaths(std::vector<boost::filesystem::path> &output, std::vector<std::string> input);
     /// \return file's "last modified time", used in original MW to determine plug-in load order
-    static std::time_t lastWriteTime(const boost::filesystem::path& filename, std::time_t defaultTime);
+    static std::time_t lastWriteTime(const boost::filesystem::path &filename, std::time_t defaultTime);
 
     bool mVerbose;
     strmap mMergeMap;
