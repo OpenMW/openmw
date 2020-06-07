75e7a3e8b1cbe4d72859ca75b082a8f93e377667
Andrei Kortunov <andrei.kortunov@yandex.ru>
2020-06-07 09:25:46 +0400

    Do not store object position, if it is the same as in CellRef

--- components/esm/defs.hpp
+++ components/esm/defs.hpp
@@ -57,6 +57,26 @@ struct Position
 };
 #pragma pack(pop)
 
+bool inline operator== (const Position& left, const Position& right) noexcept
+{
+    return left.pos[0] == right.pos[0] &&
+           left.pos[1] == right.pos[1] &&
+           left.pos[2] == right.pos[2] &&
+           left.rot[0] == right.rot[0] &&
+           left.rot[1] == right.rot[1] &&
+           left.rot[2] == right.rot[2];
+}
+
+bool inline operator!= (const Position& left, const Position& right) noexcept
+{
+    return left.pos[0] != right.pos[0] ||
+           left.pos[1] != right.pos[1] ||
+           left.pos[2] != right.pos[2] ||
+           left.rot[0] != right.rot[0] ||
+           left.rot[1] != right.rot[1] ||
+           left.rot[2] != right.rot[2];
+}
+
 template <int a, int b, int c, int d>
 struct FourCC
 {
