3eeee4bc9477451c0ec01377b3d96e73c59e17ef
Bret Curtis <psi29a@gmail.com>
2020-07-01 17:52:34 +0200

    drop need to upgrade qt, 5.15 is by default installed; add CXX_FLAGS; retarget SYSROOT to 10.15

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -2,7 +2,6 @@
 
 brew update
 brew outdated pkgconfig || brew upgrade pkgconfig
-brew install qt
 brew install ccache
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-ef2462c.zip -o ~/openmw-deps.zip
