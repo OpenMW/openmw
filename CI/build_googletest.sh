23fe60a06788352f6140a2570869d1352cf30c8b
elsid <elsid.mail@gmail.com>
2020-10-02 22:46:23 +0200

    Run unit tests in a separate build

--- CI/build_googletest.sh
+++ CI/build_googletest.sh
@@ -1,13 +1,17 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 git clone -b release-1.10.0 https://github.com/google/googletest.git
 cd googletest
 mkdir build
 cd build
 cmake \
+    -D CMAKE_C_COMPILER="${CC}" \
+    -D CMAKE_CXX_COMPILER="${CXX}" \
+    -D CMAKE_C_COMPILER_LAUNCHER=ccache \
+    -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -D CMAKE_BUILD_TYPE="${CONFIGURATION}" \
-    -D CMAKE_INSTALL_PREFIX=. \
+    -D CMAKE_INSTALL_PREFIX="${GOOGLETEST_DIR}" \
     -G "${GENERATOR}" \
     ..
-cmake --build . --config "${CONFIGURATION}"
-cmake --build . --target install --config "${CONFIGURATION}"
+cmake --build . --config "${CONFIGURATION}" -- -j $(nproc)
+cmake --install . --config "${CONFIGURATION}"
