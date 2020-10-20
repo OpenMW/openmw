06689cfe2a0f627058cc95fd5c3dacbc144ddbfb
Bret Curtis <psi29a@gmail.com>
2020-10-20 23:22:37 +0200

    bingo; cleanup

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -4,10 +4,9 @@
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
 command -v qmake >/dev/null 2>&1 || brew install qt
-brew link --overwrite --dry-run lz4
-brew link --overwrite lz4
+
+brew link --overwrite lz4  #  overwrite system lz4; use brew
 brew reinstall lz4
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-ef2462c.zip -o ~/openmw-deps.zip
-rm -fr /tmp/openmw-deps
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
