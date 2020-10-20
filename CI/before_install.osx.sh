c3b464a0c82d9a8ce0761054109d6374013af7c5
Bret Curtis <psi29a@gmail.com>
2020-10-20 23:00:37 +0200

    brew reinstall lz4; purge /tmp/openmw-deps just in case

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -4,7 +4,8 @@
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
 command -v qmake >/dev/null 2>&1 || brew install qt
-command -v lz4 >/dev/null 2>&1 || brew install lz4
+brew reinstall lz4
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-ef2462c.zip -o ~/openmw-deps.zip
+rm -fr /tmp/openmw-deps
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
