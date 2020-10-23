ce0966b9b7f11f8f6a899fc0bdf570ce6dd83b16
AnyOldName3 <krizdjali+github@gmail.com>
2020-10-23 13:13:47 +0100

    Improve validate implementation

--- components/fallback/validate.cpp
+++ components/fallback/validate.cpp
@@ -9,11 +9,11 @@ void Fallback::validate(boost::any& v, std::vector<std::string> const& tokens, F
 
     FallbackMap *map = boost::any_cast<FallbackMap>(&v);
 
-    for (std::vector<std::string>::const_iterator it = tokens.begin(); it != tokens.end(); ++it)
+    for (const auto& token : tokens)
     {
-        std::string temp = Files::EscapeHashString::processString(*it);
-        int sep = temp.find(",");
-        if (sep < 1 || sep == (int)temp.length() - 1)
+        std::string temp = Files::EscapeHashString::processString(token);
+        size_t sep = temp.find(",");
+        if (sep < 1 || sep == temp.length() - 1)
 #if (BOOST_VERSION < 104200)
             throw boost::program_options::validation_error("invalid value");
 #else
