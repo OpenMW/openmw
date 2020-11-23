96e22bd44e58e69487456759ffc0a11125ecc7b7
psi29a <psi29a@gmail.com>
2020-11-23 06:05:45 +0000

    Merge branch 'fastforwardpos' into 'master'
    Discard physics simulation results after fast forward
    
    See merge request OpenMW/openmw!423
    
    (cherry picked from commit ff2d7695698341ef059c75707aa092cef48deea4)
    
    03a37433 In case of time fast forward (resting, jail), force reset of positions

--- apps/openmw/mwworld/worldimp.cpp
+++ apps/openmw/mwworld/worldimp.cpp
@@ -890,6 +890,7 @@ namespace MWWorld
         {
             mRendering->notifyWorldSpaceChanged();
             mProjectileManager->clear();
+            mDiscardMovements = true;
         }
     }
 
