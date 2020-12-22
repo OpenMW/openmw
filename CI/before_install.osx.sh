d8f04ed09ce2479441f66e6cc451e836d6add2e5
Chris Djali <AnyOldName3@users.noreply.github.com>
2020-12-22 18:19:22 +0000

    Investigate where MacOS on Travis is failing

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -e
 
 # Some of these tools can come from places other than brew, so check before installing
+echo line 4
 command -v ccache >/dev/null 2>&1 || brew install ccache
+echo line 6
 command -v cmake >/dev/null 2>&1 || brew install cmake
+echo line 8
 command -v qmake >/dev/null 2>&1 || brew install qt
-
+echo line 10
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-f8918dd.zip -o ~/openmw-deps.zip
+echo line 12
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
+echo line 14
