d576f5a67a6f14d48fe0051bdcc842b0b19b3816
Bret Curtis <psi29a@gmail.com>
2021-01-17 13:36:17 +0100

    Update before_install.osx.sh

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# workaround python issue on travis
+HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --ignore-dependencies python@3.9
+brew install python@3.9
+
 # Some of these tools can come from places other than brew, so check before installing
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
