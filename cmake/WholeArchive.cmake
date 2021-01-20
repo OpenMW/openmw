8737453498d7dc4b62458e53f5318c76bc40865d
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-20 19:19:36 +0000

    cmake: Compiler-specific whole-archive macro

---
+++ cmake/WholeArchive.cmake
@@ -0,0 +1,10 @@
+macro (get_whole_archive_options OUT_VAR)
+    # We use --whole-archive because OSG plugins use registration.
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(${OUT_VAR} -Wl,--whole-archive ${ARGN} -Wl,--no-whole-archive)
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+        set(${OUT_VAR} -Wl,-all_load ${ARGN} -Wl,-noall_load)
+    else ()
+        message(FATAL_ERROR "get_whole_archive_options not implemented for CMAKE_CXX_COMPILER_ID ${CMAKE_CXX_COMPILER_ID}")
+    endif()
+endmacro (whole_archive_options)
