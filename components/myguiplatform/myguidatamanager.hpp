4495b67d77c958a9c33c70111dac04613a8f9701
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-23 13:49:16 +0000

    MyGUI HEAD compatibility
    Makes OpenMW compatible with the current MyGUI HEAD at:
    https://github.com/MyGUI/mygui/commit/f93d4fb614f843390cfe3492586466dc8d06c4b3
    
    Refs #5806

--- components/myguiplatform/myguidatamanager.hpp
+++ components/myguiplatform/myguidatamanager.hpp
@@ -3,9 +3,10 @@
 
 #include <MyGUI_DataManager.h>
 
+#include "myguicompat.h"
+
 namespace osgMyGUI
 {
-
 
 class DataManager : public MyGUI::DataManager
 {
@@ -18,7 +19,7 @@ public:
     /** Get data stream from specified resource name.
         @param _name Resource name (usually file name).
     */
-    MyGUI::IDataStream* getData(const std::string& _name) override;
+    MyGUI::IDataStream* getData(const std::string& _name) OPENMW_MYGUI_CONST_GETTER_3_4_1 override;
 
     /** Free data stream.
         @param _data Data stream.
@@ -28,18 +29,18 @@ public:
     /** Is data with specified name exist.
         @param _name Resource name.
     */
-    bool isDataExist(const std::string& _name) override;
+    bool isDataExist(const std::string& _name) OPENMW_MYGUI_CONST_GETTER_3_4_1 override;
 
     /** Get all data names with names that matches pattern.
         @param _pattern Pattern to match (for example "*.layout").
     */
-    const MyGUI::VectorString& getDataListNames(const std::string& _pattern) override;
+    const MyGUI::VectorString& getDataListNames(const std::string& _pattern) OPENMW_MYGUI_CONST_GETTER_3_4_1 override;
 
     /** Get full path to data.
         @param _name Resource name.
         @return Return full path to specified data.
     */
-    const std::string& getDataPath(const std::string& _name) override;
+    const std::string& getDataPath(const std::string& _name) OPENMW_MYGUI_CONST_GETTER_3_4_1 override;
 
 private:
     std::string mResourcePath;
