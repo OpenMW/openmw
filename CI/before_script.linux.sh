2798db541844fffe4b1f9b572087e0ee7f20deed
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-24 18:44:18 +0000

    CI/before_script.linux.sh: <tab> -> 4 spaces and remove unused ENV

--- CI/before_script.linux.sh
+++ CI/before_script.linux.sh
@@ -5,51 +5,50 @@ set -xeo pipefail
 free -m
 
 if [[ "${BUILD_TESTS_ONLY}" ]]; then
-	export GOOGLETEST_DIR="${PWD}/googletest/build/install"
-	env GENERATOR='Unix Makefiles' CONFIGURATION=Release CI/build_googletest.sh
+    export GOOGLETEST_DIR="${PWD}/googletest/build/install"
+    env GENERATOR='Unix Makefiles' CONFIGURATION=Release CI/build_googletest.sh
 fi
 
 declare -a CMAKE_CONF_OPTS=(
-	-DCMAKE_C_COMPILER="${CC:-/usr/bin/cc}"
-	-DCMAKE_CXX_COMPILER="${CXX:-/usr/bin/c++}"
-	-DCMAKE_C_COMPILER_LAUNCHER=ccache
-	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-	-DCMAKE_INSTALL_PREFIX=install
-	-DCMAKE_BUILD_TYPE=RelWithDebInfo
-	-DBUILD_SHARED_LIBS=OFF
-	-DUSE_SYSTEM_TINYXML=ON
-	-DCMAKE_INSTALL_PREFIX=install
+    -DCMAKE_C_COMPILER="${CC:-/usr/bin/cc}"
+    -DCMAKE_CXX_COMPILER="${CXX:-/usr/bin/c++}"
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    -DCMAKE_INSTALL_PREFIX=install
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    -DBUILD_SHARED_LIBS=OFF
+    -DUSE_SYSTEM_TINYXML=ON
+    -DCMAKE_INSTALL_PREFIX=install
 )
-declare -a CMAKE_CONF_ENV=()
 
 if [[ $CI_OPENMW_USE_STATIC_DEPS ]]; then
-	CMAKE_CONF_OPTS+=(
-		-DOPENMW_USE_SYSTEM_MYGUI=OFF
-		-DOPENMW_USE_SYSTEM_OSG=OFF
-		-DOPENMW_USE_SYSTEM_BULLET=OFF
-	)
+    CMAKE_CONF_OPTS+=(
+        -DOPENMW_USE_SYSTEM_MYGUI=OFF
+        -DOPENMW_USE_SYSTEM_OSG=OFF
+        -DOPENMW_USE_SYSTEM_BULLET=OFF
+    )
 fi
 
 mkdir -p build
 cd build
 
 if [[ "${BUILD_TESTS_ONLY}" ]]; then
-	env "${CMAKE_CONF_ENV[@]}" ${ANALYZE} cmake \
-		"${CMAKE_CONF_OPTS[@]}" \
-		-DBUILD_OPENMW=OFF \
-		-DBUILD_BSATOOL=OFF \
-		-DBUILD_ESMTOOL=OFF \
-		-DBUILD_LAUNCHER=OFF \
-		-DBUILD_MWINIIMPORTER=OFF \
-		-DBUILD_ESSIMPORTER=OFF \
-		-DBUILD_OPENCS=OFF \
-		-DBUILD_WIZARD=OFF \
-		-DBUILD_UNITTESTS=ON \
-		-DGTEST_ROOT="${GOOGLETEST_DIR}" \
-		-DGMOCK_ROOT="${GOOGLETEST_DIR}" \
-		..
+    ${ANALYZE} cmake \
+        "${CMAKE_CONF_OPTS[@]}" \
+        -DBUILD_OPENMW=OFF \
+        -DBUILD_BSATOOL=OFF \
+        -DBUILD_ESMTOOL=OFF \
+        -DBUILD_LAUNCHER=OFF \
+        -DBUILD_MWINIIMPORTER=OFF \
+        -DBUILD_ESSIMPORTER=OFF \
+        -DBUILD_OPENCS=OFF \
+        -DBUILD_WIZARD=OFF \
+        -DBUILD_UNITTESTS=ON \
+        -DGTEST_ROOT="${GOOGLETEST_DIR}" \
+        -DGMOCK_ROOT="${GOOGLETEST_DIR}" \
+        ..
 else
-	env "${CMAKE_CONF_ENV[@]}" ${ANALYZE} cmake \
-		"${CMAKE_CONF_OPTS[@]}" \
-		..
+    ${ANALYZE} cmake \
+        "${CMAKE_CONF_OPTS[@]}" \
+        ..
 fi
