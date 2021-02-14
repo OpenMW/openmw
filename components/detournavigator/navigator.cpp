8dba61f7aed8072edac0755454e0df7edd269531
elsid <elsid.mail@gmail.com>
2021-02-14 04:07:08 +0100

    Use navmesh raycast to find reachable position around target

--- components/detournavigator/navigator.cpp
+++ components/detournavigator/navigator.cpp
@@ -1,5 +1,6 @@
 #include "findrandompointaroundcircle.hpp"
 #include "navigator.hpp"
+#include "raycast.hpp"
 
 namespace DetourNavigator
 {
@@ -16,5 +17,20 @@ namespace DetourNavigator
         if (!result)
             return std::optional<osg::Vec3f>();
         return std::optional<osg::Vec3f>(fromNavMeshCoordinates(settings, *result));
+    }
+
+    std::optional<osg::Vec3f> Navigator::raycast(const osg::Vec3f& agentHalfExtents, const osg::Vec3f& start,
+        const osg::Vec3f& end, const Flags includeFlags) const
+    {
+        const auto navMesh = getNavMesh(agentHalfExtents);
+        if (navMesh == nullptr)
+            return {};
+        const auto settings = getSettings();
+        const auto result = DetourNavigator::raycast(navMesh->lockConst()->getImpl(),
+            toNavMeshCoordinates(settings, agentHalfExtents), toNavMeshCoordinates(settings, start),
+            toNavMeshCoordinates(settings, end), includeFlags, settings);
+        if (!result)
+            return {};
+        return fromNavMeshCoordinates(settings, *result);
     }
 }
