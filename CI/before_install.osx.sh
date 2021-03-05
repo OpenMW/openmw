8fc0f965bb04de2f857a9c567699545e92aa1179
Bret Curtis <psi29a@gmail.com>
2021-03-05 13:10:22 +0100

    which qmake?

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -9,5 +9,8 @@ command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
 command -v qmake >/dev/null 2>&1 || brew install qt@5
 
+qmake --version
+which qmake
+
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-f8918dd.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
