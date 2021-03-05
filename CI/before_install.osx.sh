caa8b0ca1a7444a960160ffb11e315eca53be2fc
Bret Curtis <psi29a@gmail.com>
2021-03-05 13:03:21 +0100

    give qt5 a show for brew

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -7,7 +7,7 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --ignore-dependencies python@3.9 || tru
 # Some of these tools can come from places other than brew, so check before installing
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
-command -v qmake >/dev/null 2>&1 || brew install qt
+command -v qmake >/dev/null 2>&1 || brew install qt5
 
 curl -fSL -R -J https://downloads.openmw.org/osx/dependencies/openmw-deps-f8918dd.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
