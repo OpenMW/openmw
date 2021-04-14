388573cf6030e5867f7567493b05541b2eb02598
Andrei Kortunov <andrei.kortunov@yandex.ru>
2021-04-14 08:07:08 +0400

    Fix uninitialized fields in the essimporter

--- apps/essimporter/importinventory.cpp
+++ apps/essimporter/importinventory.cpp
@@ -19,6 +19,7 @@ namespace ESSImport
             item.mCount = contItem.mCount;
             item.mRelativeEquipmentSlot = -1;
             item.mLockLevel = 0;
+            item.mRefNum.unset();
 
             unsigned int itemCount = std::abs(item.mCount);
             bool separateStacks = false;
