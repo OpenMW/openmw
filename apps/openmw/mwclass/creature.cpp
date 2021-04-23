e1d955d92b80dc26fd59f865467545aa1a7f2125
elsid <elsid.mail@gmail.com>
2021-04-24 00:16:57 +0200

    Remove noexcept specifier from CreatureCustomData move ctor
    C++17 doesn't allow to declare defaulted move ctor with not matching
    exception specification to the calculated one:
    /Users/eddie/Downloads/BUILD/openmw-master/apps/openmw/mwclass/creature.cpp:63:9: error:
          exception specification of explicitly defaulted move constructor does not
          match the calculated one
            CreatureCustomData(CreatureCustomData&& other) noexcept = default;
            ^
    
    http://wg21.link/p1286r2 makes this possible in C++20 and newer compilers
    already have it working even with -std=c++17 but older onces give an error.

--- apps/openmw/mwclass/creature.cpp
+++ apps/openmw/mwclass/creature.cpp
@@ -60,7 +60,7 @@ namespace MWClass
 
         CreatureCustomData() = default;
         CreatureCustomData(const CreatureCustomData& other);
-        CreatureCustomData(CreatureCustomData&& other) noexcept = default;
+        CreatureCustomData(CreatureCustomData&& other) = default;
 
         CreatureCustomData& asCreatureCustomData() override
         {
