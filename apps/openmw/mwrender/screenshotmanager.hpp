5b9a2b73b0f47728c12759f7918da50dd9ef6f2a
madsbuvi <madssandvei@zoho.com>
2021-05-16 13:04:30 +0200

    Retain final draw callback as a member variable in ScreenshotManager, and do not call setFinalDrawCallback after init.

--- apps/openmw/mwrender/screenshotmanager.hpp
+++ apps/openmw/mwrender/screenshotmanager.hpp
@@ -16,11 +16,13 @@ namespace Resource
 namespace MWRender
 {
     class Water;
+    class NotifyDrawCompletedCallback;
 
     class ScreenshotManager
     {
     public:
         ScreenshotManager(osgViewer::Viewer* viewer, osg::ref_ptr<osg::Group> rootNode, osg::ref_ptr<osg::Group> sceneRoot, Resource::ResourceSystem* resourceSystem, Water* water);
+        ~ScreenshotManager();
 
         void screenshot(osg::Image* image, int w, int h);
         bool screenshot360(osg::Image* image);
@@ -29,9 +31,11 @@ namespace MWRender
         osg::ref_ptr<osgViewer::Viewer> mViewer;
         osg::ref_ptr<osg::Group> mRootNode;
         osg::ref_ptr<osg::Group> mSceneRoot;
+        osg::ref_ptr<NotifyDrawCompletedCallback> mDrawCompleteCallback;
         Resource::ResourceSystem* mResourceSystem;
         Water* mWater;
 
+        void traversalsAndWait(unsigned int frame);
         void renderCameraToImage(osg::Camera *camera, osg::Image *image, int w, int h);
         void makeCubemapScreenshot(osg::Image* image, int w, int h, osg::Matrixd cameraTransform=osg::Matrixd());
     };
