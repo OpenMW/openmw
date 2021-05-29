9f2f5174d4c66d355c23f5395202949565558854
Thunderforge <wjherrmann@gmail.com>
2021-05-29 16:17:30 -0500

    Adding Object Paging Min Size combo box to Advanced -> Visuals
    Requested in Discord by CMAugust and hammered out with a few other users.

--- apps/launcher/advancedpage.cpp
+++ apps/launcher/advancedpage.cpp
@@ -136,6 +136,7 @@ bool Launcher::AdvancedPage::loadSettings()
 
         loadSettingBool(activeGridObjectPagingCheckBox, "object paging active grid", "Terrain");
         viewingDistanceComboBox->setValue(convertToCells(Settings::Manager::getInt("viewing distance", "Camera")));
+        objectPagingMinSizeComboBox->setValue(Settings::Manager::getFloat("object paging min size", "Terrain"));
     }
 
     // Audio
@@ -287,6 +288,9 @@ void Launcher::AdvancedPage::saveSettings()
         {
             Settings::Manager::setInt("viewing distance", "Camera", convertToUnits(viewingDistance));
         }
+        double objectPagingMinSize = objectPagingMinSizeComboBox->value();
+        if (objectPagingMinSize != Settings::Manager::getFloat("object paging min size", "Terrain"))
+            Settings::Manager::setFloat("object paging min size", "Terrain", objectPagingMinSize);
     }
     
     // Audio
