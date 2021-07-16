6bcb7a86ddee8b8a2d2100abb24fdb0636f880b9
Bret Curtis <bret.curtis@gmail.com>
2021-07-16 20:01:12 +0200

    update brew; remove need for brew install lua

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -ex
 
+export HOMEBREW_NO_EMOJI=1
+brew update --quiet
+
 # workaround python issue on travis
-[ -z "${TRAVIS}" ] && HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --ignore-dependencies python@3.8 || true
-[ -z "${TRAVIS}" ] && HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --ignore-dependencies python@3.9 || true
-[ -z "${TRAVIS}" ] && HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall --ignore-dependencies qt@6 || true
+[ -z "${TRAVIS}" ] brew uninstall --ignore-dependencies python@3.8 || true
+[ -z "${TRAVIS}" ] brew uninstall --ignore-dependencies python@3.9 || true
+[ -z "${TRAVIS}" ] brew uninstall --ignore-dependencies qt@6 || true
 
 # Some of these tools can come from places other than brew, so check before installing
+[ -z "${TRAVIS}" ] brew install fontconfig
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
 command -v qmake >/dev/null 2>&1 || brew install qt@5
@@ -15,10 +19,6 @@ ccache --version
 cmake --version
 qmake --version
 
-brew install lua
-
 curl -fSL -R -J https://gitlab.com/OpenMW/openmw-deps/-/raw/main/macos/openmw-deps-20210716.zip -o ~/openmw-deps.zip
 unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
 
-# additional libraries
-[ -z "${TRAVIS}" ] && HOMEBREW_NO_AUTO_UPDATE=1 brew install fontconfig
