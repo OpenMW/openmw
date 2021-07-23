ee3361a118d51850f99935cd393edcae7186f078
cc9cii <cc9c@iinet.net.au>
2021-07-23 23:18:11 +1000

    Fix table being sorted twice (at least it appeared that way according to the sample profiler)
    - Quoting Qt-4.8: "Note: . Setting the property to true with setSortingEnabled() immediately triggers a call to sortByColumn() with the current sort section and order."
    
    (copied the changes from commit SHA-1: 77394fce99281cea65f30c67dbcd69a2d3eeba88)

--- apps/opencs/view/world/table.cpp
+++ apps/opencs/view/world/table.cpp
@@ -269,12 +269,6 @@ CSVWorld::Table::Table (const CSMWorld::UniversalId& id,
     setSelectionBehavior (QAbstractItemView::SelectRows);
     setSelectionMode (QAbstractItemView::ExtendedSelection);
 
-    setSortingEnabled (sorting);
-    if (sorting)
-    {
-        sortByColumn (mModel->findColumnIndex(CSMWorld::Columns::ColumnId_Id), Qt::AscendingOrder);
-    }
-
     int columns = mModel->columnCount();
     for (int i=0; i<columns; ++i)
     {
@@ -294,6 +288,13 @@ CSVWorld::Table::Table (const CSMWorld::UniversalId& id,
         else
             hideColumn (i);
     }
+
+    if (sorting)
+    {
+        // FIXME: some tables (e.g. CellRef) have this column hidden, which makes it confusing
+        sortByColumn (mModel->findColumnIndex(CSMWorld::Columns::ColumnId_Id), Qt::AscendingOrder);
+    }
+    setSortingEnabled (sorting);
 
     mEditAction = new QAction (tr ("Edit Record"), this);
     connect (mEditAction, SIGNAL (triggered()), this, SLOT (editRecord()));
