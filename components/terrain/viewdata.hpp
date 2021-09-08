b2999584902b73811aaa64d34476989158602e89
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-09-08 10:07:24 +0000

    viewdata.hpp getreusedistance

--- components/terrain/viewdata.hpp
+++ components/terrain/viewdata.hpp
@@ -95,6 +95,8 @@ namespace Terrain
         void rebuildViews();
         bool storeView(const ViewData* view, double referenceTime);
 
+        float getReuseDistance() const { return mReuseDistance; }
+
     private:
         std::list<ViewData> mViewVector;
 
