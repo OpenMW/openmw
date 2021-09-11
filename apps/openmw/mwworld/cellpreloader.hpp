acc06fc9f2f60da0a52f88622118b22b9bde1dcc
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-09-11 09:17:08 +0000

    cellpreloader.hpp isTerrainLoaded

--- apps/openmw/mwworld/cellpreloader.hpp
+++ apps/openmw/mwworld/cellpreloader.hpp
@@ -79,6 +79,7 @@ namespace MWWorld
 
         bool syncTerrainLoad(const std::vector<CellPreloader::PositionCellGrid> &positions, double timestamp, Loading::Listener& listener);
         void abortTerrainPreloadExcept(const PositionCellGrid *exceptPos);
+        bool isTerrainLoaded(const CellPreloader::PositionCellGrid &position, double referenceTime) const;
 
     private:
         Resource::ResourceSystem* mResourceSystem;
@@ -119,6 +120,9 @@ namespace MWWorld
         std::vector<PositionCellGrid> mTerrainPreloadPositions;
         osg::ref_ptr<TerrainPreloadItem> mTerrainPreloadItem;
         osg::ref_ptr<SceneUtil::WorkItem> mUpdateCacheItem;
+
+        std::vector<PositionCellGrid> mLoadedTerrainPositions;
+        double mLoadedTerrainTimestamp;
     };
 
 }
