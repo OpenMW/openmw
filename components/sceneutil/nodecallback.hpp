e5310bb76b13f5ba4a8d5423966a99e63810376a
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-10-03 11:26:46 +0000

    nodecallback.hpp

---
+++ components/sceneutil/nodecallback.hpp
@@ -0,0 +1,35 @@
+#ifndef SCENEUTIL_NODECALLBACK_H
+#define SCENEUTIL_NODECALLBACK_H
+
+#include <osg/Callback>
+
+namespace osg
+{
+    class Node;
+    class NodeVisitor;
+}
+
+namespace SceneUtil
+{
+
+template <class Derived, typename NodeType=osg::Node*, typename VisitorType=osg::NodeVisitor*>
+class NodeCallback : public osg::Callback
+{
+public:
+    virtual bool run(osg::Object* object, osg::Object* data)
+    {
+        static_cast<Derived*>(this)->operator()(static_cast<NodeType>(object), static_cast<VisitorType>(data));
+        return true;
+    }
+
+    void traverse(NodeType object, VisitorType data);
+    {
+        if (_nestedCallback.valid())
+            _nestedCallback->run(object, data);
+        else
+            data->traverse(*object);
+    }
+};
+
+}
+#endif
