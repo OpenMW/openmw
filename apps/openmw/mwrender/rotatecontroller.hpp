bc29e64e19d25e49de4847c8f71c5af5abf1f1c1
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-10-04 10:36:34 +0000

    rotatecontroller.hpp

--- apps/openmw/mwrender/rotatecontroller.hpp
+++ apps/openmw/mwrender/rotatecontroller.hpp
@@ -1,8 +1,13 @@
 #ifndef OPENMW_MWRENDER_ROTATECONTROLLER_H
 #define OPENMW_MWRENDER_ROTATECONTROLLER_H
 
-#include <osg/NodeCallback>
+#include <components/sceneutil/nodecallback.hpp>
 #include <osg/Quat>
+
+namespace osg
+{
+    class MatrixTransform;
+}
 
 namespace MWRender
 {
@@ -10,8 +15,7 @@ namespace MWRender
 /// Applies a rotation in \a relativeTo's space.
 /// @note Assumes that the node being rotated has its "original" orientation set every frame by a different controller.
 /// The rotation is then applied on top of that orientation.
-/// @note Must be set on a MatrixTransform.
-class RotateController : public osg::NodeCallback
+class RotateController : public SceneUtil::NodeCallback<RotateController, osg::MatrixTransform*>
 {
 public:
     RotateController(osg::Node* relativeTo);
@@ -20,7 +24,7 @@ public:
 
     void setRotate(const osg::Quat& rotate);
 
-    void operator()(osg::Node* node, osg::NodeVisitor* nv) override;
+    void operator()(osg::MatrixTransform* node, osg::NodeVisitor* nv);
 
 protected:
     osg::Quat getWorldOrientation(osg::Node* node);
