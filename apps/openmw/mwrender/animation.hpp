98d53546e286409861e92056c4423b8d158c25c5
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-10-21 09:52:34 +0000

    animation.hpp

--- apps/openmw/mwrender/animation.hpp
+++ apps/openmw/mwrender/animation.hpp
@@ -7,6 +7,7 @@
 #include <components/sceneutil/textkeymap.hpp>
 #include <components/sceneutil/util.hpp>
 #include <components/sceneutil/nodecallback.hpp>
+#include <components/misc/stringops.hpp>
 
 #include <vector>
 #include <unordered_map>
