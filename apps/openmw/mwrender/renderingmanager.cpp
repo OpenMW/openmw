40a8ca532a607ccbcd05e20e25f033ea52502acf
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-11-05 09:37:10 +0000

    [ci skip]

--- apps/openmw/mwrender/renderingmanager.cpp
+++ apps/openmw/mwrender/renderingmanager.cpp
@@ -438,7 +438,7 @@ namespace MWRender
             float density = Settings::Manager::getFloat("density", "Groundcover");
             density = std::clamp(density, 0.f, 1.f);
 
-            mGroundcover.reset(new Groundcover(mResourceSystem->getSceneManager(), density, viewDistance));
+            mGroundcover.reset(new Groundcover(mResourceSystem->getSceneManager(), density, groundcoverDistance));
             static_cast<Terrain::QuadTreeWorld*>(mTerrain.get())->addChunkManager(mGroundcover.get());
             mResourceSystem->addResourceManager(mGroundcover.get());
         }
