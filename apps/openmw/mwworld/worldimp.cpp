d3b0714a561fe7eebaa0aa1c1349f3bfa151d838
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-11-06 10:48:15 +0000

    [ci skip]

--- apps/openmw/mwworld/worldimp.cpp
+++ apps/openmw/mwworld/worldimp.cpp
@@ -2991,7 +2991,7 @@ namespace MWWorld
                 std::string message = "Failed loading " + file + ": the groundcover file does not exist";
                 throw std::runtime_error(message);
             }
-            idx++;
+            mEsm.back().setIndex(idx);
         }
     }
 
