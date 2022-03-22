b3e0275e89171f8d927df81ceddc1698ce626a44
Cody Glassman <glassmancody.info@gmail.com>
2022-03-22 05:41:26 +0000

    Lua UI API: Let image manager handle missing textures

--- components/lua_ui/resources.cpp
+++ components/lua_ui/resources.cpp
@@ -7,15 +7,9 @@ namespace LuaUi
 {
     std::shared_ptr<TextureResource> ResourceManager::registerTexture(TextureData data)
     {
-        std::string normalizedPath = mVfs->normalizeFilename(data.mPath);
-        if (!mVfs->exists(normalizedPath))
-        {
-            auto error = Misc::StringUtils::format("Texture with path \"%s\" doesn't exist", data.mPath);
-            throw std::logic_error(error);
-        }
-        data.mPath = normalizedPath;
-        
-        TextureResources& list = mTextures[normalizedPath];
+        data.mPath = mVfs->normalizeFilename(data.mPath);
+
+        TextureResources& list = mTextures[data.mPath];
         list.push_back(std::make_shared<TextureResource>(data));
         return list.back();
     }
