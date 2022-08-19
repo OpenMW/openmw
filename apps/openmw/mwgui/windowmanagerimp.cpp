5863790c74fd387f3f3315e034b6616b00c17bbb
Mads Buvik Sandvei <madssandvei@protonmail.com>
2022-08-19 21:15:50 +0200

    Show a message if player attempts to access postprocessor hud when postprocessing is disabled

--- apps/openmw/mwgui/windowmanagerimp.cpp
+++ apps/openmw/mwgui/windowmanagerimp.cpp
@@ -2079,7 +2079,10 @@ namespace MWGui
     void WindowManager::togglePostProcessorHud()
     {
         if (!MWBase::Environment::get().getWorld()->getPostProcessor()->isEnabled())
+        {
+            messageBox("Postprocessor is not enabled.");
             return;
+        }
 
         bool visible = mPostProcessorHud->isVisible();
 
