7c485fae0c70990e7119a85301e7715aafb32be5
jvoisin <julien.voisin@dustri.org>
2022-10-06 22:59:33 +0200

    Make use of `git ls-files` filtering instead of piping to grep

--- CI/check_clang_format.sh
+++ CI/check_clang_format.sh
@@ -3,8 +3,6 @@
 set -o pipefail
 
 CLANG_FORMAT="${CLANG_FORMAT:-clang-format}"
-git ls-files |
-    grep -v '^extern/' |
-    grep -P '\.(cpp|hpp|h)$' |
+git ls-files -- ':(exclude)extern/' '*.cpp' '*.hpp' '*.h' |
     xargs -I '{}' -P $(nproc) bash -ec "\"${CLANG_FORMAT:?}\" --dry-run -Werror \"\${0:?}\" &> /dev/null || \"${CLANG_FORMAT:?}\" \"\${0:?}\" | git diff --color=always --no-index \"\${0:?}\" -" '{}' ||
     ( echo "clang-format differences detected"; exit -1 )
