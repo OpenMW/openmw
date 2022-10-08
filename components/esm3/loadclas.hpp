60c26832d264692d24dee6c59ec9c2b7fcf5ddbb
Evil Eye <malusluminis@hotmail.com>
2022-10-08 17:00:33 +0200

    Use class service flags for autocalc NPCs

--- components/esm3/loadclas.hpp
+++ components/esm3/loadclas.hpp
@@ -14,37 +14,12 @@ namespace ESM
     /*
      * Character class definitions
      */
-
-    // These flags tells us which items should be auto-calculated for this
-    // class
     struct Class
     {
         constexpr static RecNameInts sRecordId = REC_CLAS;
 
         /// Return a string descriptor for this record type. Currently used for debugging / error logs only.
         static std::string_view getRecordType() { return "Class"; }
-
-        enum AutoCalc
-        {
-            Weapon = 0x00001,
-            Armor = 0x00002,
-            Clothing = 0x00004,
-            Books = 0x00008,
-            Ingredient = 0x00010,
-            Lockpick = 0x00020,
-            Probe = 0x00040,
-            Lights = 0x00080,
-            Apparatus = 0x00100,
-            Repair = 0x00200,
-            Misc = 0x00400,
-            Spells = 0x00800,
-            MagicItems = 0x01000,
-            Potions = 0x02000,
-            Training = 0x04000,
-            Spellmaking = 0x08000,
-            Enchanting = 0x10000,
-            RepairItem = 0x20000
-        };
 
         enum Specialization
         {
@@ -62,9 +37,7 @@ namespace ESM
             int mSpecialization; // 0 = Combat, 1 = Magic, 2 = Stealth
             int mSkills[5][2]; // Minor and major skills.
             int mIsPlayable; // 0x0001 - Playable class
-
-            // I have no idea how to autocalculate these items...
-            int mCalc;
+            int mServices;
 
             int& getSkill(int index, bool major);
             ///< Throws an exception for invalid values of \a index.
