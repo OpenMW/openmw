fdb731a6d067be0af0094a8bc47398c57d4c4068
elsid <elsid.mail@gmail.com>
2022-11-20 14:22:03 +0100

    Set collada_dom version before finding the package
    The package cmake config uses wrong variables for minor and major versions. They
    should be collada_dom_VERSION_MINOR and collada_dom_VERSION_MAJOR but they are
    COLLADA_DOM_VERSION_MINOR and COLLADA_DOM_VERSION_MAJOR. CMake variables are
    case sensitive so used variables are just empty strings. Set them manually as a
    workaround.

--- CI/install_debian_deps.sh
+++ CI/install_debian_deps.sh
@@ -25,7 +25,7 @@ declare -rA GROUPED_DEPS=(
   "
 
   # These dependencies can alternatively be built and linked statically.
-  [openmw-deps-dynamic]="libmygui-dev libopenscenegraph-dev libsqlite3-dev"
+  [openmw-deps-dynamic]="libmygui-dev libopenscenegraph-dev libsqlite3-dev libcollada-dom-dev"
   [clang-tidy]="clang-tidy"
 
   # Pre-requisites for building MyGUI and OSG for static linking.
