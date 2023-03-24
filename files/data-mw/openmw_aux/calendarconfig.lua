f442e2831ed61c0b7d980ece603b57690bb3d50d
Petr Mikheev <ptmikheev@gmail.com>
2023-03-24 19:27:25 +0100

    Fixes in calendar.lua

---
+++ files/data-mw/openmw_aux/calendarconfig.lua
@@ -0,0 +1,7 @@
+return {
+    monthsDuration = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    daysInWeek = 7,
+    startingYear = 427,
+    startingYearDay = 227,
+    startingWeekDay = 1,
+}
