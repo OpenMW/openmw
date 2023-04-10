bc87b0535d892b03ca58a5ac9ab6e6defe741ea2
Sisah2 <64099446+Sisah2@users.noreply.github.com>
2023-04-10 17:54:55 +0200

    Initialize new settings

--- components/settings/categories/groundcover.hpp
+++ components/settings/categories/groundcover.hpp
@@ -17,6 +17,10 @@ namespace Settings
     struct GroundcoverCategory
     {
         SettingValue<bool> mEnabled{ "Groundcover", "enabled" };
+        SettingValue<bool> mPaging{ "Groundcover", "paging" };
+        SettingValue<bool> mAutoLoad{ "Groundcover", "auto load" };
+        SettingValue<float> mMinChunkSize{ "Groundcover", "min chunk size", makeClampSanitizerFloat(0.125, 1) };
+        SettingValue<float> mMergeFactor{ "Groundcover", "merge factor", makeClampSanitizerFloat(0, 5000) };
         SettingValue<float> mDensity{ "Groundcover", "density", makeClampSanitizerFloat(0, 1) };
         SettingValue<float> mRenderingDistance{ "Groundcover", "rendering distance", makeMaxSanitizerFloat(0) };
         SettingValue<int> mStompMode{ "Groundcover", "stomp mode", makeEnumSanitizerInt({ 0, 1, 2 }) };
