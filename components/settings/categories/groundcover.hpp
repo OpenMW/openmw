37a010e2c808c0288b71af782cfb301ffe3b866b
Sisah2 <64099446+Sisah2@users.noreply.github.com>
2023-04-15 12:06:21 +0200

    Correct it

--- components/settings/categories/groundcover.hpp
+++ components/settings/categories/groundcover.hpp
@@ -20,8 +20,8 @@ namespace Settings
         SettingValue<bool> mPaging{ "Groundcover", "paging" };
         SettingValue<bool> mAutoLoad{ "Groundcover", "auto load" };
         SettingValue<float> mMinChunkSize{ "Groundcover", "min chunk size", makeClampSanitizerFloat(0.125, 1) };
-        SettingValue<float> mMergeFactor{ "Groundcover", "merge factor", makeClampSanitizerFloat(0, 5000) };
-        SettingValue<float> mDensity{ "Groundcover", "density", makeClampSanitizerFloat(0, 1) };
+        SettingValue<float> mMergeFactor{ "Groundcover", "merge factor", makeMaxSanitizerFloat(0) };
+        SettingValue<float> mDensity{ "Groundcover", "density", makeClampSanitizerFloat(0, 100) };
         SettingValue<float> mRenderingDistance{ "Groundcover", "rendering distance", makeMaxSanitizerFloat(0) };
         SettingValue<int> mStompMode{ "Groundcover", "stomp mode", makeEnumSanitizerInt({ 0, 1, 2 }) };
         SettingValue<int> mStompIntensity{ "Groundcover", "stomp intensity", makeEnumSanitizerInt({ 0, 1, 2 }) };
