ce5fbd6b6816547c5791d5adfacbf4deaed3ba21
elsid <elsid.mail@gmail.com>
2023-04-28 13:28:52 +0200

    Write separator between Detour status flags

--- components/detournavigator/debug.cpp
+++ components/detournavigator/debug.cpp
@@ -114,9 +114,17 @@ namespace DetourNavigator
 
     std::ostream& operator<<(std::ostream& stream, const WriteDtStatus& value)
     {
-        for (const auto& status : dtStatuses)
-            if (value.mStatus & status.mStatus)
-                stream << status.mString;
+        bool first = true;
+        for (const auto& v : dtStatuses)
+        {
+            if ((value.mStatus & v.mStatus) == 0)
+                continue;
+            if (first)
+                first = false;
+            else
+                stream << " | ";
+            stream << v.mString;
+        }
         return stream;
     }
 
