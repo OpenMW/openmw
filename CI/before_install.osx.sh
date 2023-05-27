a5c2ad45dc462e5f416d68b687f8258a92875c7c
psi29a <psi29a@gmail.com>
2023-05-27 13:34:31 +0000

    Support macOS: AMD64 and ARM64

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -4,27 +4,36 @@ export HOMEBREW_NO_EMOJI=1
 
 brew uninstall --ignore-dependencies python@3.8 || true
 brew uninstall --ignore-dependencies python@3.9 || true
-brew uninstall --ignore-dependencies qt@6 || true
+brew uninstall --ignore-dependencies qt@5 || true
 brew uninstall --ignore-dependencies jpeg || true
 
 brew tap --repair
 brew update --quiet
 
 # Some of these tools can come from places other than brew, so check before installing
-brew reinstall xquartz fontconfig
+brew reinstall xquartz fontconfig freetype harfbuzz brotli
+
+# Fix: can't open file: @loader_path/libbrotlicommon.1.dylib (No such file or directory)
+BREW_LIB_PATH="$(brew --prefix)/lib"
+install_name_tool -change "@loader_path/libbrotlicommon.1.dylib" "${BREW_LIB_PATH}/libbrotlicommon.1.dylib" ${BREW_LIB_PATH}/libbrotlidec.1.dylib
+install_name_tool -change "@loader_path/libbrotlicommon.1.dylib" "${BREW_LIB_PATH}/libbrotlicommon.1.dylib" ${BREW_LIB_PATH}/libbrotlienc.1.dylib
 
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
-command -v qmake >/dev/null 2>&1 || brew install qt@5
+command -v qmake >/dev/null 2>&1 || brew install qt@6
 
 # Install deps
 brew install icu4c yaml-cpp sqlite
-export PATH="/usr/local/opt/qt@5/bin:$PATH"  # needed to use qmake in none default path as qt now points to qt6
 
 ccache --version
 cmake --version
 qmake --version
 
-curl -fSL -R -J https://gitlab.com/OpenMW/openmw-deps/-/raw/main/macos/openmw-deps-20221113.zip -o ~/openmw-deps.zip
-unzip -o ~/openmw-deps.zip -d /private/tmp/openmw-deps > /dev/null
+if [[ "${MACOS_AMD64}" ]]; then
+    curl -fSL -R -J https://gitlab.com/OpenMW/openmw-deps/-/raw/main/macos/openmw-deps-20221113.zip -o ~/openmw-deps.zip
+else
+    curl -fSL -R -J https://gitlab.com/OpenMW/openmw-deps/-/raw/main/macos/openmw-deps-20230223_arm64.zip -o ~/openmw-deps.zip
+fi
+
+unzip -o ~/openmw-deps.zip -d /tmp > /dev/null
 
