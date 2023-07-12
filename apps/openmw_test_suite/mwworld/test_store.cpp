974b4739fa56e74f813718984ccc9b76d669bf1d
Evil Eye <malusluminis@hotmail.com>
2023-07-12 21:38:20 +0200

    Add a test for three way merges

--- apps/openmw_test_suite/mwworld/test_store.cpp
+++ apps/openmw_test_suite/mwworld/test_store.cpp
@@ -833,4 +833,30 @@ namespace
         EXPECT_THAT(dialogue->mInfo, ElementsAre(HasIdEqualTo("info0"), HasIdEqualTo("info2"), HasIdEqualTo("info1")));
         EXPECT_EQ(std::next(dialogue->mInfo.begin())->mActor, "newActor");
     }
+
+    TEST(MWWorldStoreTest, shouldPreservePositionWhenPrevIsTheSame)
+    {
+        const DialogueData data = generateDialogueWithInfos(3);
+
+        MWWorld::ESMStore esmStore;
+        loadEsmStore(0, saveDialogueWithInfos(data.mDialogue, data.mInfos), esmStore);
+
+        ESM::DialInfo newInfo = data.mInfos[0];
+        newInfo.mPrev = data.mInfos[2].mId;
+        newInfo.mNext = {};
+
+        loadEsmStore(1, saveDialogueWithInfos(data.mDialogue, std::array{ newInfo }), esmStore);
+
+        newInfo = data.mInfos[1];
+        newInfo.mResponse = "test";
+
+        loadEsmStore(2, saveDialogueWithInfos(data.mDialogue, std::array{ newInfo }), esmStore);
+
+        esmStore.setUp();
+
+        const ESM::Dialogue* dialogue = esmStore.get<ESM::Dialogue>().search(ESM::RefId::stringRefId("dialogue"));
+        ASSERT_NE(dialogue, nullptr);
+        EXPECT_THAT(dialogue->mInfo, ElementsAre(HasIdEqualTo("info1"), HasIdEqualTo("info2"), HasIdEqualTo("info0")));
+        EXPECT_EQ(dialogue->mInfo.begin()->mResponse, "test");
+    }
 }
