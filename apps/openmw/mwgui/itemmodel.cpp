61f1f4a013cc189e88189e5f22a505293d546f4a
Mads Buvik Sandvei <madssandvei@zoho.com>
2023-07-17 20:41:33 +0200

    signed/unsigned comparison warning.

--- apps/openmw/mwgui/itemmodel.cpp
+++ apps/openmw/mwgui/itemmodel.cpp
@@ -61,7 +61,7 @@ namespace MWGui
     MWWorld::Ptr ItemModel::moveItem(const ItemStack& item, size_t count, ItemModel* otherModel, bool allowAutoEquip)
     {
         MWWorld::Ptr ret = MWWorld::Ptr();
-        if (item.mBase.getRefData().getCount() <= count)
+        if (static_cast<size_t>(item.mBase.getRefData().getCount()) <= count)
         {
             // We are moving the full stack
             ret = otherModel->addItem(item, count, allowAutoEquip);
