ba1b3ae962374bd1f5c9216fec78bd053a9856d2
elsid <elsid.mail@gmail.com>
2023-08-19 01:29:08 +0200

    Avoid duplicated addEntryToObjectCache call

--- apps/openmw/mwrender/landmanager.cpp
+++ apps/openmw/mwrender/landmanager.cpp
@@ -24,25 +24,25 @@ namespace MWRender
             return static_cast<ESMTerrain::LandObject*>(obj.get());
 
         const MWBase::World& world = *MWBase::Environment::get().getWorld();
+        osg::ref_ptr<ESMTerrain::LandObject> landObj = nullptr;
 
         if (ESM::isEsm4Ext(cellIndex.mWorldspace))
         {
             const ESM4::Land* land = world.getStore().get<ESM4::Land>().search(cellIndex);
             if (land == nullptr)
                 return nullptr;
-            osg::ref_ptr<ESMTerrain::LandObject> landObj(new ESMTerrain::LandObject(*land, mLoadFlags));
-            mCache->addEntryToObjectCache(cellIndex, landObj.get());
-            return landObj;
+            landObj = new ESMTerrain::LandObject(*land, mLoadFlags);
         }
         else
         {
             const ESM::Land* land = world.getStore().get<ESM::Land>().search(cellIndex.mX, cellIndex.mY);
             if (land == nullptr)
                 return nullptr;
-            osg::ref_ptr<ESMTerrain::LandObject> landObj(new ESMTerrain::LandObject(*land, mLoadFlags));
-            mCache->addEntryToObjectCache(cellIndex, landObj.get());
-            return landObj;
+            landObj = new ESMTerrain::LandObject(*land, mLoadFlags);
         }
+
+        mCache->addEntryToObjectCache(cellIndex, landObj.get());
+        return landObj;
     }
 
     void LandManager::reportStats(unsigned int frameNumber, osg::Stats* stats) const
