f05318824734e9989be431eeffdb4ae29129ccf7
Andrei Kortunov <andrei.kortunov@yandex.ru>
2023-08-18 11:18:35 +0400

    Increase Lua API version

--- apps/openmw/mwlua/luabindings.cpp
+++ apps/openmw/mwlua/luabindings.cpp
@@ -114,7 +114,7 @@ namespace MWLua
     {
         auto* lua = context.mLua;
         sol::table api(lua->sol(), sol::create);
-        api["API_REVISION"] = 42;
+        api["API_REVISION"] = 43;
         api["quit"] = [lua]() {
             Log(Debug::Warning) << "Quit requested by a Lua script.\n" << lua->debugTraceback();
             MWBase::Environment::get().getStateManager()->requestQuit();
