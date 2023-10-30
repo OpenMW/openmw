9a6c2fd2ccab30ee6e9b14643562cd4e7de5233f
Alexei Kotov <alexdobrohotov@yandex.ru>
2023-10-30 23:23:39 +0300

    Fill out ESM4::ItemMod

--- components/esm4/loadimod.cpp
+++ components/esm4/loadimod.cpp
@@ -46,16 +46,50 @@ void ESM4::ItemMod::load(ESM4::Reader& reader)
             case ESM4::SUB_EDID:
                 reader.getZString(mEditorId);
                 break;
-            case ESM4::SUB_OBND:
             case ESM4::SUB_FULL:
+                reader.getLocalizedString(mFullName);
+                break;
             case ESM4::SUB_MODL:
-            case ESM4::SUB_ICON:
-            case ESM4::SUB_MICO:
-            case ESM4::SUB_SCRI:
+                reader.getZString(mModel);
+                break;
+            case ESM4::SUB_MODB:
+                reader.get(mBoundRadius);
+                break;
             case ESM4::SUB_DESC:
+                reader.getLocalizedString(mText);
+                break;
+            case ESM4::SUB_ICON:
+                reader.getZString(mIcon);
+                break;
+            case ESM4::SUB_MICO:
+                reader.getZString(mMiniIcon);
+                break;
+            case ESM4::SUB_SCRI:
+                reader.getFormId(mScriptId);
+                break;
             case ESM4::SUB_YNAM:
+                reader.getFormId(mPickUpSound);
+                break;
             case ESM4::SUB_ZNAM:
+                reader.getFormId(mDropSound);
+                break;
             case ESM4::SUB_DATA:
+                reader.get(mData.mValue);
+                reader.get(mData.mWeight);
+                break;
+            case ESM4::SUB_OBND:
+            case ESM4::SUB_MODT: // Model data
+            case ESM4::SUB_MODS:
+            case ESM4::SUB_MODD: // Model data end
+            case ESM4::SUB_DAMC: // Destructible
+            case ESM4::SUB_DEST:
+            case ESM4::SUB_DMDC:
+            case ESM4::SUB_DMDL:
+            case ESM4::SUB_DMDT:
+            case ESM4::SUB_DMDS:
+            case ESM4::SUB_DSTA:
+            case ESM4::SUB_DSTD:
+            case ESM4::SUB_DSTF: // Destructible end
                 reader.skipSubRecordData();
                 break;
             default:
