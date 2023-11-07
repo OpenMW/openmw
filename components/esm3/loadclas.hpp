ec81bd7f1bd5e211e009130477efb7acd52427cd
Zackhasacat <tobias@tribble.dev>
2023-11-07 12:01:54 -0600

    rename specilizationIndexToLuaId

--- components/esm3/loadclas.hpp
+++ components/esm3/loadclas.hpp
@@ -30,9 +30,9 @@ namespace ESM
             Magic = 1,
             Stealth = 2
         };
-        
+
         static const std::string_view sGmstSpecializationIds[3];
-        static const std::array<std::string_view, 3> indexToLuaId;
+        static const std::array<std::string_view, 3> specilizationIndexToLuaId;
 
         struct CLDTstruct
         {
