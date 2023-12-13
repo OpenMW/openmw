OpenMW/openmw

commit e07d8f30662512b5f376bfdaaee2040be83ce112

Add separate setting type for subcategory

apps/opencs/model/prefs/boolsetting.cpp
@@ -24,7 +24,7 @@ CSMPrefs::BoolSetting& CSMPrefs::BoolSetting::setTooltip(const std::string& tool
     return *this;
 }
 
-std::pair<QWidget*, QWidget*> CSMPrefs::BoolSetting::makeWidgets(QWidget* parent)
+CSMPrefs::SettingWidgets CSMPrefs::BoolSetting::makeWidgets(QWidget* parent)
 {
     mWidget = new QCheckBox(getLabel(), parent);
     mWidget->setCheckState(mDefault ? Qt::Checked : Qt::Unchecked);
@@ -37,7 +37,7 @@ std::pair<QWidget*, QWidget*> CSMPrefs::BoolSetting::makeWidgets(QWidget* parent
 
     connect(mWidget, &QCheckBox::stateChanged, this, &BoolSetting::valueChanged);
 
-    return std::make_pair(static_cast<QWidget*>(nullptr), mWidget);
+    return SettingWidgets{ .mLabel = nullptr, .mInput = mWidget, .mLayout = nullptr };
 }
 
 void CSMPrefs::BoolSetting::updateWidget()

apps/opencs/model/prefs/coloursetting.cpp
@@ -27,7 +27,7 @@ CSMPrefs::ColourSetting& CSMPrefs::ColourSetting::setTooltip(const std::string& 
     return *this;
 }
 
-std::pair<QWidget*, QWidget*> CSMPrefs::ColourSetting::makeWidgets(QWidget* parent)
+CSMPrefs::SettingWidgets CSMPrefs::ColourSetting::makeWidgets(QWidget* parent)
 {
     QLabel* label = new QLabel(getLabel(), parent);
 
@@ -42,7 +42,7 @@ std::pair<QWidget*, QWidget*> CSMPrefs::ColourSetting::makeWidgets(QWidget* pare
 
     connect(mWidget, &CSVWidget::ColorEditor::pickingFinished, this, &ColourSetting::valueChanged);
 
-    return std::make_pair(label, mWidget);
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
 }
 
 void CSMPrefs::ColourSetting::updateWidget()

apps/opencs/model/prefs/doublesetting.cpp
@@ -56,7 +56,7 @@ CSMPrefs::DoubleSetting& CSMPrefs::DoubleSetting::setTooltip(const std::string& 
     return *this;
 }
 
-std::pair<QWidget*, QWidget*> CSMPrefs::DoubleSetting::makeWidgets(QWidget* parent)
+CSMPrefs::SettingWidgets CSMPrefs::DoubleSetting::makeWidgets(QWidget* parent)
 {
     QLabel* label = new QLabel(getLabel(), parent);
 
@@ -74,7 +74,7 @@ std::pair<QWidget*, QWidget*> CSMPrefs::DoubleSetting::makeWidgets(QWidget* pare
 
     connect(mWidget, qOverload<double>(&QDoubleSpinBox::valueChanged), this, &DoubleSetting::valueChanged);
 
-    return std::make_pair(label, mWidget);
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
 }
 
 void CSMPrefs::DoubleSetting::updateWidget()

apps/opencs/model/prefs/enumsetting.cpp
@@ -76,7 +76,7 @@ CSMPrefs::EnumSetting& CSMPrefs::EnumSetting::addValue(const std::string& value,
     return *this;
 }
 
-std::pair<QWidget*, QWidget*> CSMPrefs::EnumSetting::makeWidgets(QWidget* parent)
+CSMPrefs::SettingWidgets CSMPrefs::EnumSetting::makeWidgets(QWidget* parent)
 {
     QLabel* label = new QLabel(getLabel(), parent);
 
@@ -105,7 +105,7 @@ std::pair<QWidget*, QWidget*> CSMPrefs::EnumSetting::makeWidgets(QWidget* parent
 
     connect(mWidget, qOverload<int>(&QComboBox::currentIndexChanged), this, &EnumSetting::valueChanged);
 
-    return std::make_pair(label, mWidget);
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
 }
 
 void CSMPrefs::EnumSetting::updateWidget()

apps/opencs/model/prefs/intsetting.cpp
@@ -49,7 +49,7 @@ CSMPrefs::IntSetting& CSMPrefs::IntSetting::setTooltip(const std::string& toolti
     return *this;
 }
 
-std::pair<QWidget*, QWidget*> CSMPrefs::IntSetting::makeWidgets(QWidget* parent)
+CSMPrefs::SettingWidgets CSMPrefs::IntSetting::makeWidgets(QWidget* parent)
 {
     QLabel* label = new QLabel(getLabel(), parent);
 
@@ -66,7 +66,7 @@ std::pair<QWidget*, QWidget*> CSMPrefs::IntSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, qOverload<int>(&QSpinBox::valueChanged), this, &IntSetting::valueChanged);
 
-    return std::make_pair(label, mWidget);
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
 }
 
 void CSMPrefs::IntSetting::updateWidget()

apps/opencs/model/prefs/modifiersetting.cpp
@@ -26,7 +26,7 @@ namespace CSMPrefs
     {
     }
 
-    std::pair<QWidget*, QWidget*> ModifierSetting::makeWidgets(QWidget* parent)
+    SettingWidgets ModifierSetting::makeWidgets(QWidget* parent)
     {
         int modifier = 0;
         State::get().getShortcutManager().getModifier(getKey(), modifier);
@@ -46,7 +46,7 @@ namespace CSMPrefs
 
         connect(widget, &QPushButton::toggled, this, &ModifierSetting::buttonToggled);
 
-        return std::make_pair(label, widget);
+        return SettingWidgets{ .mLabel = label, .mInput = widget, .mLayout = nullptr };
     }
 
     void ModifierSetting::updateWidget()

apps/opencs/model/prefs/setting.hpp
@@ -9,10 +9,19 @@
 class QWidget;
 class QColor;
 class QMutex;
+class QGridLayout;
+class QLabel;
 
 namespace CSMPrefs
 {
     class Category;
+
+    struct SettingWidgets
+    {
+        QLabel* mLabel;
+        QWidget* mInput;
+        QGridLayout* mLayout;
+    };
 
     class Setting : public QObject
     {
@@ -31,16 +40,12 @@ namespace CSMPrefs
 
         ~Setting() override = default;
 
-        /// Return label, input widget.
-        ///
-        /// \note first can be a 0-pointer, which means that the label is part of the input
-        /// widget.
-        virtual std::pair<QWidget*, QWidget*> makeWidgets(QWidget* parent);
+        virtual SettingWidgets makeWidgets(QWidget* parent) = 0;
 
         /// Updates the widget returned by makeWidgets() to the current setting.
         ///
         /// \note If make_widgets() has not been called yet then nothing happens.
-        virtual void updateWidget();
+        virtual void updateWidget() = 0;
 
         const Category* getParent() const;
 

apps/opencs/model/prefs/shortcutsetting.cpp
@@ -30,7 +30,7 @@ namespace CSMPrefs
         }
     }
 
-    std::pair<QWidget*, QWidget*> ShortcutSetting::makeWidgets(QWidget* parent)
+    SettingWidgets ShortcutSetting::makeWidgets(QWidget* parent)
     {
         QKeySequence sequence;
         State::get().getShortcutManager().getSequence(getKey(), sequence);
@@ -50,7 +50,7 @@ namespace CSMPrefs
 
         connect(widget, &QPushButton::toggled, this, &ShortcutSetting::buttonToggled);
 
-        return std::make_pair(label, widget);
+        return SettingWidgets{ .mLabel = label, .mInput = widget, .mLayout = nullptr };
     }
 
     void ShortcutSetting::updateWidget()

commit a29ae07957a0a80be8090f618778337698c6aa4b

Fix CS Key Binding settings page
This got broken by e07d8f3066. Creating QGridLayout with parent and setting it
later has not the same effect.

apps/opencs/model/prefs/boolsetting.cpp
@@ -36,7 +36,7 @@ CSMPrefs::SettingWidgets CSMPrefs::BoolSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, &QCheckBox::stateChanged, this, &BoolSetting::valueChanged);
 
-    return SettingWidgets{ .mLabel = nullptr, .mInput = mWidget, .mLayout = nullptr };
+    return SettingWidgets{ .mLabel = nullptr, .mInput = mWidget };
 }
 
 void CSMPrefs::BoolSetting::updateWidget()

apps/opencs/model/prefs/coloursetting.cpp
@@ -41,7 +41,7 @@ CSMPrefs::SettingWidgets CSMPrefs::ColourSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, &CSVWidget::ColorEditor::pickingFinished, this, &ColourSetting::valueChanged);
 
-    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget };
 }
 
 void CSMPrefs::ColourSetting::updateWidget()

apps/opencs/model/prefs/doublesetting.cpp
@@ -73,7 +73,7 @@ CSMPrefs::SettingWidgets CSMPrefs::DoubleSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, qOverload<double>(&QDoubleSpinBox::valueChanged), this, &DoubleSetting::valueChanged);
 
-    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget };
 }
 
 void CSMPrefs::DoubleSetting::updateWidget()

apps/opencs/model/prefs/enumsetting.cpp
@@ -106,7 +106,7 @@ CSMPrefs::SettingWidgets CSMPrefs::EnumSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, qOverload<int>(&QComboBox::currentIndexChanged), this, &EnumSetting::valueChanged);
 
-    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget };
 }
 
 void CSMPrefs::EnumSetting::updateWidget()

apps/opencs/model/prefs/intsetting.cpp
@@ -65,7 +65,7 @@ CSMPrefs::SettingWidgets CSMPrefs::IntSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, qOverload<int>(&QSpinBox::valueChanged), this, &IntSetting::valueChanged);
 
-    return SettingWidgets{ .mLabel = label, .mInput = mWidget, .mLayout = nullptr };
+    return SettingWidgets{ .mLabel = label, .mInput = mWidget };
 }
 
 void CSMPrefs::IntSetting::updateWidget()

apps/opencs/model/prefs/modifiersetting.cpp
@@ -47,7 +47,7 @@ namespace CSMPrefs
 
         connect(widget, &QPushButton::toggled, this, &ModifierSetting::buttonToggled);
 
-        return SettingWidgets{ .mLabel = label, .mInput = widget, .mLayout = nullptr };
+        return SettingWidgets{ .mLabel = label, .mInput = widget };
     }
 
     void ModifierSetting::updateWidget()

apps/opencs/model/prefs/setting.hpp
@@ -23,7 +23,6 @@ namespace CSMPrefs
     {
         QLabel* mLabel;
         QWidget* mInput;
-        QGridLayout* mLayout;
     };
 
     class Setting : public QObject

apps/opencs/model/prefs/shortcutsetting.cpp
@@ -51,7 +51,7 @@ namespace CSMPrefs
 
         connect(widget, &QPushButton::toggled, this, &ShortcutSetting::buttonToggled);
 
-        return SettingWidgets{ .mLabel = label, .mInput = widget, .mLayout = nullptr };
+        return SettingWidgets{ .mLabel = label, .mInput = widget };
     }
 
     void ShortcutSetting::updateWidget()

apps/opencs/model/prefs/stringsetting.cpp
@@ -36,7 +36,7 @@ CSMPrefs::SettingWidgets CSMPrefs::StringSetting::makeWidgets(QWidget* parent)
 
     connect(mWidget, &QLineEdit::textChanged, this, &StringSetting::textChanged);
 
-    return SettingWidgets{ .mLabel = nullptr, .mInput = mWidget, .mLayout = nullptr };
+    return SettingWidgets{ .mLabel = nullptr, .mInput = mWidget };
 }
 
 void CSMPrefs::StringSetting::updateWidget()

apps/opencs/model/prefs/subcategory.cpp
@@ -13,9 +13,6 @@ namespace CSMPrefs
 
     SettingWidgets Subcategory::makeWidgets(QWidget* /*parent*/)
     {
-        QGridLayout* const layout = new QGridLayout();
-        layout->setSizeConstraint(QLayout::SetMinAndMaxSize);
-
-        return SettingWidgets{ .mLabel = nullptr, .mInput = nullptr, .mLayout = layout };
+        return SettingWidgets{ .mLabel = nullptr, .mInput = nullptr };
     }
 }

apps/opencs/view/prefs/keybindingpage.cpp
@@ -81,12 +81,12 @@ namespace CSVPrefs
             int next = mPageLayout->rowCount();
             mPageLayout->addWidget(widgets.mInput, next, 0, 1, 2);
         }
-        else if (widgets.mLayout != nullptr)
+        else
         {
             // Create new page
             QWidget* pageWidget = new QWidget();
-            mPageLayout = widgets.mLayout;
-            mPageLayout->setParent(pageWidget);
+            mPageLayout = new QGridLayout(pageWidget);
+            mPageLayout->setSizeConstraint(QLayout::SetMinAndMaxSize);
 
             mStackedLayout->addWidget(pageWidget);
 