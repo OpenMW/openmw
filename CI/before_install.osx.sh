fc6c94fc253262e95790412e384044d621104f63
psi29a <psi29a@gmail.com>
2024-01-01 16:08:12 +0000

    Update file before_install.osx.sh

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -ex
 
 export HOMEBREW_NO_EMOJI=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
 
-brew uninstall --ignore-dependencies python@3.8 || true
-brew uninstall --ignore-dependencies python@3.9 || true
-brew uninstall --ignore-dependencies qt@6 || true
 brew uninstall --ignore-dependencies jpeg || true
 
 brew tap --repair
@@ -20,8 +18,8 @@ install_name_tool -change "@loader_path/libbrotlicommon.1.dylib" "${BREW_LIB_PAT
 
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
-command -v qmake >/dev/null 2>&1 || brew install qt@5
-export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"
+command -v qmake >/dev/null 2>&1 || brew install qt@6
+export PATH="/opt/homebrew/opt/qt@6/bin:$PATH"
 
 
 # Install deps
