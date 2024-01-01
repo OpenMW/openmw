9dcb7bb3bc4f8ee21a4a3d06529fa53d313b438b
psi29a <psi29a@gmail.com>
2024-01-01 16:59:54 +0000

    no really, qt please

--- CI/before_install.osx.sh
+++ CI/before_install.osx.sh
@@ -19,7 +19,7 @@ install_name_tool -change "@loader_path/libbrotlicommon.1.dylib" "${BREW_LIB_PAT
 
 command -v ccache >/dev/null 2>&1 || brew install ccache
 command -v cmake >/dev/null 2>&1 || brew install cmake
-command -v qmake >/dev/null 2>&1 || brew install qt@6
+command -v qmake >/dev/null 2>&1 || brew install qt
 export PATH="/opt/homebrew/opt/qt@6/bin:$PATH"
 
 
