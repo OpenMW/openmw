9a7b9572fdf9b4acbba46ac4ea5cc0c27bc919aa
Andrei Kortunov <andrei.kortunov@yandex.ru>
2024-02-05 19:13:32 +0400

    Deploy base Qt translations when needed

---
+++ components/l10n/qttranslations.hpp
@@ -0,0 +1,16 @@
+#ifndef COMPONENTS_L10N_QTTRANSLATIONS_H
+#define COMPONENTS_L10N_QTTRANSLATIONS_H
+
+#include <QApplication>
+#include <QTranslator>
+
+namespace l10n
+{
+    extern QTranslator AppTranslator;
+    extern QTranslator ComponentsTranslator;
+    extern QTranslator QtBaseAppTranslator;
+
+    void installQtTranslations(QApplication& app, const QString& appName, const QString& resourcesPath);
+}
+
+#endif // COMPONENTS_L10N_QTTRANSLATIONS_H
