d8c74e1d6267cc3af3885ea03f2fea18bf20ec26
Martin <spleefer90@gmail.com>
2024-03-06 23:22:03 +0000

    conf.py: Set navigation_with_keys to allow navigating documentation through arrow keys

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -143,7 +143,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'navigation_with_keys': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
