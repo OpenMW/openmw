72cf015401f3b455b9860a617b1bf5d9312c75f6
AnyOldName3 <krizdjali@gmail.com>
2024-03-09 20:18:41 +0000

    Make ccache viable for Windows Release builds

--- CI/before_script.msvc.sh
+++ CI/before_script.msvc.sh
@@ -528,8 +528,10 @@ if ! [ -z $UNITY_BUILD ]; then
 	add_cmake_opts "-DOPENMW_UNITY_BUILD=True"
 fi
 
-if ! [ -z $USE_CCACHE ]; then
-	add_cmake_opts "-DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+if [ -n "$USE_CCACHE" ] && ([ -n "$NMAKE" ] || [ -n "$NINJA" ]); then
+	add_cmake_opts "-DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DPRECOMPILE_HEADERS_WITH_MSVC=OFF"
+elif [ -n "$USE_CCACHE" ]; then
+	echo "Ignoring -C (CCache) as it is incompatible with Visual Studio CMake generators"
 fi
 
 # turn on LTO by default
