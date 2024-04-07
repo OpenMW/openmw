810e2c44a6d5cf1a131f15626b6365fc9c135486
AnyOldName3 <krizdjali@gmail.com>
2024-04-07 22:34:17 +0100

    Merge conflict resolution part II

--- files/lang/wizard_fr.ts
+++ files/lang/wizard_fr.ts
@@ -506,10 +506,6 @@ to default Morrowind fonts. Check this box if you still prefer original fonts ov
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Error writing OpenMW configuration file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Are you sure you want to exit the Wizard?</source>
         <translation type="unfinished"></translation>
     </message>
@@ -519,6 +515,10 @@ to default Morrowind fonts. Check this box if you still prefer original fonts ov
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;b&gt;Could not create %1&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Please make sure you have the right permissions and try again.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error writing OpenMW configuration file</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
