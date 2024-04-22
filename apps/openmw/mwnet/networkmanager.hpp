38fe168e7b44379d7a89dcc12f35e7c5b1dbd091
Dave Corley <smokydave@icloud.com>
2024-04-22 12:12:42 -0500

    FEAT: Add network manager

---
+++ apps/openmw/mwnet/networkmanager.hpp
@@ -0,0 +1,38 @@
+#ifndef NETWORKMANAGER_H_
+#define NETWORKMANAGER_H_
+
+#include <memory>
+
+#include "client.hpp"
+#include "server.hpp"
+
+namespace MWNet
+{
+    class NetworkManager
+    {
+        bool mIsServer = false;
+        std::unique_ptr<Connection> mConnection;
+
+    public:
+        NetworkManager(bool isServer)
+            : mIsServer(isServer)
+        {
+            if (isServer)
+            {
+                mConnection = std::make_unique<MWNet::Server>();
+            }
+            else
+            {
+                mConnection = std::make_unique<MWNet::Client>();
+            }
+        }
+
+        void update();
+
+        bool isServer() { return mIsServer; }
+
+        Connection& getConnection() { return *mConnection; }
+    };
+}
+
+#endif // NETWORKMANAGER_H_
