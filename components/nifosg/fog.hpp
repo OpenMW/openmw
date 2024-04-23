745be2e91bc986dddde47f211cf6a8552726c503
Alexei Kotov <alexdobrohotov@yandex.ru>
2024-04-24 01:32:12 +0300

    Implement NifOsg::Fog::compare()

--- components/nifosg/fog.hpp
+++ components/nifosg/fog.hpp
@@ -15,6 +15,21 @@ namespace NifOsg
 
         META_StateAttribute(NifOsg, Fog, FOG)
 
+        int compare(const StateAttribute& sa) const override
+        {
+            COMPARE_StateAttribute_Types(Fog, sa);
+
+            COMPARE_StateAttribute_Parameter(_mode);
+            COMPARE_StateAttribute_Parameter(_density);
+            // _start and _end are intentionally ignored as they go unused
+            COMPARE_StateAttribute_Parameter(_color);
+            COMPARE_StateAttribute_Parameter(_fogCoordinateSource);
+            COMPARE_StateAttribute_Parameter(_useRadialFog);
+            COMPARE_StateAttribute_Parameter(mDepth);
+
+            return 0;
+        }
+
         void setDepth(float depth) { mDepth = depth; }
         float getDepth() const { return mDepth; }
 
