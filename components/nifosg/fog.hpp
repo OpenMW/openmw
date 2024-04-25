7f13f6a4e920ea26721e80862e4e3fe35f6b3a47
Alexei Kotov <alexdobrohotov@yandex.ru>
2024-04-25 03:09:04 +0300

    Use osg::Fog::compare in NifOsg::Fog::compare

--- components/nifosg/fog.hpp
+++ components/nifosg/fog.hpp
@@ -17,16 +17,10 @@ namespace NifOsg
 
         int compare(const StateAttribute& sa) const override
         {
-            COMPARE_StateAttribute_Types(Fog, sa);
-
-            COMPARE_StateAttribute_Parameter(_mode);
-            COMPARE_StateAttribute_Parameter(_density);
-            // _start and _end are intentionally ignored as they go unused
-            COMPARE_StateAttribute_Parameter(_color);
-            COMPARE_StateAttribute_Parameter(_fogCoordinateSource);
-            COMPARE_StateAttribute_Parameter(_useRadialFog);
+            if (const int base = osg::Fog::compare(sa); base != 0)
+                return base;
+            const Fog& rhs = static_cast<const Fog&>(sa);
             COMPARE_StateAttribute_Parameter(mDepth);
-
             return 0;
         }
 
