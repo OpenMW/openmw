b55a0fd32a5d94ee3dde6cb355ae274254ea19a3
AnyOldName3 <krizdjali@gmail.com>
2024-04-30 13:50:50 +0100

    || and && have different binding precedence

--- components/contentselector/model/contentmodel.cpp
+++ components/contentselector/model/contentmodel.cpp
@@ -602,8 +602,8 @@ void ContentSelectorModel::ContentModel::sortFiles()
     emit layoutAboutToBeChanged();
 
     int firstModifiable = 0;
-    while (firstModifiable < mFiles.size() && mFiles.at(firstModifiable)->builtIn()
-        || mFiles.at(firstModifiable)->fromAnotherConfigFile())
+    while (firstModifiable < mFiles.size()
+        && (mFiles.at(firstModifiable)->builtIn() || mFiles.at(firstModifiable)->fromAnotherConfigFile()))
         ++firstModifiable;
 
     // Dependency sort
