88e4ec198d1b51e6d2efd17143cc76cf1fd7294f
Max Yari <maksyarik@gmail.com>
2024-06-22 00:15:19 +0200

    Changed value type in the docs

--- files/lua_api/openmw/self.lua
+++ files/lua_api/openmw/self.lua
@@ -37,7 +37,7 @@
 -- @field [parent=#ActorControls] #boolean run true - run, false - walk
 -- @field [parent=#ActorControls] #boolean sneak If true - sneak
 -- @field [parent=#ActorControls] #boolean jump If true - initiate a jump
--- @field [parent=#ActorControls] #number use Accepts an @{#AttackTYPE} value. Activates the readied weapon/spell according to a provided value. For weapons, keeping this value modified will charge the attack until set to @{#AttackTYPE.NoAttack}. If an @{#AttackTYPE} not appropriate for a currently equipped weapon provided - an appropriate @{#AttackTYPE} will be used instead.
+-- @field [parent=#ActorControls] #AttackTYPE use Activates the readied weapon/spell according to a provided value. For weapons, keeping this value modified will charge the attack until set to @{#AttackTYPE.NoAttack}. If an @{#AttackTYPE} not appropriate for a currently equipped weapon provided - an appropriate @{#AttackTYPE} will be used instead.
 
 ---
 -- @type AttackTYPE
