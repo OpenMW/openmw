35058a49d17901f25bb36be08ae010053aad922a
Evil Eye <malusluminis@hotmail.com>
2024-07-08 21:06:15 +0200

    Update deps for RTD build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenMW'
-copyright = u'2023, OpenMW Team'
+copyright = u'2024, OpenMW Team'
 
 
 # The version info for the project you're documenting, acts as replacement for
