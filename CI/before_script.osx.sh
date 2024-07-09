0858fa439d256ab52563a3c81b38d734833dc70b
Sarah Sunday <1644563-ssunday@users.noreply.gitlab.com>
2024-07-09 18:56:13 -0500

    [CI] Tweak mac build script for new deps

--- CI/before_script.osx.sh
+++ CI/before_script.osx.sh
@@ -6,14 +6,15 @@ export CC=clang
 # Silence a git warning
 git config --global advice.detachedHead false
 
+mkdir build
+cd build
+
 DEPENDENCIES_ROOT="/tmp/openmw-deps"
 
 QT_PATH=$(brew --prefix qt@5)
 ICU_PATH=$(brew --prefix icu4c)
 OPENAL_PATH=$(brew --prefix openal-soft)
 CCACHE_EXECUTABLE=$(brew --prefix ccache)/bin/ccache
-mkdir build
-cd build
 
 cmake \
 -D CMAKE_PREFIX_PATH="$DEPENDENCIES_ROOT;$QT_PATH;$OPENAL_PATH" \
@@ -22,10 +23,9 @@ cmake \
 -D CMAKE_CXX_FLAGS="-stdlib=libc++" \
 -D CMAKE_C_FLAGS_RELEASE="-g -O0" \
 -D CMAKE_CXX_FLAGS_RELEASE="-g -O0" \
--D CMAKE_OSX_DEPLOYMENT_TARGET="11" \
+-D CMAKE_OSX_DEPLOYMENT_TARGET="14.5" \
 -D CMAKE_BUILD_TYPE=RELEASE \
 -D OPENMW_OSX_DEPLOYMENT=TRUE \
--D OPENMW_USE_SYSTEM_RECASTNAVIGATION=TRUE \
 -D BUILD_OPENMW=TRUE \
 -D BUILD_OPENCS=TRUE \
 -D BUILD_ESMTOOL=TRUE \
@@ -34,6 +34,11 @@ cmake \
 -D BUILD_NIFTEST=TRUE \
 -D BUILD_NAVMESHTOOL=TRUE \
 -D BUILD_BULLETOBJECTTOOL=TRUE \
+-D OPENMW_USE_SYSTEM_BULLET=off \
+-D OPENMW_USE_SYSTEM_OSG=TRUE \
+-D OPENMW_USE_SYSTEM_RECASTNAVIGATION=off \
+-D Boost_INCLUDE_DIR="$DEPENDENCIES_ROOT/include" \
+-D OSGPlugins_LIB_DIR="$DEPENDENCIES_ROOT/lib/osgPlugins-3.6.5" \
 -D ICU_ROOT="$ICU_PATH" \
 -G"Unix Makefiles" \
 ..
