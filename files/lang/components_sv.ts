4ae239a0eeedf056ba24500809d1ce87dd0a5514
AnyOldName3 <krizdjali+gitlab@gmail.com>
2024-08-12 13:18:52 +0000

    sv translation from Lysol

--- files/lang/components_sv.ts
+++ files/lang/components_sv.ts
@@ -39,7 +39,7 @@
     </message>
     <message>
         <source>&lt;b&gt;This file is specified in a non-user config file, but does not exist in the VFS.&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Den här filen anges i en konfigfil som inte är avsedd för användare, men den finns inte i det virtuella filsystemet.&lt;/b&gt;</translation>
     </message>
 </context>
 <context>
