58b72e383664a9d17e0c16a4ed7fa27d3e52df0b
elsid <elsid.mail@gmail.com>
2024-08-20 23:55:00 +0200

    Access channels and controls via at function
    To avoid UB on out of range access.

--- extern/oics/ICSInputControlSystem.h
+++ extern/oics/ICSInputControlSystem.h
@@ -84,11 +84,11 @@ namespace ICS
 		// in seconds
 		void update(float timeSinceLastFrame);
 
-        inline Channel* getChannel(int i){ return mChannels[i]; };
+		Channel* getChannel(int i){ return mChannels.at(i); };
 		float getChannelValue(int i);
 		inline int getChannelCount(){ return (int)mChannels.size(); };
 
-		inline Control* getControl(int i){ return mControls[i]; };
+		Control* getControl(int i){ return mControls.at(i); };
 		float getControlValue(int i);
 		inline int getControlCount(){ return (int)mControls.size(); };
 		inline void addControl(Control* control){ mControls.push_back(control); };
