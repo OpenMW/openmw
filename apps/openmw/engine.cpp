ae93ffd51a82b460fc8325a12db4be4afb4a9017
sisah <jirielbunda@seznam.cz>
2025-02-16 09:16:09 -0500

    hardcode 0010-android-fix-context-being-lost-on-app-minimize patch

--- apps/openmw/engine.cpp
+++ apps/openmw/engine.cpp
@@ -483,6 +483,10 @@ void OMW::Engine::setSkipMenu(bool skipMenu, bool newGame)
     mNewGame = newGame;
 }
 
+
+// To share the viewer with Android interfaces
+osg::ref_ptr<osgViewer::Viewer> g_viewer;
+
 void OMW::Engine::createWindow()
 {
     const int screen = Settings::video().mScreen;
@@ -688,6 +692,9 @@ void OMW::Engine::createWindow()
 
     mViewer->getEventQueue()->getCurrentEventState()->setWindowRectangle(
         0, 0, graphicsWindow->getTraits()->width, graphicsWindow->getTraits()->height);
+
+    // To share the viewer with Android interfaces
+    g_viewer = mViewer;
 }
 
 void OMW::Engine::setWindowIcon()
@@ -1066,6 +1073,8 @@ void OMW::Engine::go()
     }
 
     mLuaWorker->join();
+
+    g_viewer.release();
 
     // Save user settings
     Settings::Manager::saveUser(mCfgMgr.getUserConfigPath() / "settings.cfg");
