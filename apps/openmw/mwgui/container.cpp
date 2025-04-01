a9870bbcde803890dfd2e42062787c2ca2b0bfef
Kindi <Kindi@DESKTOP-7D8MM3O>
2025-04-01 13:03:11 +0800

    update container window when item is added or removed

--- apps/openmw/mwgui/container.cpp
+++ apps/openmw/mwgui/container.cpp
@@ -160,8 +160,12 @@ namespace MWGui
         MWBase::Environment::get().getWindowManager()->setKeyFocusWidget(mCloseButton);
 
         setTitle(container.getClass().getName(container));
-
         mPtr.getClass().getContainerStore(mPtr).setContListener(this);
+    }
+
+    void ContainerWindow::updateItemView()
+    {
+        mItemView->update();
     }
 
     void ContainerWindow::resetReference()
@@ -321,10 +325,5 @@ namespace MWGui
     {
         if (mModel && mModel->usesContainer(ptr))
             MWBase::Environment::get().getWindowManager()->removeGuiMode(GM_Container);
-    }
-
-    void ContainerWindow::updateItemView()
-    {
-        mItemView->update();
     }
 }
