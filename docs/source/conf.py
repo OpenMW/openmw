28de55df6acc5f6af88488cd76562a3e2f8e0e85
Cody Glassman <glassmancody.info@gmail.com>
2025-07-04 11:19:04 -0700

    docs - use default pygment style in light mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,7 +139,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 pygments_style_dark = 'github-dark'
 
 # A list of ignored prefixes for module index sorting.
