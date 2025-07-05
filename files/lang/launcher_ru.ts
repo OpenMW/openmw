dfa3b5f7edb59ce85ece8b54bc4c9163f404e27b
Andrew Lanzone <andy@enoznal.com>
2025-07-04 23:09:31 -0700

    Add Russian translation for controller menu launcher checkboxes

--- files/lang/launcher_ru.ts
+++ files/lang/launcher_ru.ts
@@ -1464,19 +1464,19 @@ to default Morrowind fonts. Check this box if you still prefer original fonts ov
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Make it easier to use game menus with a controller.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Упрощает использование игровых меню на геймпаде.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>Enable Controller Menus</source>
-        <translation type="unfinished"></translation>
+        <translation>Интерфейс для геймпадов</translation>
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;When using controller menus, make tooltips visible by default.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Показывать подсказки интерфейса для геймпадов по умолчанию.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>Show Controller Tooltips By Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Подсказки для геймпадов по умолчанию</translation>
     </message>
 </context>
 </TS>
