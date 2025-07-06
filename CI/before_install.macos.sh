6a9b2d330210ccdeb6f98a69ad86a8a0928fb1f4
Sarah Sunday <1644563-ssunday@users.noreply.gitlab.com>
2025-07-06 17:27:40 -0500

    [CI] Brew cleanup/simplify

--- CI/before_install.macos.sh
+++ CI/before_install.macos.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -ex
 
-export HOMEBREW_NO_EMOJI=1
-export HOMEBREW_NO_INSTALL_CLEANUP=1
-export HOMEBREW_AUTOREMOVE=1
-
 if [[ "${MACOS_AMD64}" ]]; then
     ./CI/macos/before_install.amd64.sh
 else
