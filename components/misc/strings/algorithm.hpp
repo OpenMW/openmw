0d7efaa2e3895d7d6664c93fa7ecb97d7237ca3b
zlice <zlice555@gmail.com>
2025-07-16 09:00:50 -0400

    fix 32bit builds

--- components/misc/strings/algorithm.hpp
+++ components/misc/strings/algorithm.hpp
@@ -4,6 +4,7 @@
 #include "lower.hpp"
 
 #include <algorithm>
+#include <cstdint>
 #include <functional>
 #include <string>
 #include <string_view>
@@ -88,14 +89,23 @@ namespace Misc::StringUtils
         constexpr std::size_t operator()(std::string_view str) const
         {
             // FNV-1a
-            std::size_t hash{ 0xcbf29ce484222325ull };
-            constexpr std::size_t prime{ 0x00000100000001B3ull };
+            std::uint64_t hash{ 0xcbf29ce484222325ull };
+            constexpr std::uint64_t prime{ 0x00000100000001B3ull };
             for (char c : str)
             {
-                hash ^= static_cast<std::size_t>(toLower(c));
+                hash ^= static_cast<std::uint64_t>(toLower(c));
                 hash *= prime;
             }
-            return hash;
+            if constexpr (sizeof(std::uint64_t) <= sizeof(std::size_t))
+                return hash;
+            else if constexpr (sizeof(std::uint32_t) == sizeof(std::size_t))
+            {
+                std::uint32_t low = hash & 0xFFFFFFFF;
+                std::uint32_t high = hash >> 32;
+                return low ^ high;
+            }
+            static_assert(sizeof(std::uint64_t) <= sizeof(std::size_t) || sizeof(std::uint32_t) == sizeof(std::size_t));
+            return {};
         }
     };
 
