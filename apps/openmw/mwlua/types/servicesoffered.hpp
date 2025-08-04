afb7f1da54feab991f95c97656a0b27b9b0ddb62
SkyHasACat <h@tribble.dev>
2025-08-03 21:39:44 -0700

    Fix formatting

--- apps/openmw/mwlua/types/servicesoffered.hpp
+++ apps/openmw/mwlua/types/servicesoffered.hpp
@@ -1,26 +1,13 @@
 #pragma once
 #include <array>
+#include <components/esm3/loadnpc.hpp> // for ESM::NPC constants
 #include <string_view>
-#include <components/esm3/loadnpc.hpp>  // for ESM::NPC constants
 
-inline constexpr std::array<std::pair<int, std::string_view>, 19> ServiceNames = {{
-    { ESM::NPC::Spells, "Spells" },
-    { ESM::NPC::Spellmaking, "Spellmaking" },
-    { ESM::NPC::Enchanting, "Enchanting" },
-    { ESM::NPC::Training, "Training" },
-    { ESM::NPC::Repair, "Repair" },
-    { ESM::NPC::AllItems, "Barter" },
-    { ESM::NPC::Weapon, "Weapon" },
-    { ESM::NPC::Armor, "Armor" },
-    { ESM::NPC::Clothing, "Clothing" },
-    { ESM::NPC::Books, "Books" },
-    { ESM::NPC::Ingredients, "Ingredients" },
-    { ESM::NPC::Picks, "Picks" },
-    { ESM::NPC::Probes, "Probes" },
-    { ESM::NPC::Lights, "Lights" },
-    { ESM::NPC::Apparatus, "Apparatus" },
-    { ESM::NPC::RepairItem, "RepairItem" },
-    { ESM::NPC::Misc, "Misc" },
-    { ESM::NPC::Potions, "Potions" },
-    { ESM::NPC::MagicItems, "MagicItems" }
-}};
+inline constexpr std::array<std::pair<int, std::string_view>, 19> ServiceNames
+    = { { { ESM::NPC::Spells, "Spells" }, { ESM::NPC::Spellmaking, "Spellmaking" },
+        { ESM::NPC::Enchanting, "Enchanting" }, { ESM::NPC::Training, "Training" }, { ESM::NPC::Repair, "Repair" },
+        { ESM::NPC::AllItems, "Barter" }, { ESM::NPC::Weapon, "Weapon" }, { ESM::NPC::Armor, "Armor" },
+        { ESM::NPC::Clothing, "Clothing" }, { ESM::NPC::Books, "Books" }, { ESM::NPC::Ingredients, "Ingredients" },
+        { ESM::NPC::Picks, "Picks" }, { ESM::NPC::Probes, "Probes" }, { ESM::NPC::Lights, "Lights" },
+        { ESM::NPC::Apparatus, "Apparatus" }, { ESM::NPC::RepairItem, "RepairItem" }, { ESM::NPC::Misc, "Misc" },
+        { ESM::NPC::Potions, "Potions" }, { ESM::NPC::MagicItems, "MagicItems" } } };
