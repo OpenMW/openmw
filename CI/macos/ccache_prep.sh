a4be773e9c6cfbac0e8faa1c3ff0091b22eb6e83
AnyOldName3 <krizdjali@gmail.com>
2025-09-03 22:04:39 +0100

    Native ccache won't work if we insist on an architecture

--- CI/macos/ccache_prep.sh
+++ CI/macos/ccache_prep.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -ex
 
-if [[ "${MACOS_AMD64}" ]]; then
-    arch -x86_64 ccache -z -M "${CCACHE_SIZE}"
-else
-    ccache -z -M "${CCACHE_SIZE}"
-fi
+ccache -z -M "${CCACHE_SIZE}"
--- CI/macos/ccache_show_stats.sh
+++ CI/macos/ccache_show_stats.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -ex
 
-if [[ "${MACOS_AMD64}" ]]; then
-    arch -x86_64 ccache -svv
-else
-    ccache -svv
-fi
+ccache -svv
