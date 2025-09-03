68d0dff890395ca9580d5db927f65e01b8b6b4be
Sarah Sunday <1644563-ssunday@users.noreply.gitlab.com>
2025-09-02 19:54:09 -0500

    [CI] Check for cmake for arm, move s3cmd out of arch specific flow

--- CI/macos/before_install.arm64.sh
+++ CI/macos/before_install.arm64.sh
@@ -3,7 +3,9 @@
 brew tap --repair
 brew update --quiet
 
-brew install curl xquartz gd fontconfig freetype harfbuzz brotli s3cmd ccache cmake qt@6 openal-soft icu4c yaml-cpp sqlite
+command -v cmake >/dev/null 2>&1 || brew install cmake
+
+brew install curl xquartz gd fontconfig freetype harfbuzz brotli qt@6 ccache openal-soft icu4c yaml-cpp sqlite
 
 curl -fSL -R -J https://gitlab.com/OpenMW/openmw-deps/-/raw/main/macos/openmw-deps-20240818-arm64.tar.xz -o ~/openmw-deps.tar.xz
 tar xf ~/openmw-deps.tar.xz -C /tmp > /dev/null
