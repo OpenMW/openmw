dda54c1154050fb06b2cdbc61b762c0fa3da7cec
AnyOldName3 <krizdjali@gmail.com>
2025-12-01 17:24:50 +0000

    Precompiled headers are fine with ccache in 2025

--- CI/before_script.msvc.sh
+++ CI/before_script.msvc.sh
@@ -530,7 +530,7 @@ fi
 
 if [ -n "$USE_CCACHE" ]; then
 	if [ -n "$NMAKE" ] || [ -n "$NINJA" ]; then
-		add_cmake_opts "-DCMAKE_C_COMPILER_LAUNCHER=ccache  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DPRECOMPILE_HEADERS_WITH_MSVC=OFF"
+		add_cmake_opts "-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
 	else
 		echo "Ignoring -C (CCache) as it is incompatible with Visual Studio CMake generators"
 	fi
