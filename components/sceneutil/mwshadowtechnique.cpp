OpenMW/openmw

commit 2754d4264c315a424aed89cb1c92ea37834e2e9c

Ensure near plane doesn't go past far plane when there are no shadow receivers

components/sceneutil/mwshadowtechnique.cpp
@@ -645,6 +645,7 @@ MWShadowTechnique::Frustum::Frustum(osgUtil::CullVisitor* cv, double minZNear, d
     {
         osg::Matrix::value_type zNear = osg::maximum<osg::Matrix::value_type>(cv->getCalculatedNearPlane(),minZNear);
         osg::Matrix::value_type zFar = osg::minimum<osg::Matrix::value_type>(cv->getCalculatedFarPlane(),maxZFar);
+        zNear = std::min(zNear, zFar);
 
         cv->clampProjectionMatrix(projectionMatrix, zNear, zFar);
 

commit 9fbe9a02aafe6d7f874696bfd9f5d2ff327224e8

Fix #8952
Regression from 2754d4264c315a424aed89cb1c92ea37834e2e9c !5047

components/sceneutil/mwshadowtechnique.cpp
@@ -645,6 +645,8 @@ MWShadowTechnique::Frustum::Frustum(osgUtil::CullVisitor* cv, double minZNear, d
     {
         osg::Matrix::value_type zNear = osg::maximum<osg::Matrix::value_type>(cv->getCalculatedNearPlane(),minZNear);
         osg::Matrix::value_type zFar = osg::minimum<osg::Matrix::value_type>(cv->getCalculatedFarPlane(),maxZFar);
+        if (zFar < 0)
+            zFar = minZNear;
         zNear = std::min(zNear, zFar);
 
         cv->clampProjectionMatrix(projectionMatrix, zNear, zFar);