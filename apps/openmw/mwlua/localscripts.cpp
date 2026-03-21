fdc418befb51b7e0406afda28afbd11cd38279e5
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-21 08:10:47 +0000

    FIX: C L A N G

--- apps/openmw/mwlua/localscripts.cpp
+++ apps/openmw/mwlua/localscripts.cpp
@@ -77,8 +77,7 @@ namespace MWLua
         selfAPI["controls"] = sol::readonly_property([](SelfObject& self) { return &self.mControls; });
         selfAPI["isActive"] = [](SelfObject& self) -> bool { return self.mIsActive; };
         selfAPI["enableAI"] = [](SelfObject& self, bool v) { self.mControls.mDisableAI = !v; };
-        selfAPI["saveState"] = sol::property(
-            [](const SelfObject& self) { return !self.ptr().getRefData().isNoSave(); },
+        selfAPI["saveState"] = sol::property([](const SelfObject& self) { return !self.ptr().getRefData().isNoSave(); },
             [](const SelfObject& self, bool save) { self.ptr().getRefData().setNoSave(!save); });
         selfAPI["ATTACK_TYPE"]
             = LuaUtil::makeStrictReadOnly(LuaUtil::tableFromPairs<std::string_view, MWMechanics::AttackType>(lua,
