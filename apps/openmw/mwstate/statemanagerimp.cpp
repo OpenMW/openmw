dd2b7c89798e2242c57c426171032253f5281a0c
Evil Eye <malusluminis@hotmail.com>
2026-04-13 16:53:16 +0200

    Bump us up to 0.52

--- apps/openmw/mwstate/statemanagerimp.cpp
+++ apps/openmw/mwstate/statemanagerimp.cpp
@@ -675,7 +675,7 @@ void MWState::StateManager::loadGame(const Character* character, const std::file
         {
             // Insert additional else if statements above to cover future releases
             static_assert(ESM::MinSupportedSaveGameFormatVersion <= ESM::OpenMW0_49MinSaveGameFormatVersion);
-            release = "OpenMW 0.51.0";
+            release = "OpenMW 0.52.0";
         }
         auto l10n = MWBase::Environment::get().getL10nManager()->getContext("OMWEngine");
         std::string error = l10n->formatMessage("LoadingRequiresOldVersionError", { "version" }, { release });
