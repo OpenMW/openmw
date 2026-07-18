cba5ff868c2b671593d303a77dca7f46548e8edf
nox7 <36878742-nox7@users.noreply.gitlab.com>
2026-07-17 21:17:12 -0500

    Add padding header struct

---
+++ components/lua_ui/padding.hpp
@@ -0,0 +1,15 @@
+#ifndef OPENMW_LUAUI_PADDING
+#define OPENMW_LUAUI_PADDING
+
+namespace LuaUi
+{
+    struct Padding
+    {
+        int mLeft;
+        int mTop;
+        int mRight;
+        int mBottom;
+    };
+}
+
+#endif
