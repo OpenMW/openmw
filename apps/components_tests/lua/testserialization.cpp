424347fa1bf81336d08d7b5ecf96b77b338ca050
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-09-03 19:20:14 -0500

    FIX: CLANG

--- apps/components_tests/lua/testserialization.cpp
+++ apps/components_tests/lua/testserialization.cpp
@@ -7,8 +7,8 @@
 #include <osg/Vec3f>
 #include <osg/Vec4f>
 
-#include <components/lua/serialization.hpp>
 #include <components/lua/luastate.hpp>
+#include <components/lua/serialization.hpp>
 #include <components/lua/utilpackage.hpp>
 
 #include <components/misc/color.hpp>
